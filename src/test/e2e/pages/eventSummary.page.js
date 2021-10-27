d4a18e124a0d5e4ee6480871ce783daf26931898
Raghu Nakkala <nraghu31@gmail.com>
2021-10-27 09:01:59 +0100

    E2E Set up with Reporting and parallel run

---
+++ src/test/e2e/pages/eventSummary.page.js
@@ -0,0 +1,18 @@
+const { I } = inject();
+
+module.exports = {
+
+  fields: {
+    summary: '#field-trigger-summary',
+    description: '#field-trigger-description',
+  },
+
+  async provideSummary(summary, description) {
+    I.fillField(this.fields.summary, summary);
+    I.fillField(this.fields.description, description);
+  },
+
+  async submit(button, locator = '.hmcts-banner--success') {
+    await I.retryUntilExists(() => I.click(button), locator);
+  },
+};
---
+++ src/test/tests/smoke_test.js
@@ -0,0 +1,15 @@
+const config = require('../e2e/config');
+const moment = require('moment');
+
+Feature('Smoke tests @smoke-tests');
+
+Scenario('Sign in as local authority and create a case', async ({I, caseListPage}) => {
+
+
+  await I.goToPage(config.baseUrl);
+  const caseName = `Smoke test case (${moment().format('YYYY-MM-DD HH:MM')})`;
+  const caseId = await I.logInAndCreateCase(config.legalProfessionalUserOne, caseName);
+  I.navigateToCaseList();
+  await I.retryUntilExists(() => caseListPage.searchForCasesWithName(caseName, 'Open'), `//ccd-search-result/table//tr[//a[contains(@href,'${caseId}')]]`);
+  I.seeCaseInSearchResult(caseId);
+});
