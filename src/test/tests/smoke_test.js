4764f2afac278a472178ba2cd2ff9913853f6a3d
nraghu32 <44773331+nraghu32@users.noreply.github.com>
2021-10-27 12:12:37 +0100

    Update E2E dry run (#16)

--- src/test/tests/smoke_test.js
+++ src/test/tests/smoke_test.js
@@ -10,6 +10,4 @@ Scenario('Sign in as local authority and create a case', async ({I, caseListPage
   const caseName = `Smoke test case (${moment().format('YYYY-MM-DD HH:MM')})`;
   const caseId = await I.logInAndCreateCase(config.legalProfessionalUserOne, caseName);
   I.navigateToCaseList();
-  await I.retryUntilExists(() => caseListPage.searchForCasesWithName(caseName, 'Open'), `//ccd-search-result/table//tr[//a[contains(@href,'${caseId}')]]`);
-  I.seeCaseInSearchResult(caseId);
 });
