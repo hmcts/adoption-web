85a49005343c2414f156d60a1e610ac8f1c0745c
abidfs <abid.sayyad@hmcts.net>
2021-11-15 10:45:42 +0000

    improved unit test coverage

---
+++ src/test/unit/common/errorHandling.spec.ts
@@ -0,0 +1,48 @@
+/* tslint:disable:no-unused-expression */
+import { ErrorHandling } from 'shared/errorHandling';
+import * as express from 'express';
+
+const mockReq = {
+
+} as express.Request;
+
+const mockRes = {
+
+} as express.Response;
+
+describe('ErrorHandling', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const resolvingRequestHandler = jest.fn((req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+    return undefined;
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const rejectingRequestHandler = jest.fn((req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+    throw new Error('An error occurred');
+  });
+
+  const nextFunction = jest.fn(() => {
+    // Nothing to do, I'm a mock
+  });
+
+  it('should invoke given request handler', async () => {
+    const handled = ErrorHandling.apply(resolvingRequestHandler);
+    await handled(mockReq, mockRes, nextFunction);
+    expect(resolvingRequestHandler).toHaveBeenCalledWith(mockReq, mockRes, nextFunction);
+  });
+
+  it('should not invoke next function when the request handler is successful', async () => {
+    const handled = ErrorHandling.apply(resolvingRequestHandler);
+    await handled(mockReq, mockRes, nextFunction);
+    expect(nextFunction).not.toHaveBeenCalled();
+  });
+
+  it('should invoke next function when the request handler has failed', async () => {
+    const handled = ErrorHandling.apply(rejectingRequestHandler);
+    try {
+      await handled(mockReq, mockRes, nextFunction);
+    } catch (err) {
+      expect(nextFunction).not.toHaveBeenCalled();
+    }
+  });
+});
--- src/test/unit/common/router/routeFinder.spec.ts
+++ src/test/unit/common/router/routeFinder.spec.ts
@@ -1,1 +1,8 @@
-import {RouterFinder} from 'shared/router/routerFinder';
+import * as path from 'path';
+import { RouterFinder } from 'shared/router/routerFinder';
+
+describe('RouterFinder', () => {
+  test('should find all routes in a given path', () => {
+    expect(RouterFinder.findAll(path.join(__dirname, '../../../../main/routes'))).toBeInstanceOf(Array);
+  });
+});
