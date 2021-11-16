fc746b2e0b9fe3cd93565591826d8eb8cba53053
abidfs <abid.sayyad@hmcts.net>
2021-11-16 09:47:38 +0000

    using different logic to merge secrets to config

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -1,16 +1,18 @@
-import config from 'config';
+
 import * as propertiesVolume from '@hmcts/properties-volume';
 import { Application } from 'express';
 import { get, set } from 'lodash';
 
+const config = require('config');
 export class PropertiesVolume {
 
   enableFor(server: Application): void {
     if (server.locals.ENV !== 'development') {
       const result = propertiesVolume.addTo({});
       console.log(result);
+      set(config, 'secrets', { ...get(config, 'secrets'), ...result });
+      console.log(config.secrets);
       this.setSecret('secrets.adoption-kv.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
-      
     }
   }
 
