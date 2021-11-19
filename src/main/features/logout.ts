41d453e2aba5547668dc2d850e6cb9c156663d27
dilipsamrahmcts <dilip.samra@hmcts.net>
2021-11-19 15:42:11 +0000

    ADOP-113-header-alignment added logout button

---
+++ src/main/features/logout.ts
@@ -0,0 +1,38 @@
+import * as express from 'express'
+import * as config from 'config'
+import Cookies from 'cookies'
+
+import { IdamClient } from 'idam/idamClient'
+import { Logger } from '@hmcts/nodejs-logging'
+
+import { Paths } from 'paths'
+import { JwtExtractor } from 'idam/jwtExtractor'
+import { JwtUtils } from '../common/utils/jwtUtils'
+import { ErrorHandling } from '../common/errorHandling'
+import { User } from 'idam/user'
+
+const sessionCookie = config.get<string>('session.cookieName')
+const logger = Logger.getLogger('routes/logout')
+
+/* tslint:disable:no-default-export */
+export default express.Router()
+  .get(Paths.logoutReceiver.uri,
+    ErrorHandling.apply(async (req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> => {
+      const jwt: string = JwtExtractor.extract(req)
+
+      if (jwt) {
+        try {
+          const user: User = await IdamClient.retrieveUserFor(jwt)
+          await IdamClient.invalidateSession(jwt, user.bearerToken)
+        } catch (error) {
+          const { id } = JwtUtils.decodePayload(jwt)
+          logger.error(`Failed invalidating JWT for userId  ${id}`)
+        }
+
+        const cookies = new Cookies(req, res)
+        cookies.set(sessionCookie, '')
+      }
+
+      res.redirect(Paths.homePage.uri)
+    })
+  )
