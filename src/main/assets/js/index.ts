d4876f5e08907b76eeb2f43a96c0d5921dc4844c
abidfs <abid.sayyad@hmcts.net>
2021-11-23 11:16:22 +0000

    removed unwanted files from applicant1 flow

--- src/main/assets/js/index.ts
+++ src/main/assets/js/index.ts
@@ -3,8 +3,6 @@ import { initAll } from 'govuk-frontend';
 import '../scss/main.scss';
 import './go-back';
 import './session-timeout';
-import './enter-address';
-import './upload-manager';
 import './cookie';
 
 initAll();
--- src/main/steps/applicant1/where-your-lives-are-based/post.ts
+++
@@ -1,3 +0,0 @@
-import { JurisdictionPostController } from '../../../app/jurisdiction/JurisdictionPostController';
-
-export default JurisdictionPostController;
