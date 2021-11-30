7315595839ca076cff88b4382faea514ce30c981
abidfs <abid.sayyad@hmcts.net>
2021-11-30 09:56:07 +0000

    added logic to display field error even if there is only one field on the page

--- src/test/tests/smoke_test.js
+++ src/test/tests/smoke_test.js
@@ -3,7 +3,6 @@ const config = require('../e2e/config');
 Feature('Smoke tests @smoke-tests');
 
 Scenario('Sign in as citizen and create a case', async ({ I, landingPage }) => {
-  console.log(config.baseUrl);
   await I.goToPage(config.baseUrl);
   await I.signIn(config.citizenUserOne);
   await landingPage.seeTheLandingPage();
