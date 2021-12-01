e7cc6f9ee0ffbbd6c8742f5391e178a67f4de9d8
dilipsamrahmcts <dilip.samra@hmcts.net>
2021-12-01 09:48:53 +0000

    "fixed PR"

--- src/main/steps/applicant1/your-personal-details/content.ts
+++ src/main/steps/applicant1/your-personal-details/content.ts
@@ -1,49 +1,18 @@
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
-import { isFieldFilledIn } from '../../../app/form/validation';
 
 const en = () => ({
   section: 'Applicant details',
-  title: 'Your personal details?',
-  one: "I'm applying on my own",
-  two: "I'm applying with my spouse or civil partner",
-  three: "I'm applying with someone who is not my spouse or civil partner",
-  errors: {
-    noOfApplicants: {
-      required: 'Select the number of people applying to adopt',
-    },
-  },
+  title: 'Your personal details',
 });
 
 const cy = () => ({
   section: 'Applicant details (in welsh)',
-  title: 'Your personal details?(in welsh)',
-  one: "I'm applying on my own (in welsh)",
-  two: "I'm applying with my spouse or civil partner (in welsh)",
-  three: "I'm applying with someone who is not my spouse or civil partner (in welsh)",
-  errors: {
-    noOfApplicants: {
-      required: 'Select the number of people applying to adopt (in welsh)',
-    },
-  },
+  title: 'Your personal details(in welsh)',
 });
 
 export const form: FormContent = {
-  fields: {
-    noOfApplicants: {
-      type: 'radios',
-      classes: 'govuk-radios',
-      label: l => l.title,
-      section: l => l.section,
-      labelHidden: false,
-      values: [
-        { label: l => l.one, value: 'alone' },
-        { label: l => l.two, value: 'withSpouseOrCivilPartner' },
-        { label: l => l.three, value: 'withSomeoneElse' },
-      ],
-      validator: value => isFieldFilledIn(value),
-    },
-  },
+  fields: {},
   submit: {
     text: l => l.continue,
   },
