bbf4d7b336ee8879d1bb6ec13f33bb8c2344fc56
Ren Joseph <ahren.joseph@hmcts.net>
2021-12-16 15:24:01 +0000

    improvements, unit tests

--- src/main/steps/applicant1/occupation/content.test.ts
+++ src/main/steps/applicant1/occupation/content.test.ts
@@ -1,22 +1,59 @@
-import { CommonContent } from '../../common/common.content';
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable jest/expect-expect */
+import { FormContent, FormFields } from '../../../app/form/Form';
+import { CommonContent, generatePageContent } from '../../common/common.content';
 
-import { cy, en, generateContent } from './content';
+import { cy as cyFunction, en as enFunction, generateContent } from './content';
 
-const cyContent = cy();
-const enContent = en();
+const CY = 'cy';
+const EN = 'en';
+const cyContent = cyFunction();
+const enContent = enFunction();
+
+const langAssertions = (language, content) => {
+  const generatedContent = generateContent({ language, userCase: {} } as CommonContent);
+  const { section, title, occupation, warningText, details, errors } = content;
+
+  expect(generatedContent.section).toEqual(section);
+  expect(generatedContent.title).toEqual(title);
+  expect(generatedContent.occupation).toEqual(occupation);
+  expect(generatedContent.warningText).toEqual(warningText);
+  expect(generatedContent.details).toEqual(details);
+  expect(generatedContent.errors).toEqual(errors);
+};
 
 describe('occupation content', () => {
+  const commonContent = { language: EN } as CommonContent;
   it('should return the correct content for language = en', () => {
-    const generatedContent = generateContent({ language: 'en', userCase: {} } as CommonContent);
-    expect(generatedContent.section).toEqual(enContent.section);
-    expect(generatedContent.section).toEqual(enContent.section);
-    expect(generatedContent.section).toEqual(enContent.section);
-    expect(generatedContent.section).toEqual(enContent.section);
-    expect(generatedContent.section).toEqual(enContent.section);
+    langAssertions(EN, enContent);
   });
 
   it('should return the correct content for language = cy', () => {
-    const generatedContent = generateContent({ language: 'cy', userCase: {} } as CommonContent);
-    expect(generatedContent.section).toEqual(cyContent.section);
+    langAssertions(CY, cyContent);
+  });
+
+  it('should have an occupation text input field', () => {
+    const generatedContent = generateContent(commonContent);
+    const form = generatedContent.form as FormContent;
+    const fields = form.fields as FormFields;
+    const field = fields.applicant1Occupation;
+
+    expect(field.type).toBe('input');
+    expect(field.classes).toBe('govuk-input--width-20');
+    expect(field).toHaveProperty('label');
+    expect(field).toHaveProperty('labelSize');
+    expect(field).toHaveProperty('validator');
+  });
+
+  it('should contain submit button', () => {
+    const generatedContent = generateContent(commonContent);
+    const form = generatedContent.form as FormContent;
+    expect((form.submit.text as Function)(generatePageContent({ language: EN }))).toBe('Save and continue');
+  });
+
+  it('should contain saveAsDraft button', () => {
+    const generatedContent = generateContent(commonContent);
+    const form = generatedContent.form as FormContent;
+    expect((form.saveAsDraft?.text as Function)(generatePageContent({ language: EN }))).toBe('Save as draft');
   });
 });
--- src/main/steps/applicant1/occupation/content.ts
+++ src/main/steps/applicant1/occupation/content.ts
@@ -8,6 +8,20 @@ export const en = () => ({
   title: "What's your occupation?",
   occupation:
     'Enter your full occupation. For example, ‘Secondary school teacher’ rather than just ‘Teacher’. If you’re self employed, say so. For example, ‘Self employed carpenter’.',
+  warningText: {
+    text: 'This information will appear on the adoption certificate.',
+    iconFallbackText: 'Warning',
+  },
+  details: {
+    summaryText: "I'm not working at the moment",
+    html: `If you’re unemployed, say what your occupation was when you were working. For example, 'Unemployed administrative assistant'.
+  <br>
+  <br>
+  If you’re retired, say that you’re retired and what your occupation was when you were working. For example, ‘Retired hairdresser’.
+  <br>
+  <br>
+  If you’re a full time parent, enter ‘Full time parent’.`,
+  },
   errors: {
     applicant1Occupation: {
       required: 'You have not entered your occupation. Enter it before continuing.',
@@ -21,6 +35,20 @@ export const cy = () => ({
   title: "What's your occupation? (in Welsh)",
   occupation:
     'Enter your full occupation. For example, ‘Secondary school teacher’ rather than just ‘Teacher’. If you’re self employed, say so. For example, ‘Self employed carpenter’. (in Welsh)',
+  warningText: {
+    text: 'This information will appear on the adoption certificate. (in Welsh)',
+    iconFallbackText: 'Warning (in Welsh)',
+  },
+  details: {
+    summaryText: "I'm not working at the moment (in Welsh)",
+    html: `If you’re unemployed, say what your occupation was when you were working. For example, 'Unemployed administrative assistant'.
+    <br>
+    <br>
+    If you’re retired, say that you’re retired and what your occupation was when you were working. For example, ‘Retired hairdresser’.
+    <br>
+    <br>
+    If you’re a full time parent, enter ‘Full time parent’. (in Welsh)`,
+  },
   errors: {
     applicant1Occupation: {
       required: 'You have not entered your occupation. Enter it before continuing. (in Welsh)',
@@ -32,7 +60,7 @@ export const cy = () => ({
 export const form: FormContent = {
   fields: {
     applicant1Occupation: {
-      type: 'text',
+      type: 'input',
       label: l => l.occupation,
       labelSize: 'normal',
       classes: 'govuk-input--width-20',
@@ -53,20 +81,6 @@ const languages = {
 };
 
 export const generateContent: TranslationFn = content => ({
-  warningText: {
-    text: 'This information will appear on the adoption certificate.',
-    iconFallbackText: 'Warning',
-  },
-  details: {
-    summaryText: "I'm not working at the moment",
-    html: `If you’re unemployed, say what your occupation was when you were working. For example, 'Unemployed administrative assistant'.
-      <br>
-      <br>
-      If you’re retired, say that you’re retired and what your occupation was when you were working. For example, ‘Retired hairdresser’.
-      <br>
-      <br>
-      If you’re a full time parent, enter ‘Full time parent’.`,
-  },
   ...languages[content.language](),
   form,
 });
