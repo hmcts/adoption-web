3bb27ddd454685132abad4aaa1ce06e1047b25f7
Raghu Nakkala <nraghu31@gmail.com>
2021-12-20 19:23:12 +0000

    E2E test for  Primary applicant - Your personal details

--- src/test/e2e/pages/login.page.js
+++ src/test/e2e/pages/login.page.js
@@ -13,6 +13,7 @@ module.exports = {
   submitButton: 'input[value="Sign in"]',
 
   async signIn(user) {
+    await I.wait('5');
     await I.waitForSelector(this.fields.username);
     I.fillField(this.fields.username, user.email);
     I.fillField(this.fields.password, user.password);
