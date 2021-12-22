073acc12faaf657b0e3f0b6b516e3a394f16554a
abidfs <abid.sayyad@hmcts.net>
2021-12-22 12:16:50 +0000

    added placement order summary page

--- src/main/steps/children/placement-order-summary/content.ts
+++ src/main/steps/children/placement-order-summary/content.ts
@@ -3,33 +3,71 @@ import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 import { isFieldFilledIn } from '../../../app/form/validation';
 
-const en = () => ({
-  section: "The child's details",
-  title: 'Orders already in place',
-  label: 'Do you want to add another order?',
-  hint: 'We need details of all orders already in place. Your social worker or adoption agency can help provide these details.',
-  errors: {
-    addAnotherOrder: {
-      required: 'Please select an answer',
-    },
-  },
-});
+const placementOrderListItems = (userCase, content) => {
+  return userCase.placementOrders?.map(item => {
+    return {
+      key: {
+        text: item.placementOrderType || content.placementOrder,
+        classes: 'font-normal',
+      },
+      actions: {
+        items: [
+          {
+            href: '#',
+            text: content.change,
+            visuallyHiddenText: 'name',
+          },
+        ],
+      },
+    };
+  });
+};
 
-const cy = () => ({
-  section: "The child's details (in welsh)",
-  title: 'Orders already in place (in welsh)',
-  label: 'Do you want to add another order? (in welsh)',
-  hint: 'We need details of all orders already in place. Your social worker or adoption agency can help provide these details. (in welsh)',
-  errors: {
-    addAnotherOrder: {
-      required: 'Please select an answer (in welsh)',
+const en = content => {
+  const enContent = {
+    section: "The child's details",
+    title: 'Orders already in place',
+    placementOrder: 'Placement Order',
+    change: 'Change',
+    remove: 'Remove',
+    label: 'Do you want to add another order?',
+    hint: 'We need details of all orders already in place. Your social worker or adoption agency can help provide these details.',
+    errors: {
+      addAnotherOrder: {
+        required: 'Please select an answer',
+      },
     },
-  },
-});
+  };
+  return {
+    ...enContent,
+    placementOrderListItems: placementOrderListItems(content.userCase, enContent),
+  };
+};
+
+const cy = content => {
+  const cyContent = {
+    section: "The child's details (in welsh)",
+    title: 'Orders already in place (in welsh)',
+    placementOrder: 'Placement Order (in welsh)',
+    change: 'Change (in welsh)',
+    remove: 'Remove (in welsh)',
+    label: 'Do you want to add another order? (in welsh)',
+    hint: 'We need details of all orders already in place. Your social worker or adoption agency can help provide these details. (in welsh)',
+    errors: {
+      addAnotherOrder: {
+        required: 'Please select an answer (in welsh)',
+      },
+    },
+  };
+  return {
+    ...cyContent,
+    placementOrderListItems: placementOrderListItems(content.userCase, cyContent),
+  };
+};
 
 export const form: FormContent = {
   fields: {
-    addAnotherOrder: {
+    addAnotherPlacementOrder: {
       type: 'radios',
       classes: 'govuk-radios govuk-radios--inline',
       label: l => l.label,
@@ -57,7 +95,7 @@ const languages = {
 };
 
 export const generateContent: TranslationFn = content => {
-  const translations = languages[content.language]();
+  const translations = languages[content.language](content);
   return {
     ...translations,
     form,
