d91f147108d0f487ef3d65fde4317977aec01f3b
abidfs <abid.sayyad@hmcts.net>
2022-01-06 21:49:15 +0000

    typo fix

--- src/main/steps/applicant2/address/lookup/content.test.ts
+++ src/main/steps/applicant2/address/lookup/content.test.ts
@@ -31,7 +31,7 @@ describe('applicant2 > address > lookup > content', () => {
     expect(generatedContent.section).toEqual(enContent.section);
     expect(generatedContent.title).toEqual(enContent.title);
     expect(generatedContent.errors).toEqual({
-      applicant1AddressPostcode: (addressLookupContent.errors as any).addressPostcode,
+      applicant2AddressPostcode: (addressLookupContent.errors as any).addressPostcode,
     });
     expect(generatedContent.manualAddressUrl).toEqual('/applicant2/address/manual');
   });
@@ -42,7 +42,7 @@ describe('applicant2 > address > lookup > content', () => {
     expect(generatedContent.section).toEqual(cyContent.section);
     expect(generatedContent.title).toEqual(cyContent.title);
     expect(generatedContent.errors).toEqual({
-      applicant1AddressPostcode: (addressLookupContent.errors as any).addressPostcode,
+      applicant2AddressPostcode: (addressLookupContent.errors as any).addressPostcode,
     });
     expect(generatedContent.manualAddressUrl).toEqual('/applicant2/address/manual');
   });
