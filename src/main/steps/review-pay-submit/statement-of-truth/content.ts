c5272c86e861d941b5d14332703cc438aeded1a1
abidfs <abid.sayyad@hmcts.net>
2022-01-26 19:47:34 +0000

    fixed ui styles

--- src/main/steps/review-pay-submit/statement-of-truth/content.ts
+++ src/main/steps/review-pay-submit/statement-of-truth/content.ts
@@ -7,13 +7,18 @@ import { isFieldFilledIn } from '../../../app/form/validation';
 export const en = (): Record<string, unknown> => ({
   section: 'Review your application, pay and send',
   title: 'Statement of truth',
-  line1: 'I understand that proceedings for contempt of court may be brought against anyone who makes, or causes to be made, a false statement in a document verified by a statement of truth without an honest belief in the truth.',
-  line2: 'Once you submit your application, you cannot make any further changes. You can select \'Save as draft\' to review your application before you submit.',
-  applicant1IBelieveApplicationIsTrue: 'I believe that the facts stated in this form and any additional documents are true.',
-  applicant1IBelieveApplicationIsTrue2: 'The primary applicant believes that the facts stated in this form and any additional documents are true.',
+  statement:
+    'I understand that proceedings for contempt of court may be brought against anyone who makes, or causes to be made, a false statement in a document verified by a statement of truth without an honest belief in the truth.',
+  reviewBeforeSubmit:
+    "Once you submit your application, you cannot make any further changes. You can select 'Save as draft' to review your application before you submit.",
+  applicant1IBelieveApplicationIsTrue:
+    'I believe that the facts stated in this form and any additional documents are true.',
+  applicant1IBelieveApplicationIsTrue2:
+    'The primary applicant believes that the facts stated in this form and any additional documents are true.',
   applicant2IBelieveApplicationIsTrue: 'I am authorised by the second applicant to sign this statement.',
   applicant1SotFullName: 'Enter your full name',
-  applicant2SotFullName: 'Enter the second applicant\'s full name (if applicable)',
+  applicant2SotFullName: "Enter the second applicant's full name (if applicable)",
+  confirm: 'Confirm',
   errors: {
     applicant1IBelieveApplicationIsTrue: {
       required: 'Confirm your statement of truth',
@@ -30,13 +35,18 @@ export const en = (): Record<string, unknown> => ({
 export const cy = (): Record<string, unknown> => ({
   section: 'Review your application, pay and send (in Welsh)',
   title: 'Statement of truth (in Welsh)',
-  line1: 'I understand that proceedings for contempt of court may be brought against anyone who makes, or causes to be made, a false statement in a document verified by a statement of truth without an honest belief in the truth. (in Welsh)',
-  line2: 'Once you submit your application, you cannot make any further changes. You can select \'Save as draft\' to review your application before you submit. (in Welsh)',
-  applicant1IBelieveApplicationIsTrue: 'I believe that the facts stated in this form and any additional documents are true. (in Welsh)',
-  applicant1IBelieveApplicationIsTru2: 'The primary applicant believes that the facts stated in this form and any additional documents are true. (in Welsh)',
+  statement:
+    'I understand that proceedings for contempt of court may be brought against anyone who makes, or causes to be made, a false statement in a document verified by a statement of truth without an honest belief in the truth. (in Welsh)',
+  reviewBeforeSubmit:
+    "Once you submit your application, you cannot make any further changes. You can select 'Save as draft' to review your application before you submit. (in Welsh)",
+  applicant1IBelieveApplicationIsTrue:
+    'I believe that the facts stated in this form and any additional documents are true. (in Welsh)',
+  applicant1IBelieveApplicationIsTru2:
+    'The primary applicant believes that the facts stated in this form and any additional documents are true. (in Welsh)',
   applicant2IBelieveApplicationIsTrue: 'I am authorised by the second applicant to sign this statement. (in Welsh)',
   applicant1SotFullName: 'Enter your full name (in Welsh)',
-  applicant2SotFullName: 'Enter the second applicant\'s full name (if applicable) (in Welsh)',
+  applicant2SotFullName: "Enter the second applicant's full name (if applicable) (in Welsh)",
+  confirm: 'Confirm (in Welsh)',
   errors: {
     applicant1IBelieveApplicationIsTrue: {
       required: 'Confirm your statement of truth (in Welsh)',
@@ -53,62 +63,77 @@ export const cy = (): Record<string, unknown> => ({
 export const form: FormContent = {
   fields: userCase => {
     return {
-      ...(userCase.applyingWith == ApplyingWith.ALONE
+      ...(userCase.applyingWith === ApplyingWith.ALONE
         ? {
-          applicant1IBelieveApplicationIsTrue: {
-            type: 'checkboxes',
-            label: l => l.label,
-            labelSize: 'l',
-            section: l => l.section,
-            hint: l => l.hint,
-            values: [{ name: 'applicant1IBelieveApplicationIsTrue', label: l => l.applicant1IBelieveApplicationIsTrue, value: Checkbox.Checked },],
-            validator: isFieldFilledIn,
-          },
-          applicant1SotFullName: {
-            type: 'text',
-            classes: 'govuk-label govuk-!-width-two-thirds',
-            label: l => l.applicant1SotFullName,
-            labelSize: null,
-            validator: isFieldFilledIn,
-          },
-        }
+            applicant1IBelieveApplicationIsTrue: {
+              type: 'checkboxes',
+              label: l => l.label,
+              labelSize: 'l',
+              section: l => l.section,
+              values: [
+                {
+                  name: 'applicant1IBelieveApplicationIsTrue',
+                  label: l => l.applicant1IBelieveApplicationIsTrue,
+                  value: Checkbox.Checked,
+                },
+              ],
+              validator: isFieldFilledIn,
+            },
+            applicant1SotFullName: {
+              type: 'text',
+              classes: 'govuk-label govuk-!-width-two-thirds',
+              label: l => l.applicant1SotFullName,
+              labelSize: null,
+              validator: isFieldFilledIn,
+            },
+          }
         : {
-          applicant1IBelieveApplicationIsTrue: {
-            type: 'checkboxes',
-            label: l => l.label,
-            labelSize: 'l',
-            section: l => l.section,
-            hint: l => l.hint,
-            values: [{ name: 'applicant1IBelieveApplicationIsTrue', label: l => l.applicant1IBelieveApplicationIsTrue2, value: Checkbox.Checked },],
-            validator: isFieldFilledIn,
-          },
-          applicant2IBelieveApplicationIsTrue: {
-            type: 'checkboxes',
-            label: l => l.label,
-            labelSize: 'l',
-            section: l => l.section,
-            hint: l => l.hint,
-            values: [{ name: 'applicant2IBelieveApplicationIsTrue', label: l => l.applicant2IBelieveApplicationIsTrue, value: Checkbox.Checked },],
-          },
-          applicant1SotFullName: {
-            type: 'text',
-            classes: 'govuk-label govuk-!-width-two-thirds',
-            label: l => l.applicant1SotFullName,
-            labelSize: null,
-            validator: isFieldFilledIn,
-          },
-          applicant2SotFullName: {
-            type: 'text',
-            classes: 'govuk-label govuk-!-width-two-thirds',
-            label: l => l.applicant2SotFullName,
-            labelSize: null,
-            validator: isFieldFilledIn,
-          },
-        }),
+            applicant1IBelieveApplicationIsTrue: {
+              type: 'checkboxes',
+              label: l => l.label,
+              labelSize: 'l',
+              section: l => l.section,
+              values: [
+                {
+                  name: 'applicant1IBelieveApplicationIsTrue',
+                  label: l => l.applicant1IBelieveApplicationIsTrue2,
+                  value: Checkbox.Checked,
+                },
+              ],
+              validator: isFieldFilledIn,
+            },
+            applicant2IBelieveApplicationIsTrue: {
+              type: 'checkboxes',
+              label: l => l.label,
+              labelSize: 'l',
+              section: l => l.section,
+              values: [
+                {
+                  name: 'applicant2IBelieveApplicationIsTrue',
+                  label: l => l.applicant2IBelieveApplicationIsTrue,
+                  value: Checkbox.Checked,
+                },
+              ],
+            },
+            applicant1SotFullName: {
+              type: 'text',
+              classes: 'govuk-label govuk-!-width-two-thirds',
+              label: l => l.applicant1SotFullName,
+              labelSize: null,
+              validator: isFieldFilledIn,
+            },
+            applicant2SotFullName: {
+              type: 'text',
+              classes: 'govuk-label govuk-!-width-two-thirds',
+              label: l => l.applicant2SotFullName,
+              labelSize: null,
+              validator: isFieldFilledIn,
+            },
+          }),
     };
   },
   submit: {
-    text: l => l.continue,
+    text: l => l.confirm,
   },
   saveAsDraft: {
     text: l => l.saveAsDraft,
@@ -127,4 +152,3 @@ export const generateContent: TranslationFn = content => {
     form: { ...form, fields: (form.fields as FormFieldsFn)(content.userCase || {}) },
   };
 };
-
