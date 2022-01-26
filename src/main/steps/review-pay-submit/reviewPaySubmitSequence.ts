9c96d68c9d68766a3483426a6f76eea0d9d3dd9a
Pankaj Kumar <pankaj.kumar@hmcts.net>
2022-01-26 13:48:38 +0000

    ADOP-11 change

--- src/main/steps/review-pay-submit/reviewPaySubmitSequence.ts
+++ src/main/steps/review-pay-submit/reviewPaySubmitSequence.ts
@@ -3,6 +3,11 @@ import * as Urls from '../urls';
 
 export const reviewPaySubmitSequence: Step[] = [
   {
+    url: Urls.STATEMENT_OF_TRUTH,
+    showInSection: Sections.ReviewPaySubmit,
+    getNextStep: () => Urls.CHECK_ANSWERS_URL,
+  },
+  {
     url: Urls.CHECK_ANSWERS_URL,
     showInSection: Sections.ReviewPaySubmit,
     getNextStep: () => Urls.PAY_YOUR_FEE,
