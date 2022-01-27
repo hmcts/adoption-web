99f449365d047dbaa274fdd3f35d57a83a5e4570
abidfs <abid.sayyad@hmcts.net>
2022-01-27 09:54:29 +0000

    deleted old fee lookup page

--- src/main/steps/applicant1/applicant1Sequence.ts
+++ src/main/steps/applicant1/applicant1Sequence.ts
@@ -12,7 +12,6 @@ import {
   APPLICANT_1_SELECT_ADDRESS,
   APPLYING_WITH_URL,
   DATE_CHILD_MOVED_IN_URL,
-  FEE_LOOKUP_URL,
   TASK_LIST_URL,
 } from '../urls';
 
@@ -76,9 +75,5 @@ export const applicant1Sequence: Step[] = [
     url: APPLICANT_1_MANUAL_ADDRESS,
     showInSection: Sections.AboutApplicant1,
     getNextStep: () => APPLICANT_1_CONTACT_DETAILS,
-  },
-  {
-    url: FEE_LOOKUP_URL,
-    getNextStep: () => TASK_LIST_URL,
   },
 ];
