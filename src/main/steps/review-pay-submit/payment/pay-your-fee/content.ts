77bc4e1b9639af4130b0c3a8c6cbfbcb573fd314
abidfs <abid.sayyad@hmcts.net>
2022-01-28 09:21:43 +0000

    updated payment screens as per ucd

--- src/main/steps/review-pay-submit/payment/pay-your-fee/content.ts
+++ src/main/steps/review-pay-submit/payment/pay-your-fee/content.ts
@@ -5,10 +5,13 @@ import { isFieldFilledIn } from '../../../../app/form/validation';
 // TODO: Update this page
 const en = content => ({
   section: 'Review your application, pay and send',
-  label: 'Pay your adoption application fee',
+  label: 'Paying your adoption court fees',
   hint: `The adoption court fees total <span class="govuk-!-font-weight-bold">£${content.fee?.feeAmount || 0}</span>.
   <br>If you have little or no savings, receive certain benefits or have a low income you may be able to get help with your adoption application fees.`,
   payingByCard: 'I am paying by card',
+  haveHWFRef: 'I have a help with fees reference number',
+  hwfRefNumber: 'Enter your help with fees reference number',
+  applyForHWF: 'I want to apply for help with fees',
   continue: 'Continue',
   errors: {
     paymentType: { errorRetrievingFee: 'Error in retrieving fee', required: 'Please select an answer' },
@@ -17,12 +20,15 @@ const en = content => ({
 
 const cy: typeof en = content => ({
   section: 'Review your application, pay and send (in welsh)',
-  label: 'Pay your adoption application fee (in welsh)',
+  label: 'Paying your adoption court fees (in welsh)',
   hint: `The adoption court fees total <span class="govuk-!-font-weight-bold">£${
     content.fee?.feeAmount || 0
   }</span> (in welsh).
   <br>If you have little or no savings, receive certain benefits or have a low income you may be able to get help with your adoption application fees. (in welsh)`,
   payingByCard: 'I am paying by card (in welsh)',
+  haveHWFRef: 'I have a help with fees reference number (in welsh)',
+  hwfRefNumber: 'Enter your help with fees reference number (in welsh)',
+  applyForHWF: 'I want to apply for help with fees (in welsh)',
   continue: 'Continue (in welsh)',
   errors: {
     paymentType: { errorRetrievingFee: 'Error in retrieving fee', required: 'Please select an answer' },
@@ -37,7 +43,22 @@ export const form: FormContent = {
       label: l => l.label,
       section: l => l.section,
       hint: l => l.hint,
-      values: [{ label: l => l.payingByCard, value: 'payingByCard' }],
+      values: [
+        { label: l => l.payingByCard, value: 'payingByCard' },
+        {
+          label: l => l.haveHWFRef,
+          value: 'haveHWFRef',
+          subFields: {
+            hwfRefNumber: {
+              type: 'text',
+              label: l => l.hwfRefNumber,
+              labelSize: null,
+              validator: isFieldFilledIn,
+            },
+          },
+        },
+        { label: l => l.applyForHWF, value: 'applyForHWF' },
+      ],
       validator: isFieldFilledIn,
     },
   },
