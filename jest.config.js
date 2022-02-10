23565a39261736d2be750a22a071673d690578b6
dilipsamrahmcts <dilip.samra@hmcts.net>
2022-02-10 17:03:42 +0000

    removed unit tests minimum coverage temporarily

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,10 @@ module.exports = {
   },
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 20,
+      functions: 20,
+      lines: 20,
+      statements: 20,
     },
   },
 };
