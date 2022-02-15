b29f36c7bd04a22a4bc5877fac75749552bb03f2
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-02-15 15:12:20 +0000

    Added status correction

--- src/main/steps/task-list/utils.ts
+++ src/main/steps/task-list/utils.ts
@@ -345,17 +345,10 @@ export const getReviewPaySubmitUrl = (userCase: CaseWithId): string => {
   }
   return urls.EQUALITY;
 };
-export const getDateChildMovedInStatus = (userCase: CaseWithId): SectionStatus => {
-  // const dateChildMovedIn = new Date(toApiDate(userCase?.dateChildMovedIn));
-  const dateChildMovedIn = userCase?.dateChildMovedIn as CaseDate;
-  if (dateChildMovedIn === undefined) {
-    return SectionStatus.NOT_STARTED;
-  }
-  const dateChildMovedInComplete = dateChildMovedIn?.day && dateChildMovedIn?.month && dateChildMovedIn?.year;
 
-  return dateChildMovedInComplete
-    ? SectionStatus.COMPLETED
-    : !dateChildMovedInComplete
-    ? SectionStatus.NOT_STARTED
-    : SectionStatus.IN_PROGRESS;
+export const getDateChildMovedInStatus = (userCase: CaseWithId): SectionStatus => {
+  const dateChildMovedIn = userCase.dateChildMovedIn as CaseDate;
+  const dateChildMovedInComplete = !!(dateChildMovedIn?.day && dateChildMovedIn.month && dateChildMovedIn.year);
+
+  return dateChildMovedInComplete ? SectionStatus.COMPLETED : SectionStatus.NOT_STARTED;
 };
