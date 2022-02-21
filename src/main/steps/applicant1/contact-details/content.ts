fd2e6363f4ed5ae287790f6b67b22dbad8b01945
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-02-21 16:21:56 +0000

    Modified test Cases

--- src/main/steps/applicant1/contact-details/content.ts
+++ src/main/steps/applicant1/contact-details/content.ts
@@ -9,7 +9,7 @@ const en = content => ({
   line1: 'We need both a contact email and telephone number for you.',
   line2:
     'We will email you updates and information about your application to adopt. You will only be contacted by telephone if the social worker or court staff need to contact you quickly.',
-  emailAddress: 'Email Address',
+  emailAddress: 'Email address',
   phoneNumber: 'UK Phone number',
   applicant1ContactDetailsConsent:
     'The court may want to use your email to serve you court orders. Are you happy to be served court orders by email?',
@@ -23,21 +23,21 @@ const en = content => ({
     },
     applicant1PhoneNumber: {
       required: 'Enter your telephone number',
-      invalid: 'Enter a UK telephone number',
+      invalid: 'Enter a valid UK telephone number',
     },
   },
 });
 
 const cy = content => ({
-  section: content.userCase.applyingWith === ApplyingWith.ALONE ? 'Applicant' : 'First applicant',
+  section: content?.userCase?.applyingWith === ApplyingWith.ALONE ? 'Applicant' : 'First applicant (in welsh)',
   title: 'What are your contact details? (in welsh)',
-  line1: 'This is so we can contact you with updates or questions about your application. (in welsh)',
+  line1: 'We need both a contact email and telephone number for you. (in welsh)',
   line2:
-    'We will email you updates and information about your application to adopt. You will only be contacted by telephone if the social worker or court staff need to contact you quickly.',
+    'We will email you updates and information about your application to adopt. You will only be contacted by telephone if the social worker or court staff need to contact you quickly. (in welsh)',
   emailAddress: 'Email address (in welsh)',
-  phoneNumber: 'UK phone number (for phone calls) (in welsh)',
+  phoneNumber: 'UK Phone number (in welsh)',
   applicant1ContactDetailsConsent:
-    'The court may want to use your email to serve you court orders. Are you happy to be served court orders by email?',
+    'The court may want to use your email to serve you court orders. Are you happy to be served court orders by email? (in welsh)',
   errors: {
     applicant1ContactDetailsConsent: {
       required: 'Please answer the question (in welsh)',
@@ -47,8 +47,8 @@ const cy = content => ({
       invalid: 'Enter an email address in the correct format, like name@example.com (in welsh)',
     },
     applicant1PhoneNumber: {
-      required: 'Enter a UK telephone number (in welsh)',
-      invalid: 'Enter a UK telephone number (in welsh)',
+      required: 'Enter your telephone number (in welsh)',
+      invalid: 'Enter a valid UK telephone number (in welsh)',
     },
   },
 });
@@ -74,7 +74,7 @@ export const form: FormContent = {
       classes: 'govuk-radios',
       label: l => l.label,
       section: l => l.section,
-      hint: l => l.applicant1Consent,
+      hint: l => l.applicant1ContactDetailsConsent,
       values: [
         { label: l => l.yes, value: YesOrNo.YES },
         { label: l => l.no, value: YesOrNo.NO },
