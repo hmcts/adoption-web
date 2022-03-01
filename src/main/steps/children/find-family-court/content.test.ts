27aac2302d8d23d0d3500cb27617a9f4b0a9aae5
Pankaj Kumar <pankaj.kumar@hmcts.net>
2022-03-01 18:44:34 +0000

    added test case

--- src/main/steps/children/find-family-court/content.test.ts
+++ src/main/steps/children/find-family-court/content.test.ts
@@ -1,6 +1,6 @@
-// import { YesOrNo } from '../../../app/case/definition';
-import { FormContent } from '../../../app/form/Form'; //, FormFields, FormInput, FormOptions
-// import { isFieldFilledIn, isTextAreaValid } from '../../../app/form/validation';
+import { YesOrNo } from '../../../app/case/definition';
+import { FormContent, FormFields, FormInput, FormOptions } from '../../../app/form/Form';
+import { isFieldFilledIn } from '../../../app/form/validation';
 import { CommonContent, generatePageContent } from '../../common/common.content';
 
 import { generateContent } from './content';
@@ -89,12 +89,14 @@ describe('find-family-court content', () => {
     generatedContent = generateContent({
       ...commonContent,
       language: 'cy',
-      userCase: { placementOrders: [{ placementOrderId: '123', placementOrderCourt: 'London Court' }] },
+      userCase: { placementOrders: [{ placementOrderId: '123', placementOrderCourt: 'oxford family court' }] },
     });
     expect(generatedContent.section).toEqual(cyContent.section);
     expect(generatedContent.title).toEqual(cyContent.title);
     expect(generatedContent.findFamilyCourtParagraph1).toEqual(cyContent.findFamilyCourtParagraph1);
-    expect(generatedContent.findFamilyCourtParagraph2).toEqual(cyContent.findFamilyCourtParagraph2);
+    expect(generatedContent.findFamilyCourtParagraph2).toEqual(
+      'You have told us that the court which issued the placement order was <b>oxford family court</b>. (in welsh)'
+    );
     expect(generatedContent.findFamilyCourt).toEqual(cyContent.findFamilyCourt);
     expect(generatedContent.findFamilyCourtHint).toEqual(cyContent.findFamilyCourtHint);
     expect(generatedContent.familyCourtNameParagraph1).toEqual(cyContent.familyCourtNameParagraph1);
@@ -104,32 +106,37 @@ describe('find-family-court content', () => {
     expect(generatedContent.errors).toEqual(cyContent.errors);
   });
 
-  // test('should contain birthFatherAddressKnown field', () => {
-  //   const fields = (generatedContent.form as FormContent).fields as FormFields;
-  //   const field = fields.birthFatherAddressKnown as FormOptions;
-  //   expect(field.type).toBe('radios');
-  //   expect(field.classes).toBe('govuk-radios');
-  //   expect((field.label as Function)(generatedContent)).toBe(enContent.label);
-  //   expect(((field as FormInput).hint as Function)(generatedContent)).toBe(enContent.hint);
-  //   expect((field.section as Function)(generatedContent)).toBe(enContent.section);
-  //   expect((field.values[0].label as Function)(commonContent)).toBe(commonContent.yes);
-  //   expect(field.values[0].value).toBe(YesOrNo.YES);
-  //   expect((field.values[1].label as Function)(commonContent)).toBe(commonContent.no);
-  //   expect(field.values[1].value).toBe(YesOrNo.NO);
-  //   expect(field.validator).toBe(isFieldFilledIn);
+  test('should contain findFamilyCourt field', () => {
+    const fields = (generatedContent.form as FormContent).fields as FormFields;
+    const field = fields.findFamilyCourt as FormOptions;
+    expect(field.type).toBe('radios');
+    expect(field.classes).toBe('govuk-radios');
+    expect((field.label as Function)(generatedContent)).toBe(enContent.findFamilyCourt);
+    expect(((field as FormInput).hint as Function)(generatedContent)).toBe(enContent.findFamilyCourtHint);
+    expect((field.section as Function)(generatedContent)).toBe(enContent.section);
+    expect((field.values[0].label as Function)(commonContent)).toBe(commonContent.yes);
+    expect(field.values[0].value).toBe(YesOrNo.YES);
+    expect((field.values[1].label as Function)(commonContent)).toBe(commonContent.no);
+    expect(field.values[1].value).toBe(YesOrNo.NO);
+    expect(field.validator).toBe(isFieldFilledIn);
 
-  //   const field2 = (fields.birthFatherAddressKnown as FormOptions).values[1].subFields!
-  //     .birthFatherAddressNotKnownReason;
-  //   expect((field2?.label as Function)(generatedContent)).toBe(enContent.moreDetails);
-  //   expect(field2.type).toBe('textarea');
-  //   expect(field2?.labelSize).toBe(null);
+    const field2 = field.values[1].subFields!.familyCourtName;
+    expect((field2?.label as Function)(generatedContent)).toBe(enContent.familyCourtName);
+    expect(field2.type).toBe('text');
+    expect(field2?.labelSize).toBe(null);
 
-  //   (field2.validator as Function)('MockTextArea');
-  //   expect(isFieldFilledIn).toHaveBeenCalledWith('MockTextArea');
-  //   expect(isTextAreaValid).toHaveBeenCalledWith('MockTextArea');
+    const field3 = field.values[1].subFields!.p1;
+    expect((field3?.label as Function)(generatedContent)).toBe(enContent.familyCourtNameParagraph1);
+    expect(field3.type).toBe('label');
 
-  //   expect((field2.attributes as HTMLTextAreaElement).rows).toBe(1);
-  // });
+    const field4 = field.values[1].subFields!.p2;
+    expect((field4?.label as Function)(generatedContent)).toBe(enContent.familyCourtNameParagraph2);
+    expect(field4.type).toBe('label');
+
+    const field5 = field.values[1].subFields!.p3;
+    expect((field5?.label as Function)(generatedContent)).toBe(enContent.familyCourtNameParagraph3);
+    expect(field5.type).toBe('label');
+  });
 
   test('should contain submit button', () => {
     expect(((generatedContent.form as FormContent).submit.text as Function)(commonContent)).toBe(
