b0fd14e0cd3226b1a4271b36ada0a9281450c05a
abidfs <abid.sayyad@hmcts.net>
2022-03-02 10:53:09 +0000

    added pa11y tests in place of functional tests in pipeline

---
+++ src/test/a11y/a11y.ts
@@ -0,0 +1,113 @@
+import fs from 'fs';
+
+import Axios from 'axios';
+import puppeteer from 'puppeteer';
+
+import * as urls from '../../main/steps/urls';
+import { config } from '../config';
+
+const pa11y = require('pa11y');
+const axios = Axios.create({ baseURL: config.TEST_URL });
+
+interface Pa11yResult {
+  documentTitle: string;
+  pageUrl: string;
+  issues: PallyIssue[];
+}
+
+interface PallyIssue {
+  code: string;
+  context: string;
+  message: string;
+  selector: string;
+  type: string;
+  typeCode: number;
+}
+
+function ensurePageCallWillSucceed(url: string): Promise<void> {
+  return axios.get(url);
+}
+
+function runPally(url: string, browser): Promise<Pa11yResult> {
+  let screenCapture: string | boolean = false;
+  if (!config.TestHeadlessBrowser) {
+    const screenshotDir = `${__dirname}/../../../functional-output/pa11y`;
+    fs.mkdirSync(screenshotDir, { recursive: true });
+    screenCapture = `${screenshotDir}/${url.replace(/^\/$/, 'home').replace('/', '')}.png`;
+  }
+
+  const fullUrl = `${config.TEST_URL}${url}`;
+  return pa11y(fullUrl, {
+    browser,
+    screenCapture,
+    hideElements: '.govuk-footer__licence-logo, .govuk-header__logotype-crown',
+  });
+}
+
+function expectNoErrors(messages: PallyIssue[]): void {
+  const errors = messages.filter(m => m.type === 'error');
+
+  if (errors.length > 0) {
+    const errorsAsJson = `${JSON.stringify(errors, null, 2)}`;
+    throw new Error(`There are accessibility issues: \n${errorsAsJson}\n`);
+  }
+}
+
+jest.retryTimes(3);
+jest.setTimeout(15000);
+
+describe('Accessibility', () => {
+  let browser;
+  let cookies;
+  let hasAfterAllRun = false;
+
+  const setup = async () => {
+    if (hasAfterAllRun) {
+      return;
+    }
+    if (browser) {
+      await browser.close();
+    }
+
+    browser = await puppeteer.launch({ ignoreHTTPSErrors: true });
+    browser.on('disconnected', setup);
+
+    // Login once only for other pages to reuse session
+    const page = await browser.newPage();
+    console.log(config.TEST_URL);
+    await page.goto(config.TEST_URL);
+    await page.type('#username', 'abid.sayyad@hmcts.net');
+    await page.type('#password', 'Pa55word12');
+    await page.click('input[type="submit"]');
+    cookies = await page.cookies(config.TEST_URL);
+    console.log(cookies);
+    await page.close();
+  };
+
+  beforeAll(setup);
+
+  beforeEach(async () => {
+    const page = await browser.newPage();
+    await page.goto(config.TEST_URL);
+    await page.setCookie(...cookies);
+    await page.goto(`${config.TEST_URL}/info`);
+    await page.close();
+  });
+
+  afterAll(async () => {
+    hasAfterAllRun = true;
+    await browser.close();
+  });
+
+  const IGNORED_URLS = [urls.SIGN_IN_URL, urls.SIGN_OUT_URL];
+  const urlsNoSignOut = Object.values(urls).filter(url => !IGNORED_URLS.includes(url));
+  // const urlsNoSignOut = [urls.RELATIONSHIP_NOT_BROKEN_URL]
+  test.each(urlsNoSignOut)('Page %s should have no accessibility errors', async url => {
+    await ensurePageCallWillSucceed(url);
+    console.log('url ', url);
+    const result = await runPally(url, browser);
+    console.log('result ', result);
+    expect(result.issues).toEqual(expect.any(Array));
+    expectNoErrors(result.issues);
+  });
+});
