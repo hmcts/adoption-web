523fee0136600dac3a91f5df53b9eaf3cbd133f9
dilipsamrahmcts <dilip.samra@hmcts.net>
2022-03-11 10:36:58 +0000

    implemented error handling pages

--- src/main/steps/common/common.content.ts
+++ src/main/steps/common/common.content.ts
@@ -22,7 +22,7 @@ const en = {
   warning: 'Warning',
   required: 'You have not answered the question. You need to select an answer before continuing.',
   notAnswered: 'You have not answered the question.',
-  errorSaving: 'Sorry, we’re having technical problems saving your application. Please try again in a few minutes.',
+  errorSaving: 'Sorry, we are unable to save your application right now. Please try again later.',
   errorSendingInvite:
     'Sorry, we’re having technical problems sending your application for review. Please try again in a few minutes.',
   ogl: 'All content is available under the <a class="govuk-link" href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" rel="license">Open Government Licence v3.0</a>, except where otherwise stated',
