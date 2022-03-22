d6e71c64e0a0b3c2ca2865757e203a5c670c47b7
Raghu Nakkala <nraghu31@gmail.com>
2022-03-22 10:02:58 +0000

    New changes

--- src/test/e2e/tests/submitAdoptionApplication_test.js
+++ src/test/e2e/tests/submitAdoptionApplication_test.js
@@ -31,68 +31,68 @@ Scenario(
     await dateChildMovedinDetailsPage.dateChildMovedInSection('10', '10', '2020');
     await taskListPage.verifyDateChildMovedInStatus();
 
-    // await taskListPage.selectChildAdoptionAgencyDetails();
-    // await childAdoptionAgencyDetailsPage.childAdoptionAgencyDetailsSection();
-    // await childAdoptionAgencyDetailsPage.childSocialWorkerDetails();
-    // await taskListPage.verifyChildAdoptionAgencyDetailsStatus;
-    //
-    // await taskListPage.selectPrimaryApplicantContactDetails();
-    // await primaryApplicantDetailsPage.primaryApplicantContactDetailsSection();
-    // await taskListPage.verifyPrimaryApplicantContactDetailsStatus();
-    //
-    // await taskListPage.selectFirstApplicantPersonalDetails();
-    // await primaryApplicantPersonalDetailsPage.primaryApplicantPersonalDetailsSection();
-    // await taskListPage.verifyPrimaryApplicantPersonalDetailsStatus();
-    //
-    // await taskListPage.selectOtherApplicantContactDetails();
-    // await otherApplicantDetailsPage.otherApplicantContactDetailsSection();
-    // await taskListPage.verifySecondApplicantPersonalDetailsStatus();
-    //
-    // await taskListPage.selectOtherApplicantPersonalDetails();
-    // await otherApplicantPersonalDetailsPage.otherApplicantPersonalDetailsSection();
-    // await taskListPage.verifyOtherApplicantPersonalDetailsStatus();
-    //
-    // await taskListPage.selectChildrenBirthCertificate();
-    // await childrenBirthCertificatePage.childDetailsBirthCertificaterSection();
-    // await taskListPage.verifyChildBirthCertificateDetailsStatus();
-    //
-    // await taskListPage.selectChildAdoptionCertificateDetails();
-    // await childAdoptionCertificateDetailsPage.childNameAfterAdoptionDetailsSection();
-    // await taskListPage.verifyAdoptionCertificateDetailStatus();
-    //
-    // await taskListPage.selectChildBirthMotherDetails();
-    // await childBirthMothersDetailsPage.childBirthMothersDetailsSection();
-    // await taskListPage.verifyChildBirthMotherDetailsStatus();
-    //
-    // await taskListPage.selectChildBirthFatherDetails();
-    // await childBirthFatherDetailsPage.childBirthFatherDetailsSection();
-    // await taskListPage.verifyChildBirthFatherDetailsStatus();
-    //
-    // await taskListPage.selectChildOtherParentDetails();
-    // await childOtherParentDetailsPage.childOtherParentDetailsSection();
-    // await taskListPage.verifyChildOtherParentDetailsStatus();
-    //
-    // await taskListPage.selectChildPlacementOrderDetails();
-    // await childDetailsPlacementOrderPage.childDetailsPlacementOrderSection();
-    //
-    // await taskListPage.selectSiblingDetails();
-    // await childSiblingDetailsPage.childDetailsSiblingSection();
-    // await taskListPage.verifySiblingDetailsStatus();
-    //
-    // await taskListPage.selectChooseYourFamilyCourtDetails();
-    // await chooseYourFamilyCourtDetailsPage.childDetailsFindFamilyCourtSection();
-    // await taskListPage.verifyChooseYourFamilyCourtStatus();
-    //
-    // await taskListPage.selectUploadDocumentsDetails();
-    // await uploadDocumentsDetailsPage.uploadDocumentsSectionWithCantNotUploadOption();
-    // await taskListPage.verifyUploadDocumentsStatus();
-    //
-    // await taskListPage.selectReviewPayAndSubmitDetails();
-    // await reviewPayAndSubmitPage.selectNoPCQOption();
-    // await reviewPayAndSubmitPage.changeValueFromReviewYourAnswers();
-    // await dateChildMovedinDetailsPage.dateChildMovedInSection('10', '12', '2020');
-    // await reviewPayAndSubmitPage.reviewYourAnswersAndContinue();
-    // await reviewPayAndSubmitPage.statementOfTruthDetailsSection();
-    // await reviewPayAndSubmitPage.adoptionCourtFeesByCard();
+    await taskListPage.selectChildAdoptionAgencyDetails();
+    await childAdoptionAgencyDetailsPage.childAdoptionAgencyDetailsSection();
+    await childAdoptionAgencyDetailsPage.childSocialWorkerDetails();
+    await taskListPage.verifyChildAdoptionAgencyDetailsStatus;
+
+    await taskListPage.selectPrimaryApplicantContactDetails();
+    await primaryApplicantDetailsPage.primaryApplicantContactDetailsSection();
+    await taskListPage.verifyPrimaryApplicantContactDetailsStatus();
+
+    await taskListPage.selectFirstApplicantPersonalDetails();
+    await primaryApplicantPersonalDetailsPage.primaryApplicantPersonalDetailsSection();
+    await taskListPage.verifyPrimaryApplicantPersonalDetailsStatus();
+
+    await taskListPage.selectOtherApplicantContactDetails();
+    await otherApplicantDetailsPage.otherApplicantContactDetailsSection();
+    await taskListPage.verifySecondApplicantPersonalDetailsStatus();
+
+    await taskListPage.selectOtherApplicantPersonalDetails();
+    await otherApplicantPersonalDetailsPage.otherApplicantPersonalDetailsSection();
+    await taskListPage.verifyOtherApplicantPersonalDetailsStatus();
+
+    await taskListPage.selectChildrenBirthCertificate();
+    await childrenBirthCertificatePage.childDetailsBirthCertificaterSection();
+    await taskListPage.verifyChildBirthCertificateDetailsStatus();
+
+    await taskListPage.selectChildAdoptionCertificateDetails();
+    await childAdoptionCertificateDetailsPage.childNameAfterAdoptionDetailsSection();
+    await taskListPage.verifyAdoptionCertificateDetailStatus();
+
+    await taskListPage.selectChildBirthMotherDetails();
+    await childBirthMothersDetailsPage.childBirthMothersDetailsSection();
+    await taskListPage.verifyChildBirthMotherDetailsStatus();
+
+    await taskListPage.selectChildBirthFatherDetails();
+    await childBirthFatherDetailsPage.childBirthFatherDetailsSection();
+    await taskListPage.verifyChildBirthFatherDetailsStatus();
+
+    await taskListPage.selectChildOtherParentDetails();
+    await childOtherParentDetailsPage.childOtherParentDetailsSection();
+    await taskListPage.verifyChildOtherParentDetailsStatus();
+
+    await taskListPage.selectChildPlacementOrderDetails();
+    await childDetailsPlacementOrderPage.childDetailsPlacementOrderSection();
+
+    await taskListPage.selectSiblingDetails();
+    await childSiblingDetailsPage.childDetailsSiblingSection();
+    await taskListPage.verifySiblingDetailsStatus();
+
+    await taskListPage.selectChooseYourFamilyCourtDetails();
+    await chooseYourFamilyCourtDetailsPage.childDetailsFindFamilyCourtSection();
+    await taskListPage.verifyChooseYourFamilyCourtStatus();
+
+    await taskListPage.selectUploadDocumentsDetails();
+    await uploadDocumentsDetailsPage.uploadDocumentsSectionWithCantNotUploadOption();
+    await taskListPage.verifyUploadDocumentsStatus();
+
+    await taskListPage.selectReviewPayAndSubmitDetails();
+    await reviewPayAndSubmitPage.selectNoPCQOption();
+    await reviewPayAndSubmitPage.changeValueFromReviewYourAnswers();
+    await dateChildMovedinDetailsPage.dateChildMovedInSection('10', '12', '2020');
+    await reviewPayAndSubmitPage.reviewYourAnswersAndContinue();
+    await reviewPayAndSubmitPage.statementOfTruthDetailsSection();
+    await reviewPayAndSubmitPage.adoptionCourtFeesByCard();
   }
 );
