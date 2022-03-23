548b7101f50c12db4090f2a57c3fd4cfdec2ed7b
abidfs <abid.sayyad@hmcts.net>
2022-03-23 13:11:41 +0000

    added mutation test config

---
+++ jest.mutation.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src/main'],
+  testRegex: '(/src/main/*|\\.(test|spec))\\.(ts|js)$',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
