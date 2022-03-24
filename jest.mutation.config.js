5cfe6d9dc0c62c4ddb3be5cc09a6e3d1aedc8e82
abidfs <abid.sayyad@hmcts.net>
2022-03-24 08:49:42 +0000

    removed mutation related stuff

--- jest.mutation.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/src/main'],
-  testRegex: '(/src/main/*|\\.(test|spec))\\.(ts|js)$',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
