c8f5166acf4b7690405f2ea7d6cdd6c9ddc5ed03
pankajhmcts <94961980+pankajhmcts@users.noreply.github.com>
2022-03-28 11:01:41 +0100

    Adop 710 zap (#323)
    * added   enableSecurityScan()
    
    * added test url
    
    * ignored issues.
    
    * updated custom-env file
    
    * Updated test url
    
    * Updated files
    
    * updated package.json
    
    * Updated zap files
    
    * update jenkinsfile
    
    * removed jenkins changes
    
    * updated audit.json
    
    * added zap folder
    
    * given permission to zap folder
    
    * Updated security.sh
    
    * updated script

--- security.sh
+++ security.sh
@@ -8,24 +8,29 @@ while !(curl -s http://0.0.0.0:1001) >/dev/null; do
   sleep .1
 done
 
+cat zap.out
 echo "ZAP has successfully started"
 zap-cli --zap-url http://0.0.0.0 -p 1001 status -t 120
 zap-cli --zap-url http://0.0.0.0 -p 1001 open-url "${TEST_URL}"
 zap-cli --zap-url http://0.0.0.0 -p 1001 spider ${TEST_URL}
 zap-cli --zap-url http://0.0.0.0 -p 1001 active-scan --scanners all --recursive "${TEST_URL}"
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescanReport.xml -f xml
-echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
-chown -R $(id -u):$(id -u) activescan.html
-chown -R $(id -u):$(id -u) activescanReport.xml
-chown -R $(id -u):$(id -u) zap
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/activescan.html -f html
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/activescanReport.xml -f xml
+# echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
+# chown -R $(id -u):$(id -u) activescan.html
+# chown -R $(id -u):$(id -u) activescanReport.xml
+# chown -R $(id -u):$(id -u) zap
 
-cp activescan.html zap/
-cp activescanReport.xml zap/
+# cp activescan.html zap/
+# cp activescanReport.xml zap/
+mkdir -p zap_report
+chmod a+wx zap_report
+cp /zap/activescan.html zap_report/
+cp /zap/activescanReport.xml zap_report/
 
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Low --exit-code False
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-cp *.* zap/
+cp *.* zap_report/
 
 echo
 echo ZAP Security vulnerabilities were found that were not ignored
