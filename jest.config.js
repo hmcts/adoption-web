13ef2932428ab18ecb2dbf817e2bae46c30b105d
dilipsamrahmcts <dilip.samra@hmcts.net>
2022-04-06 10:42:34 +0100

    reverted config

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,10 @@ module.exports = {
   },
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
