3fbf4d18cca67be823b26fd7660e8f63565c6eb5
Pankaj Kumar <pankaj.kumar@hmcts.net>
2022-04-07 10:01:20 +0100

    Added test case

---
+++ src/main/steps/applicant2/same-address/SameAddressController.test.ts
@@ -0,0 +1,92 @@
+const mockGetParsedBody = jest.fn();
+const mockGetErrors = jest.fn();
+jest.mock('../../../app/form/Form', () => {
+  return {
+    Form: jest.fn().mockImplementation(() => {
+      return { getParsedBody: mockGetParsedBody, getErrors: mockGetErrors };
+    }),
+  };
+});
+
+// import { FieldPrefix } from '../../../app/case/case';
+import { mockRequest } from '../../../../test/unit/utils/mockRequest';
+import { mockResponse } from '../../../../test/unit/utils/mockResponse';
+import { PostController } from '../../../app/controller/PostController';
+
+import SameAddressPostController from './SameAddressController';
+// import { FormFields } from '../../../app/form/Form';
+
+describe('applicant2 > other-names > OtherNamesPostController', () => {
+  let req;
+  let res;
+  let controller;
+
+  beforeEach(() => {
+    req = mockRequest({
+      session: {
+        userCase: {
+          applicant1Address1: '101',
+          applicant1Address2: 'xyz palace',
+          applicant1AddressTown: 'London',
+          applicant1AddressCounty: 'London',
+          applicant1AddressPostcode: 'TW7 AAA',
+        },
+        save: jest.fn(done => done()),
+      },
+    });
+    res = mockResponse();
+    controller = new SameAddressPostController({});
+  });
+
+  test('should extend SameAddressPostController', async () => {
+    expect(controller).toBeInstanceOf(PostController);
+  });
+
+  describe('when there are no form errors', () => {
+    beforeEach(() => {
+      mockGetParsedBody.mockReturnValue({});
+      mockGetErrors.mockReturnValue([]);
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    describe('and when there is a applicant1Address', () => {
+      beforeEach(() => {
+        mockGetParsedBody.mockReturnValue({
+          applicant1Address1: '101',
+          applicant1Address2: 'xyz palace',
+          applicant1AddressTown: 'London',
+          applicant1AddressCounty: 'London',
+          applicant1AddressPostcode: 'TW7 AAA',
+        });
+        mockGetErrors.mockReturnValue([]);
+        controller = new SameAddressPostController({});
+        req.locals.api.triggerEvent.mockResolvedValue({
+          applicant1Address1: '101',
+          applicant1Address2: 'xyz palace',
+          applicant1AddressTown: 'London',
+          applicant1AddressCounty: 'London',
+          applicant1AddressPostcode: 'TW7 AAA',
+        });
+      });
+
+      test('should set the formData fields in userCase applicant1Address session data', async () => {
+        await controller.post(req, res);
+        expect(req.session.errors).toEqual([]);
+        // expect(req.session.userCase.applicant1Address1).toEqual([
+        //   { applicant1Address1: '101'},
+        // ]);
+        expect(req.session.save).toHaveBeenCalled();
+      });
+
+      //   test('should redirect to correct screen', async () => {
+      //     mockGetNextStepUrl.mockReturnValue('/MOCK_ENDPOINT');
+      //     await controller.post(req, res);
+      //     expect(mockGetNextStepUrl).toHaveBeenCalledWith(req, req.session.userCase);
+      //     expect(res.redirect).toHaveBeenCalledWith('/MOCK_ENDPOINT');
+      //   });
+    });
+  });
+});
