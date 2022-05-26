b7e30bf8bb00bcaf74a7f5e22abfb3c0a1a52872
Pankaj Kumar <pankaj.kumar@hmcts.net>
2022-05-26 12:51:49 +0100

    Fixed review comments

--- src/main/steps/save-as-draft/post.ts
+++ src/main/steps/save-as-draft/post.ts
@@ -14,13 +14,9 @@ export class SaveAsDraftPostController extends PostController<AnyObject> {
 
   public async post(req: AppRequest<AnyObject>, res: Response): Promise<void> {
     if (req.body.saveAsDraft) {
-      //return res.redirect(SIGN_OUT_URL);
       req.session.destroy(() => res.redirect('/'));
-      //return this.redirect(req, res, SIGN_OUT_URL);
     } else {
-      console.log('8. submit ' + req.body.saveAsDraft);
       return this.redirect(req, res, TASK_LIST_URL);
     }
-    // this.checkReturnUrlAndRedirect(req, res, this.ALLOWED_RETURN_URLS);
   }
 }
