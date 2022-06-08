ab9097f687fad0bd2f1521f4ca82c06cd1e625ef
pankajhmcts <94961980+pankajhmcts@users.noreply.github.com>
2022-06-08 13:14:43 +0100

    Save-as-draft change (#515)
    * Save-as-draft change
    
    * added test case
    
    * Fixed review comments
    
    * refactoring change
    
    * Fixed issue for other names & birth father/mother other nationality to support save as draft feature.
    
    * Save as Draft New tests added
    
    Co-authored-by: abidfs <abid.sayyad@hmcts.net>
    Co-authored-by: Raghu Nakkala <nraghu31@gmail.com>

---
+++ src/test/e2e/tests/saveAsDraft_test.js
@@ -0,0 +1,31 @@
+const config = require('../config');
+
+Feature('Save As Draft tests').retry(1);
+
+Scenario(
+  'Verify child adoption agency save as draft',
+  async ({ loginPage, landingPage, taskListPage, childAdoptionAgencyDetailsPage, saveAsDraftPage }) => {
+    await loginPage.createCitizenUserAndSignIn();
+    await landingPage.seeTheLandingPage();
+    await landingPage.selectApplyWithMySpouseSection();
+
+    await taskListPage.selectChildAdoptionAgencyDetails();
+    await childAdoptionAgencyDetailsPage.childAdoptionAgencyDetailsSaveAsDraft();
+    await saveAsDraftPage.saveAsDraftConfirmation();
+    await taskListPage.verifyChildAdoptionAgencyInProgresStatus();
+  }
+);
+
+Scenario(
+  'Verify child birth father details save as draft',
+  async ({ loginPage, landingPage, taskListPage, childBirthFatherDetailsPage, saveAsDraftPage }) => {
+    await loginPage.createCitizenUserAndSignIn();
+    await landingPage.seeTheLandingPage();
+    await landingPage.selectApplyWithMySpouseSection();
+
+    await taskListPage.selectChildBirthFatherDetails();
+    await childBirthFatherDetailsPage.childBirthFatherDetailsSaveAsDraft();
+    await saveAsDraftPage.saveAsDraftConfirmation();
+    await taskListPage.verifyChildBirthFatherInProgresStatus();
+  }
+);
