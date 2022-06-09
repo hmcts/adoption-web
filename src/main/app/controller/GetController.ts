67c934c3a723bfdc7d98ecd76319e57d1f56515f
abidfs <abid.sayyad@hmcts.net>
2022-06-09 16:30:27 +0100

    improved test coverage

--- src/main/app/controller/GetController.ts
+++ src/main/app/controller/GetController.ts
@@ -110,7 +110,7 @@ export class GetController {
     });
   }
 
-  protected getEventName(req: AppRequest): string {
+  public getEventName(req: AppRequest): string {
     if (req.session.user?.isSystemUser) {
       return SYSTEM_USER_UPDATE;
     }
