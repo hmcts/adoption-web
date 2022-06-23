a6cfeafbb2f670aa57adfd266b77b60f4d64af98
Raghu <44773331+nraghu32@users.noreply.github.com>
2022-06-23 17:29:52 +0100

    ADOP-1031 Test changes (#582)

--- src/test/e2e/pages/dateChildMovedinDetails.page.js
+++ src/test/e2e/pages/dateChildMovedinDetails.page.js
@@ -20,7 +20,7 @@ module.exports = {
     await I.retry(3).waitForText('When did the child move in with you?', 30);
     await I.retry(3).click('Save and continue');
     await I.retry(3).see('There is a problem');
-    await I.retry(3).see('Enter your date of birth');
+    await I.retry(3).see('Enter the date the child moved in with you');
     await I.wait(4);
     await I.retry(3).fillField(this.fields.dateChildMovedInDay, date);
     await I.retry(3).fillField(this.fields.dateChildMovedInMonth, month);
