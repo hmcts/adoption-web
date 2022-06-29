3c899535bcbfc72fe61b4e4ba90b0579a63881c7
Raghu Nakkala <nraghu31@gmail.com>
2022-06-29 13:29:17 +0100

    ADOP-979 Test changes

--- src/test/e2e/pages/childDetailsPlacementOrder.page.js
+++ src/test/e2e/pages/childDetailsPlacementOrder.page.js
@@ -17,9 +17,6 @@ module.exports = {
     await I.retry(3).fillField(this.fields.placementOrderNumber, childDetailsPlacementOrder.placementOrderNumber);
     await I.retry(3).click('Save and continue');
     await I.wait(2);
-    await I.retry(3).waitForText('Which court made the placement order?');
-    await I.retry(3).fillField(this.fields.placementOrderCourt, childDetailsPlacementOrder.placementOrderCourt);
-    await I.retry(3).click('Save and continue');
     await I.retry(3).waitForText('What date is on the placement order?');
     await I.retry(3).fillField(this.fields.placementOrderDateDay, childDetailsPlacementOrder.placementOrderDateDay);
     await I.retry(3).fillField(this.fields.placementOrderDateMonth, childDetailsPlacementOrder.placementOrderDateMonth);
@@ -36,12 +33,10 @@ module.exports = {
     await I.retry(3).click(this.fields.placementOrderType);
     await I.retry(3).click('Save and continue');
     await I.wait(2);
+    await I.retry(3).waitForText('What is the serial or case number on the placement order?');
     await I.retry(3).fillField(this.fields.placementOrderNumber, childDetailsPlacementOrder.placementOrderNumber);
     await I.retry(3).click('Save and continue');
     await I.wait(2);
-    await I.retry(3).waitForText('Which court made the placement order?');
-    await I.retry(3).fillField(this.fields.placementOrderCourt, childDetailsPlacementOrder.placementOrderCourt);
-    await I.retry(3).click('Save and continue');
     await I.retry(3).waitForText('What date is on the placement order?');
     await I.retry(3).fillField(this.fields.placementOrderDateDay, childDetailsPlacementOrder.placementOrderDateDay);
     await I.retry(3).fillField(this.fields.placementOrderDateMonth, childDetailsPlacementOrder.placementOrderDateMonth);
