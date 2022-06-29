e8e0b6d4f5dbd11437448cf5c90be2f833328a7d
Raghu Nakkala <nraghu31@gmail.com>
2022-06-29 16:18:53 +0100

    ADOP-979 Test changes

--- src/test/e2e/pages/childDetailsPlacementOrder.page.js
+++ src/test/e2e/pages/childDetailsPlacementOrder.page.js
@@ -16,7 +16,7 @@ module.exports = {
     await I.retry(3).waitForText('What is the serial or case number on the placement order?');
     await I.retry(3).fillField(this.fields.placementOrderNumber, childDetailsPlacementOrder.placementOrderNumber);
     await I.retry(3).click('Save and continue');
-    await I.wait(2);
+    await I.wait(4);
     await I.retry(3).waitForText('What date is on the placement order?');
     await I.retry(3).fillField(this.fields.placementOrderDateDay, childDetailsPlacementOrder.placementOrderDateDay);
     await I.retry(3).fillField(this.fields.placementOrderDateMonth, childDetailsPlacementOrder.placementOrderDateMonth);
