4bf862f6f5fa6253cc3862aab142bbff85d31611
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-07-25 11:14:03 +0100

    ADOP-1153

--- src/main/modules/draft-store/draft-store-service.ts
+++ src/main/modules/draft-store/draft-store-service.ts
@@ -1,9 +1,6 @@
 import { CaseWithId } from '../../app/case/case';
 import { AppRequest } from '../../app/controller/AppRequest';
 
-const { Logger } = require('@hmcts/nodejs-logging');
-const logger = Logger.getLogger('draftStoreService');
-
 export const getDraftCaseFromStore = async (req: AppRequest, caseRef: string): Promise<CaseWithId> => {
   const dataFromRedis = await req.app.locals.draftStoreClient.get(caseRef);
   let returnData;
@@ -11,7 +8,9 @@ export const getDraftCaseFromStore = async (req: AppRequest, caseRef: string): P
     try {
       returnData = JSON.parse(dataFromRedis) as CaseWithId;
     } catch (err: unknown) {
-      logger.error(`${(err as Error).stack || err}`);
+      console.log(err);
+
+      return returnData;
     }
   }
   return returnData;
@@ -19,9 +18,7 @@ export const getDraftCaseFromStore = async (req: AppRequest, caseRef: string): P
 
 export const saveDraftCase = async (req: AppRequest, caseRef: string): Promise<CaseWithId> => {
   const dataToStore = req.session.userCase;
-
   const draftStoreClient = req.app.locals.draftStoreClient;
-
   draftStoreClient.set(caseRef, JSON.stringify(dataToStore));
   return dataToStore;
 };
