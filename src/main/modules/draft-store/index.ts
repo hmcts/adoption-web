9baa0322530558df799327ec50312becd0246151
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-07-27 18:51:03 +0100

    removed default username

--- src/main/modules/draft-store/index.ts
+++ src/main/modules/draft-store/index.ts
@@ -8,14 +8,21 @@ export class DraftStoreClient {
   public static REDIS_CONNECTION_SUCCESS = 'Connected to Redis instance successfully';
 
   public enableFor(app: Application): void {
-    const protocol = config.get('services.draftStore.redis.tls') ? 'rediss://' : 'redis://';
-    const connectionString = `${protocol}${config.get('session.redis.key')}@${config.get(
-      'services.draftStore.redis.host'
-    )}:${config.get('services.draftStore.redis.port')}`;
+    // const protocol = config.get('services.draftStore.redis.tls') ? 'rediss://' : 'redis://';
+    // const connectionString = `${protocol}${config.get('session.redis.key')}@${config.get(
+    //   'services.draftStore.redis.host'
+    // )}:${config.get('services.draftStore.redis.port')}`;
 
-    Logger.error('connection string', connectionString);
+    const client = new Redis({
+      host: config.get('services.draftStore.redis.host'),
+      port: config.get('services.draftStore.redis.port'),
+      password: config.get('session.redis.key'),
+      tls: {
+        servername: config.get('services.draftStore.redis.host'),
+      },
+    });
 
-    const client = new Redis(connectionString);
+    Logger.error('connection string');
 
     app.locals.draftStoreClient = client;
   }
