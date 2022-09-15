cb085d4c2a540dfb96090e578364aeae4893e30a
rajatkgupta029 <rajatkumar.gupta@hmcts.net>
2022-09-15 19:01:21 +0100

    Adding redis 30 days logic with 5 mins on PR env

--- src/main/modules/draft-store/draft-store-service.ts
+++ src/main/modules/draft-store/draft-store-service.ts
@@ -1,3 +1,5 @@
+import config from 'config';
+
 import { Case, CaseWithId } from '../../app/case/case';
 import { AppRequest } from '../../app/controller/AppRequest';
 
@@ -24,20 +26,23 @@ export const saveDraftCase = async (req: AppRequest, caseRef: string, formData: 
     'selectedSiblingPoType',
     'selectedSiblingRelation',
   ];
+  const expireTimeInMs: number = config.get('services.draftStore.redis.ttl');
   fieldsToBeExcluded.forEach(item => {
     if (formData[item]) {
       delete formData[item];
     }
   });
+  let ttl: number = expireTimeInMs;
   let dataToStore = formData;
   const draftCaseFromRedis = await getDraftCaseFromStore(req, caseRef);
   if (draftCaseFromRedis) {
     dataToStore = { ...draftCaseFromRedis, ...dataToStore };
   } else {
+    ttl = ttl - req.app.locals.draftStoreClient.ttl(caseRef);
     dataToStore = { ...dataToStore };
   }
   const draftStoreClient = req.app.locals.draftStoreClient;
-  draftStoreClient.set(caseRef, JSON.stringify(dataToStore));
+  draftStoreClient.set(caseRef, JSON.stringify(dataToStore), 'PX', ttl);
 
   return req.session.userCase;
 };
