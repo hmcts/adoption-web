e453f9502a39cf76f928a6fea70a3814cfdf69d2
REMYA MARY JAMES <RemyaMary.James@hmcts.net>
2022-10-31 12:22:09 +0000

    enabling trigger word search for courts

--- src/main/steps/children/find-family-court/util.ts
+++ src/main/steps/children/find-family-court/util.ts
@@ -2,18 +2,27 @@ import config from 'config';
 
 export const getCourtEmailId = (key: string): string => {
   const courtKey: string = key && key.trim().length > 0 ? key.trim().toLowerCase() : 'court name not provided';
+  console.log('*********');
+  console.log(courtKey);
   const map = new Map<string, string>([
-    ['chelmsford family court', config.get('localCourt.emailId.CHELMSFORD_FAMILY_COURT')],
-    ['oxford family court', config.get('localCourt.emailId.OXFORD_FAMILY_COURT')],
-    ['medway family court', config.get('localCourt.emailId.MEDWAY_FAMILY_COURT')],
-    ['portsmouth family court', config.get('localCourt.emailId.PORTSMOUTH_FAMILY_COURT')],
-    ['leeds family court', config.get('localCourt.emailId.LEEDS_FAMILY_COURT')],
-    ['northampton family court', config.get('localCourt.emailId.NORTHAMPTON_FAMILY_COURT')],
-    ['worcester family court', config.get('localCourt.emailId.WORCESTER_FAMILY_COURT')],
-    ['newport (gwent) family court', config.get('localCourt.emailId.NEWPORT_GWENT_FAMILY_COURT')],
-    ['liverpool family court', config.get('localCourt.emailId.LIVERPOOL_FAMILY_COURT')],
-    ['central london family court', config.get('localCourt.emailId.CENTRAL_LONDON_FAMILY_COURT')],
-    ['reading family court', config.get('localCourt.emailId.READING_FAMILY_COURT')],
+    ['chelmsford', config.get('localCourt.emailId.CHELMSFORD_FAMILY_COURT')],
+    ['oxford', config.get('localCourt.emailId.OXFORD_FAMILY_COURT')],
+    ['medway', config.get('localCourt.emailId.MEDWAY_FAMILY_COURT')],
+    ['portsmouth', config.get('localCourt.emailId.PORTSMOUTH_FAMILY_COURT')],
+    ['leeds', config.get('localCourt.emailId.LEEDS_FAMILY_COURT')],
+    ['northampton', config.get('localCourt.emailId.NORTHAMPTON_FAMILY_COURT')],
+    ['worcester', config.get('localCourt.emailId.WORCESTER_FAMILY_COURT')],
+    ['newport', config.get('localCourt.emailId.NEWPORT_GWENT_FAMILY_COURT')],
+    ['liverpool', config.get('localCourt.emailId.LIVERPOOL_FAMILY_COURT')],
+    ['central london', config.get('localCourt.emailId.CENTRAL_LONDON_FAMILY_COURT')],
+    ['reading', config.get('localCourt.emailId.READING_FAMILY_COURT')],
   ]);
-  return map.has(courtKey) ? (map.get(courtKey) as string) : config.get('localCourt.emailId.FALLBACK_EMAIL_ID');
+  for (const mKey of map.keys()) {
+    if (courtKey.includes(mKey)) {
+      console.log('*********');
+      console.log(mKey);
+      return map.has(mKey) ? (map.get(mKey) as string) : config.get('localCourt.emailId.FALLBACK_EMAIL_ID');
+    }
+  }
+  return map.has(key) ? (map.get(key) as string) : config.get('localCourt.emailId.FALLBACK_EMAIL_ID');
 };
