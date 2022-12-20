9c95ea2a62e03b5fd724fd85f6cfaab3a6fecbb5
2219956 <ramesh.meripe@hmcts.net>
2022-12-20 16:56:12 +0000

    FPET-232 Adoption - Start Page : Function test case fix

--- src/test/e2e/pages/LandingPage.js
+++ src/test/e2e/pages/LandingPage.js
@@ -4,11 +4,13 @@ module.exports = {
   fields: {
     applyingWith: 'input[id$="applyingWith"]',
     applyWithMySpouse: 'input[id$="applyingWith-2"]',
+    startNowButtonLink: 'a[href$="/applying-with"]',
   },
   async seeTheLandingPage() {
    // await I.retry(2).goToPage(config.baseUrl + '/start-placement');
     //await I.wait(2);
-    await I.retry(3).click('Start now');
+    //await I.retry(3).click('Start now');
+    await I.retry(3).click(this.fields.startNowButtonLink);
     await I.wait(2);
     await I.retry(3).see('Are you applying on your own, or with someone else?');
   },
