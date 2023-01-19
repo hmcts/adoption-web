a643f9b94e62559391ae12c6816e669882767fbb
Thirunavukkarasu Soundararajan <Thirunavukkarasu.Soundararajan@hmcts.net>
2023-01-19 16:50:06 +0000

    ADOP-1945-LAportal-DevChanges

--- src/main/steps/children/remove-placement-order/content.ts
+++ src/main/steps/children/remove-placement-order/content.ts
@@ -1,37 +1,63 @@
-import { YesOrNo } from '../../../app/case/definition';
+import { CaseWithId } from '../../../app/case/case';
+import { PlacementOrderTypeEnum, YesOrNo } from '../../../app/case/definition';
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 import { isFieldFilledIn } from '../../../app/form/validation';
 import { defaultButtons } from '../../../steps/common/components/common/default-buttons';
 
+export const getPlacementOrderType = (userCase: Partial<CaseWithId>): string => {
+  const placementOrder = userCase.placementOrders?.find(
+    item => item.placementOrderId === userCase.selectedPlacementOrderId
+  );
+  return `${placementOrder?.placementOrderType}`;
+};
+
 //eslint-disable-next-line @typescript-eslint/no-unused-vars
 const en = content => {
   const enContent = {
     section: "The child's details",
+    childsPOType: {
+      [PlacementOrderTypeEnum.CareOrder]: 'Care order',
+      [PlacementOrderTypeEnum.ContactOrder]: 'Child arrangements order',
+      [PlacementOrderTypeEnum.AdoptionOrder]: 'Previous adoption order',
+      [PlacementOrderTypeEnum.PlacementOrder]: 'Previous placement order',
+      [PlacementOrderTypeEnum.SupervisionOrder]: 'Supervision order',
+      [PlacementOrderTypeEnum.Other]: 'Other order',
+    },
     errors: {
       confirm: {
         required: 'Please select an answer',
       },
     },
   };
+  const placementOrderText = getPlacementOrderType(content.userCase);
   return {
     ...enContent,
-    title: 'Are you sure you want to remove this ?',
+    title: `Are you sure you want to remove this ${enContent.childsPOType[placementOrderText]} ?`,
   };
 };
 //eslint-disable-next-line @typescript-eslint/no-unused-vars
 const cy: typeof en = content => {
   const cyContent = {
     section: 'Manylion y plentyn',
+    childsPOType: {
+      [PlacementOrderTypeEnum.CareOrder]: 'Gorchymyn Gofal',
+      [PlacementOrderTypeEnum.ContactOrder]: 'Gorchymyn trefniadau plant',
+      [PlacementOrderTypeEnum.AdoptionOrder]: 'Gorchymyn mabwysiadu blaenorol',
+      [PlacementOrderTypeEnum.PlacementOrder]: 'Gorchymyn lleoli blaenorol',
+      [PlacementOrderTypeEnum.SupervisionOrder]: 'Gorchymyn Goruchwylio',
+      [PlacementOrderTypeEnum.Other]: 'Gorchymyn Arall',
+    },
     errors: {
       confirm: {
         required: 'Dewiswch ateb os gwelwch yn dda',
       },
     },
   };
+  const placementOrderText = getPlacementOrderType(content.userCase);
   return {
     ...cyContent,
-    title: "Ydych chi'n siŵr eich bod am ddileu hwn ?",
+    title: `Ydych chi'n siŵr eich bod am ddileu hwn ${cyContent.childsPOType[placementOrderText]} ?`,
   };
 };
 
