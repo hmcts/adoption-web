64f4d46acd0e29fd27371ab39cb4cfead9c00417
raghyn <nraghu31@gmail.com>
2023-02-03 13:54:52 +0000

    ADOP-2054 e2e tests

--- src/test/e2e/pages/saveAsDraft.page.js
+++ src/test/e2e/pages/saveAsDraft.page.js
@@ -5,11 +5,12 @@ module.exports = {
     dateChildMovedInDay: 'input[id$="dateChildMovedIn-day"]',
     dateChildMovedInMonth: 'input[id$="dateChildMovedIn-month"]',
     dateChildMovedInYear: 'input[id$="dateChildMovedIn-year"]',
+    continueButton: "//form[@id='main-form']//button[contains(text(), 'Continue with your application')]",
   },
 
   async saveAsDraftConfirmation() {
     await I.retry(3).see('Your application has been saved');
-    await I.retry(3).click('Continue with your application');
+    await I.retry(3).click(this.fields.continueButton);
     await I.wait(4);
   },
 };
