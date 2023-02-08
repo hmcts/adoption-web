9226d9bb05937477c887ac6e5ec18a7fd7bc504b
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2023-02-08 09:27:37 +0000

    ADOP-2169: Add english content to welsh section

--- src/main/steps/review-pay-submit/pay-and-submit/content.test.ts
+++ src/main/steps/review-pay-submit/pay-and-submit/content.test.ts
@@ -17,12 +17,14 @@ const enContent = {
 };
 
 const cyContent = {
-  section: '',
-  title: '',
-  line1: '',
-  line2: '',
-  line3: '',
-  payandsubmit: '',
+  section: 'Review your application',
+  title: 'Pay and submit',
+  line1:
+    'You will be taken to the payment page. Your application will be submitted to the local authority and they will be asked to progress it. You cannot edit the application once it has been submitted.',
+  line2:
+    "If you're applying for more than one child, you must submit a new application for each child. You will not be charged if you submit these before midnight on the day of your first application. If you submit after the day of the first application, you will be charged another £183. You must sign in using the same email address and password used in your first application.",
+  line3: 'A confirmation email of your payment will be sent to you.',
+  payandsubmit: 'Pay and submit application',
 };
 
 const commonContent = { language: 'en' } as CommonContent;
