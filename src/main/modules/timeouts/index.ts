49650d46de9e75005af2eb5d3a980b9febc00968
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2023-02-20 15:04:33 +0000

    added console logs

--- src/main/modules/timeouts/index.ts
+++ src/main/modules/timeouts/index.ts
@@ -21,12 +21,15 @@ export class LoadTimeouts {
 
       // Set the timeout for all HTTP requests
       req.setTimeout(timeoutMs, () => {
+        console.log('timeoutMs inside request Timeout ----   ' + timeoutMs);
+
         const err = new HTTPError('Request Timeout', StatusCodes.REQUEST_TIMEOUT);
         errorController.internalServerError(err, req as AppRequest, res);
       });
 
       // Set the server response timeout for all HTTP requests
       res.setTimeout(timeoutMs, () => {
+        console.log('timeoutMs inside response Timeout ----   ' + timeoutMs);
         const err = new HTTPError('Service Unavailable', StatusCodes.SERVICE_UNAVAILABLE);
         errorController.internalServerError(err, req as AppRequest, res);
       });
