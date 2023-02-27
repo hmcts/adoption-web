cd9cb95024db99da75b029ef0fc0e87db7982d3a
hcarslaw <Hugh.Carslaw@HMCTS.NET>
2023-02-27 12:16:48 +0000

    ADOP-2121: Update contact us page details

--- src/main/steps/application/contact-us/content.ts
+++ src/main/steps/application/contact-us/content.ts
@@ -2,16 +2,23 @@ import { TranslationFn } from '../../../app/controller/GetController';
 
 const en = {
   title: 'Adoption',
-  email: 'Email',
-  emailAddress:
-    'Email us at <a href="mailto:adoptionproject@justice.gov.uk" class="govuk-link">adoptionproject@justice.gov.uk.</a>',
+  sendUsAMessageHeading: 'Send us a message',
+  email: 'Email: <a href="mailto:adoptionproject@justice.gov.uk" class="govuk-link">adoptionproject@justice.gov.uk</a>',
+  sendUsAMessageText: 'We aim to get back to you within 5 working days',
+  telephoneHeading: 'Telephone',
+  telephoneNumber: 'Telephone: 01634 887900',
+  telephoneText: 'Monday to Friday 9am to 5pm',
 };
 
 const cy: typeof en = {
   title: 'Mabwysiadu',
-  email: 'E-bost',
-  emailAddress:
-    'Anfonwch neges e-bost i <a href="mailto:adoptionproject@justice.gov.uk" class="govuk-link">adoptionproject@justice.gov.uk.</a>',
+  sendUsAMessageHeading: 'Anfonwch neges atom',
+  email:
+    'E-bost: <a href="mailto:ymholiadaucymraeg@justice.gov.uk" class="govuk-link">ymholiadaucymraeg@justice.gov.uk</a>',
+  sendUsAMessageText: 'Byddwn yn anelu at ymateb o fewn 5 diwrnod gwaith.',
+  telephoneHeading: 'Rhif ffôn',
+  telephoneNumber: 'Rhif ffôn: 01634 887900',
+  telephoneText: 'Dydd Llun i ddydd Iau 9am - 5pm',
 };
 
 const languages = {
