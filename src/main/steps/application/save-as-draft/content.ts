3a5c5532ad9dbb8e74bfa0c7ec82d294f5272caf
tsounda <111878461+tsounda@users.noreply.github.com>
2023-03-14 15:50:47 +0000

    ADOP-2207-MC-SaveDraft (#1048)
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    * ADOP-2207-MC-SaveDraft
    
    ---------
    
    Co-authored-by: Raghu <44773331+nraghu32@users.noreply.github.com>

--- src/main/steps/application/save-as-draft/content.ts
+++ src/main/steps/application/save-as-draft/content.ts
@@ -1,15 +1,21 @@
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 
-const en = () => ({
+const en = content => ({
   title: 'Your application has been saved',
+  multipleChildrenMessage: content.userCase.canPaymentIgnored
+    ? 'You must submit any additional applications before midnight on the same day that you submitted your first application. This is to avoid additional application fees.'
+    : '',
   continueApplication: 'Continue with your application',
   signOut: 'Sign out',
 });
 
-const cy: typeof en = () => ({
+const cy: typeof en = content => ({
   title: 'Mae eich cais wedi cael ei gadw',
-  continueApplication: 'Parhau â’ch cais',
+  multipleChildrenMessage: content.userCase.canPaymentIgnored
+    ? 'Mae’n rhaid ichi gyflwyno unrhyw geisiadau ychwanegol cyn hanner nos ar yr un diwrnod y bu ichi gyflwyno eich cais cyntaf. Rhaid gwneud hyn i beidio â gorfod talu ffioedd gwneud cais ychwanegol.'
+    : '',
+  continueApplication: 'Parhau gyda’ch cais',
   signOut: 'Allgofnodi',
 });
 
@@ -29,6 +35,6 @@ const languages = {
 };
 
 export const generateContent: TranslationFn = content => ({
-  ...languages[content.language](),
+  ...languages[content.language](content),
   form,
 });
