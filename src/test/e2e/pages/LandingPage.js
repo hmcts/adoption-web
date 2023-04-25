8b52b33e56d7c21763793bb1b734e47a9ccebbc1
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2023-04-25 15:32:17 +0100

    removed 2nd start page test

--- src/test/e2e/pages/LandingPage.js
+++ src/test/e2e/pages/LandingPage.js
@@ -6,16 +6,13 @@ module.exports = {
     applyWithMySpouse: 'input[id$="applyingWith-2"]',
   },
   async seeTheLandingPage() {
-   await I.wait(2);
-   I.see("Apply to adopt a child placed in your care");
-  },
-  async seeStartPlacementAndClickPage() {
     await I.wait(2);
-     await I.retry(3).click('Start now');
+    I.see('Apply to adopt a child placed in your care');
   },
+
   async seeFirstQuestionPage() {
-      await I.wait(2);
-      await I.retry(3).see('Are you applying on your own, or with someone else?');
+    await I.wait(2);
+    await I.retry(3).see('Are you applying on your own, or with someone else?');
   },
 
   async selectApplyOnMyownSection() {
