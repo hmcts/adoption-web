6e43adbbf16f215933964818a49462ec79ce3191
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2023-04-27 18:14:55 +0100

    Master rebase (#1141)
    * removed eligibility start now page post idam login
    
    * updated package.json
    
    * removed 2nd start page test
    
    * removed 2nd start page test
    
    * removed 1 line for start now page test
    
    * comment out seeLandingPage for now
    
    * Removed seeLandingPage in tests
    
    * functional and smoke test pointed to staging url
    
    ---------
    
    Co-authored-by: elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
    Co-authored-by: priteshkr <101888106+priteshkr@users.noreply.github.com>

--- src/test/e2e/config.js
+++ src/test/e2e/config.js
@@ -4,7 +4,7 @@ module.exports = {
     password: process.env.CITIZEN_PASSWORD,
   },
 
-  baseUrl: process.env.ADOP_WEB_URL || 'https://adoption-web.aat.platform.hmcts.net/',
+  baseUrl: process.env.ADOP_WEB_URL || 'https://adoption-web-staging.aat.platform.hmcts.net/',
 
   definition: {
     jurisdiction: 'PUBLICLAW',
