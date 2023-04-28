b2dc3d0145066616be7dcd6df3678dec26722fd2
sairamkantheti1 <sai.kantheti@HMCTS.NET>
2023-04-28 13:31:12 +0100

    FPET-529 coverage increase

--- src/main/steps/review-pay-submit/check-your-answers/CheckYouAnswersPostController.test.ts
+++ src/main/steps/review-pay-submit/check-your-answers/CheckYouAnswersPostController.test.ts
@@ -12,11 +12,16 @@ const mockGetNextStepUrl = jest.fn();
 jest.mock('../../../steps', () => {
   return { getNextStepUrl: mockGetNextStepUrl };
 });
+import moment from 'moment';
 
 import { mockRequest } from '../../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../../test/unit/utils/mockResponse';
+import * as caseApi from '../../../app/case/CaseApi';
+import { STATEMENT_OF_TRUTH } from '../../urls';
 
 import CheckYouAnswersPostController from './CheckYouAnswersPostController';
+
+const getCaseApiMock = jest.spyOn(caseApi, 'getCaseApi');
 
 describe('CheckYouAnswersPostController', () => {
   let req;
@@ -56,6 +61,59 @@ describe('CheckYouAnswersPostController', () => {
       await controller.post(req, res);
       expect(mockGetNextStepUrl).not.toHaveBeenCalled();
       expect(res.redirect).toHaveBeenCalledWith('/request');
+    });
+  });
+
+  describe('when there is no submitted cases today object', () => {
+    const caseApiMockFn = {
+      getCases: jest.fn(() => {
+        return [
+          {
+            id: '123456',
+            state: 'Submitted',
+            case_data: { applyingWith: 'alone' },
+            dateSubmitted: '2023-04-02',
+          },
+          {
+            id: '123456',
+            state: 'Submitted',
+            case_data: { applyingWith: 'alone' },
+            dateSubmitted: '2023-04-02',
+          },
+        ];
+      }),
+      unlinkStaleDraftCaseIfFound: jest.fn(() => {
+        return undefined;
+      }),
+    };
+    (getCaseApiMock as jest.Mock).mockReturnValue(caseApiMockFn);
+
+    test('should redirect to same page', async () => {
+      await controller.post(req, res);
+      expect(res.redirect).toHaveBeenCalledWith(STATEMENT_OF_TRUTH);
+    });
+  });
+
+  describe('when there is one submitted cases today object', () => {
+    const caseApiMockFn = {
+      getCases: jest.fn(() => {
+        return [
+          {
+            id: '123456',
+            state: 'Submitted',
+            case_data: { applyingWith: 'alone', dateSubmitted: moment(new Date()).format('YYYY-MM-DD') },
+          },
+        ];
+      }),
+      unlinkStaleDraftCaseIfFound: jest.fn(() => {
+        return undefined;
+      }),
+    };
+    (getCaseApiMock as jest.Mock).mockReturnValue(caseApiMockFn);
+
+    test('should redirect to same page', async () => {
+      await controller.post(req, res);
+      expect(res.redirect).toHaveBeenCalledWith(STATEMENT_OF_TRUTH);
     });
   });
 
