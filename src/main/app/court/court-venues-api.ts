7f0d0dd823e0edb161e9eccdb7dfb79f6d39ead1
mohitvijay45 <107183474+mohitvijay45@users.noreply.github.com>
2023-09-13 15:17:25 +0100

    added court api call for LA portal

--- src/main/app/court/court-venues-api.ts
+++ src/main/app/court/court-venues-api.ts
@@ -1,63 +1,70 @@
-//import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import config from 'config';
-import Logger from 'winston';
+import Logger, { LoggerInstance } from 'winston';
 
-import { AppRequest } from '../../app/controller/AppRequest';
-//import { getServiceAuthToken } from '../auth/service/get-service-auth-token';
+import { AppRequest, UserDetails } from '../../app/controller/AppRequest';
+import { getServiceAuthToken } from '../auth/service/get-service-auth-token';
 
-import { CourtVenue } from './location';
+import { CourtVenue, LocationResponse } from './location';
 
-export async function getCourtList(req: AppRequest): Promise<CourtVenue[]> {
+export async function getCourtListFromStaticList(): Promise<CourtVenue[]> {
   // Adoption:	ABA4
   const adoptionCourtList = await getCourtVenues();
-  console.log(req.locals.lang);
-  /* `${config.get('services.adoptionCourt.code')}`,
+  return [...adoptionCourtList];
+}
+
+export async function getCourtListFromAPI(req: AppRequest): Promise<CourtVenue[]> {
+  // Adoption:	ABA4
+  const adoptionCourtList = await getCourtVenuesFromAPI(
+    `${config.get('services.adoptionCourt.code')}`,
     req.session.user,
-    req.locals.logger */
+    req.locals.logger
+  );
+
   return [...adoptionCourtList];
 }
 
 /* This is used to get all the court list refdata API call. 
 Commenting it out for now to fetch the list from static data */
 
-// export const getCourtVenuesOld = async (
-//   serviceCode: string,
-//   userDetails: UserDetails,
-//   logger: LoggerInstance
-// ): Promise<CourtVenue[]> => {
-//   Logger.error('services.location_api.url: ' + config.get('services.location_api.url'));
+export const getCourtVenuesFromAPI = async (
+  serviceCode: string,
+  userDetails: UserDetails,
+  logger: LoggerInstance
+): Promise<CourtVenue[]> => {
+  Logger.error('services.location_api.url: ' + config.get('services.location_api.url'));
 
-//   try {
-//     const response: AxiosResponse<LocationResponse> = await axios.get(
-//       `${config.get('services.location_api.url')}/refdata/location/court-venues/services`,
-//       {
-//         headers: {
-//           Authorization: 'Bearer ' + userDetails.accessToken,
-//           ServiceAuthorization: getServiceAuthToken(),
-//           Accept: '*/*',
-//           'Content-Type': 'application/json',
-//         },
-//         params: {
-//           service_code: serviceCode,
-//         },
-//       }
-//     );
+  try {
+    const response: AxiosResponse<LocationResponse> = await axios.get(
+      `${config.get('services.location_api.url')}/refdata/location/court-venues/services`,
+      {
+        headers: {
+          Authorization: 'Bearer ' + userDetails.accessToken,
+          ServiceAuthorization: getServiceAuthToken(),
+          Accept: '*/*',
+          'Content-Type': 'application/json',
+        },
+        params: {
+          service_code: serviceCode,
+        },
+      }
+    );
 
-//     if (!response.data?.court_venues) {
-//       return [];
-//     }
+    if (!response.data?.court_venues) {
+      return [];
+    }
 
-//     return response.data.court_venues;
-//   } catch (err) {
-//     logger.error(
-//       `Error occurred while fetching court venues data from ${config.get(
-//         'services.location_api.url'
-//       )} for ${serviceCode}`
-//       // err
-//     );
-//     return [];
-//   }
-// };
+    return response.data.court_venues;
+  } catch (err) {
+    logger.error(
+      `Error occurred while fetching court venues data from ${config.get(
+        'services.location_api.url'
+      )} for ${serviceCode}`
+      // err
+    );
+    return [];
+  }
+};
 
 // This is used to get all the court list through a static list
 export const getCourtVenues = async (): Promise<CourtVenue[]> => {
@@ -72,7 +79,5 @@ export const getCourtVenues = async (): Promise<CourtVenue[]> => {
     };
     courtVenueList.push(courtVenue);
   }
-
-  console.log(JSON.stringify(courtVenueList));
   return courtVenueList;
 };
