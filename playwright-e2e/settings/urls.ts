7af2dbce3217f8d913cca7e0649af4ebe54bf489
Georgina <georgina.hall1@hmcts.net>
2024-04-16 16:16:20 +0100

    ADOP-2403 Fix linting issues

--- playwright-e2e/settings/urls.ts
+++ playwright-e2e/settings/urls.ts
@@ -1,5 +1,5 @@
 // aat, demo, perftest, ithc
-const env = process.env.ENVIRONMENT || "aat";
+const env = process.env.ENVIRONMENT || 'aat';
 
 interface UrlConfig {
   [key: string]: string;
@@ -8,6 +8,6 @@ interface UrlConfig {
 export const urlConfig: UrlConfig = {
   idamUrl: process.env.AAT_IDAM_URL || `https://idam-api.${env}.platform.hmcts.net`,
   citizenStartUrl: process.env.CIT_BASE_URL || `https://adoption-web.${env}.platform.hmcts.net/eligibility/start`,
-  laPortalUrl: process.env.LA_BASE_URL|| `https://adoption-web.${env}.platform.hmcts.net/la-portal/kba-case-ref/`
+  laPortalUrl: process.env.LA_BASE_URL || `https://adoption-web.${env}.platform.hmcts.net/la-portal/kba-case-ref/`,
   // You can add other URLs as needed
 };
--- playwright-e2e/smoke.spec.ts
+++ playwright-e2e/smoke.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
-import { urlConfig } from "./settings/urls";
+import { expect, test } from '@playwright/test';
+
+import { urlConfig } from './settings/urls';
 
 test.describe('Adoption', () => {
-  
   test('should not have any automatically detectable accessibility issues @accessibility', async ({ page }) => {
     await page.goto(`${urlConfig.citizenStartUrl}`);
 
     const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22a", "wcag22aa"])
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22a', 'wcag22aa'])
       .analyze();
 
     expect(accessibilityScanResults.violations).toEqual([]);
