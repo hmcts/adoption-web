e2a1dbcd975dac694ed363400c9560df58985f76
Georgina <georgina.hall1@hmcts.net>
2024-04-16 15:30:48 +0100

    ADOP-2403 WIP

--- playwright-e2e/smoke.spec.ts
+++ playwright-e2e/smoke.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
+import { urlConfig } from "./settings/urls";
 
 test.describe('Adoption', () => {
   
   test('should not have any automatically detectable accessibility issues @accessibility', async ({ page }) => {
-    await page.goto('https://adoption-web.aat.platform.hmcts.net/');
+    await page.goto(`${urlConfig.citizenStartUrl}`);
 
     const accessibilityScanResults = await new AxeBuilder({ page })
       .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22a", "wcag22aa"])
@@ -15,8 +16,8 @@ test.describe('Adoption', () => {
 });
 
 test('has title @smoke-test', async ({ page }) => {
-  await page.goto('https://adoption-web.aat.platform.hmcts.net/');
+  await page.goto(`${urlConfig.citizenStartUrl}`);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Sign in - HMCTS Access - GOV.UK/);
+  await expect(page).toHaveTitle(/Apply for adoption - Apply to adopt a child placed in your care - GOV.UK/);
 });
