b451644dd43c547437ed16b3ae88afe17e0c4fe8
Paul Trelease <paul.trelease@justice.gov.uk>
2024-05-02 12:18:02 +0100

    ADOP-2323-disabling-fee-call: removing fee call, hardcoding 1234 response

--- src/main/app/fee/fee-lookup-api.ts
+++ src/main/app/fee/fee-lookup-api.ts
@@ -1,35 +1,15 @@
-import axios, { AxiosResponse } from 'axios';
-import config from 'config';
 import { LoggerInstance } from 'winston';
 
 import { Fee } from '../../app/case/definition';
 
 export const getFee = async (logger: LoggerInstance): Promise<Fee | undefined> => {
   try {
-    const response: AxiosResponse = await axios.get(config.get('services.feeLookup.url'), {
-      headers: {
-        accept: 'application/json',
-      },
-      params: {
-        application_type: 'all',
-        channel: 'default',
-        event: 'issue',
-        jurisdiction1: 'family',
-        jurisdiction2: 'family court',
-        keyword: 'ApplyAdoption',
-        service: 'adoption',
-      },
-    });
-
-    if (!response.data) {
-      return;
-    }
 
     return {
-      FeeCode: `${response.data.code}`,
-      FeeDescription: `${response.data.description}`,
-      FeeVersion: `${response.data.version}`,
-      FeeAmount: `${response.data.fee_amount}`,
+      FeeAmount: '1234',
+      FeeCode: 'MOCK_CODE',
+      FeeDescription: 'MOCK_DESCRIPTION',
+      FeeVersion: 'MOCK_VERSION',
     };
   } catch (err) {
     logger.error('Fee lookup error occurred', err);
