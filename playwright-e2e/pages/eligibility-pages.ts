b033cca0ab64a4a6d8f1fabfc3dd41b12f0edd89
Abigail Smith <abigail.smith@hmcts.net>
2024-05-14 16:12:53 +0100

    fix basepage error

--- playwright-e2e/pages/eligibility-pages.ts
+++ playwright-e2e/pages/eligibility-pages.ts
@@ -1,5 +1,5 @@
 import { type Locator, type Page, expect } from '@playwright/test';
-import { BasePage } from "./base-page";
+import { BasePage } from './base-page';
 export class Eligibility extends BasePage{
   readonly h1: Locator;
   readonly mainContent: Locator;
