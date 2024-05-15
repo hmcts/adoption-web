5e34ea023685d8334cd2ad57ecc317bf84a04b71
Abigail Smith <abigail.smith@hmcts.net>
2024-05-15 11:24:33 +0100

    fix linting issue

--- playwright-e2e/pages/eligibility-pages.ts
+++ playwright-e2e/pages/eligibility-pages.ts
@@ -152,23 +152,37 @@ export class Eligibility extends BasePage {
     await expect(this.mainContent).toContainText(['You cannot apply to adopt']);
   }
 
-  //Error checking tests
+  //Error checking test
   async errorCheck(): Promise<void> {
     await this.handleFormError('Select if you are applying to adopt more than one child', this.applyMoreThanOneChildNo);
-    await this.handleFormError('Select if the child will be under 18 years old on the date you submit your application.', this.childUnder18Yes);
-    await this.handleFormError('Select if the child is married or in a civil partnership.', this.isChildMarriedOrCivilPartnershipNo);
-    await this.handleFormError('Select if you, and the other applicant if relevant, are both aged 21 or over.', this.areYouAndApplicantOver21Yes);
-    await this.handleFormError('Select if the UK, Channel Islands or Isle of Man is the main country of residence for you, and the other applicant.', this.uKCountryOfResidenceYes);
-    await this.handleFormError('Select if you, and the other applicant if relevant, have lived in the UK, Channel Islands or Isle of Man for the last 12 months.');
+    await this.handleFormError(
+      'Select if the child will be under 18 years old on the date you submit your application.',
+      this.childUnder18Yes
+    );
+    await this.handleFormError(
+      'Select if the child is married or in a civil partnership.',
+      this.isChildMarriedOrCivilPartnershipNo
+    );
+    await this.handleFormError(
+      'Select if you, and the other applicant if relevant, are both aged 21 or over.',
+      this.areYouAndApplicantOver21Yes
+    );
+    await this.handleFormError(
+      'Select if the UK, Channel Islands or Isle of Man is the main country of residence for you, and the other applicant.',
+      this.uKCountryOfResidenceYes
+    );
+    await this.handleFormError(
+      'Select if you, and the other applicant if relevant, have lived in the UK, Channel Islands or Isle of Man for the last 12 months.'
+    );
   }
 
-  async handleFormError(errorMessage: string, eligibilityELement?: any): Promise<void> {
+  async handleFormError(errorMessage: string, eligibilityElement?: { check: () => Promise<void> }): Promise<void> {
     await this.clickContinue();
     await this.clickContinue();
     await expect(this.problemErrorMessage).toBeVisible;
     await expect(this.selectErrorMessage).toContainText([errorMessage]);
-    if (eligibilityELement) {
-        await eligibilityELement.check();
+    if (eligibilityElement) {
+      await eligibilityElement.check();
     }
   }
 }
