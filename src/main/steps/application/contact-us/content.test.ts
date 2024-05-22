afd08d73e99ed58d4d056353d621afd3aff1a9e8
suresh-sharada <88669246+suresh-sharada@users.noreply.github.com>
2024-05-22 09:08:07 +0100

    Update Contact us

--- src/main/steps/application/contact-us/content.test.ts
+++ src/main/steps/application/contact-us/content.test.ts
@@ -7,7 +7,11 @@ jest.mock('../../../app/form/validation');
 
 const en = {
   title: 'Adoption',
-  sendUsAMessageHeading: 'Send us a message',
+  sendUsAMessageHeading: 'Contact a court that deals with adoption',
+  findCourtTribunalDetails:
+    'Use the <a href="https://www.gov.uk/find-court-tribunal" class="govuk-link" target="blank">Find a Court or Tribunal service</a> ' +
+    ' to locate your nearest court that deals with adoption. This may not be the same court that made your placement order.',
+  askIfUnsure: 'If you are still unsure which court to contact, you can ask your social worker for help.',
   email: 'Email: <a href="mailto:adoptionproject@justice.gov.uk" class="govuk-link">adoptionproject@justice.gov.uk</a>',
   sendUsAMessageText: 'We aim to get back to you within 5 working days',
   telephoneHeading: 'Telephone',
@@ -18,6 +22,10 @@ const en = {
 const cy: typeof en = {
   title: 'Mabwysiadu',
   sendUsAMessageHeading: 'Anfonwch neges atom',
+  findCourtTribunalDetails:
+    'Use the <a href="https://www.gov.uk/find-court-tribunal" class="govuk-link" target="blank">Find a Court or Tribunal service</a> ' +
+    ' to locate your nearest court that deals with adoption. This may not be the same court that made your placement order.',
+  askIfUnsure: 'If you are still unsure which court to contact, you can ask your social worker for help.',
   email:
     'E-bost: <a href="mailto:ymholiadaucymraeg@justice.gov.uk" class="govuk-link">ymholiadaucymraeg@justice.gov.uk</a>',
   sendUsAMessageText: 'Byddwn yn anelu at ymateb o fewn 5 diwrnod gwaith.',
