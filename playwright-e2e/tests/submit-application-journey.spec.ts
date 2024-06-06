39dacf4ff05e571996464d5c93b0d21e13d393c7
Georgina <georgina.hall1@hmcts.net>
2024-06-06 17:26:59 +0100

    ADOP-2461 wip

--- playwright-e2e/tests/submit-application-journey.spec.ts
+++ playwright-e2e/tests/submit-application-journey.spec.ts
@@ -1,12 +1,157 @@
 import { test } from '@playwright/test';
-import { createCitizenUser, getAccessToken} from '../helpers/idam-test-api-helpers';
+import { createCitizenUser, deleteCitizenUser, getAccessToken } from '../helpers/idam-test-api-helpers';
+import * as dotenv from 'dotenv';
+import App from '../pages/app.page';
 
-test('check access token', async ({ page }) => {
-    await getAccessToken;
-    console.log(getAccessToken);
-});
+dotenv.config();
 
-test('create citizen', async ({ page }) => {
-    await createCitizenUser;
-    //console.log(createCitizenUser);
-});
+test.describe(
+  'e2e submit journeys',
+  {
+    tag: '@submit',
+  },
+  () => {
+    let userEmail: string;
+    let userPassword: string;
+    let userId: string;
+
+    test.beforeEach(async ({ page }) => {
+      const token = await getAccessToken();
+      if (token) {
+        const { email, password, id } = await createCitizenUser(token);
+        userEmail = email;
+        userPassword = password;
+        userId = id;
+      } else {
+        console.error('Failed to retrieve bearer token. User creation skipped.');
+      }
+    });
+
+    test.afterEach('Status check', async ({ page }) => {
+      const token = await getAccessToken();
+      if (token) {
+        try {
+          await deleteCitizenUser(token, userId);
+          console.info(`User with ID: ${userId} deleted request has been sent successfully, please allow 24 hrs.`);
+        } catch (error) {
+          console.error(`Failed to delete user with ID: ${userId}`, error);
+        }
+      } else {
+        console.error('Failed to retrieve bearer token. User deletion skipped.');
+      }
+    });
+
+    test('submitting application with spouse or civil partner', async ({ page }) => {
+      // Navigate to the start page
+      const app = new App(page);
+
+      await app.signIn.signIn(userEmail, userPassword);
+      await app.numberOfApplicants.applyWithSpouseOrCivil();
+      await app.basePage.clickSaveAndContinue();
+
+      // Date child moved in with you
+      await app.tasklist.dateChildMovedIn.click();
+      await app.dateChildMoved.dateChildMovedInToday();
+      await app.basePage.clickSaveAndContinue();
+
+      // Child's details before adoption
+      await app.tasklist.childsDetails.click();
+      await app.childDetails.childsName();
+      await app.basePage.clickSaveAndContinue();
+
+      // Child's details after adoption
+      await app.childDetails.childsName();
+      await app.basePage.clickSaveAndContinue();
+      await app.childDetails.childsDob();
+      await app.basePage.clickSaveAndContinue();
+
+      // Adoption agency and social worker
+      await app.tasklist.adoptionAgency.click();
+      await app.adoptionAgency.childsChildSocialWorkerDetails(); 
+      await app.basePage.clickSaveAndContinue();
+      await app.adoptionAgency.childsYourSocialWorkerDetails();
+      await app.basePage.clickSaveAndContinue();
+      await app.adoptionAgency.anotherAdoptionAgencyNo();
+      await app.basePage.clickSaveAndContinue();
+
+      // The family court details
+      await app.tasklist.familyCourtDetails.click();
+
+      await app.familyCourt.selectWhichCourt();
+      await app.basePage.clickSaveAndContinue();
+      await app.familyCourt.sameCourtYes();
+      await app.basePage.clickSaveAndContinue();
+
+      // First applicant Your personal details
+      await app.tasklist.firstApplicantPersonalDetails.click();
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.otherNamesSelectNo();
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.dob();
+      // await app.basePage.clickSaveAndContinue();
+      // await page.locator('#applicant1Occupation').click();
+      // await page.locator('#applicant1Occupation').fill('teacher');
+      // await app.basePage.clickSaveAndContinue();
+      // await page.getByLabel('No - I do not need any extra').check();
+      // await app.basePage.clickSaveAndContinue();
+
+      // // First applicant Your contact details
+      // await page.getByRole('link', { name: 'Your contact details  First' }).click();
+      // await page.getByLabel('Postcode').click();
+      // await page.getByLabel('Postcode').click();
+      // await page.getByLabel('Postcode').fill('SW1H 9AJ');
+      // await page.getByRole('button', { name: 'Find address' }).click();
+      // await page.getByLabel('Select an address').selectOption('0');
+      // await app.basePage.clickSaveAndContinue();
+      // await page.getByLabel('Email address').click();
+      // await page.getByLabel('Email address').fill('1234567890@domain.com');
+      // await page.getByLabel('UK phone number').click();
+      // await page.getByLabel('UK phone number').fill('0800800800');
+      // await app.basePage.clickSaveAndContinue();
+      // await page.getByLabel('English').check();
+      // await app.basePage.clickSaveAndContinue();
+
+      // // Second applicant Your personal details
+      // await page.getByRole('link', { name: 'Your personal details  Second' }).click();
+      // await page.getByLabel('First names').click();
+      // await page.getByLabel('First names').fill('first name');
+      // await page.getByLabel('Last names').click();
+      // await page.getByLabel('Last names').fill('surname');
+      // await page.getByRole('button', { name: 'Save and continue' }).click();
+      // await page.getByLabel('No', { exact: true }).check();
+      // await page.getByRole('button', { name: 'Save and continue' }).click();
+      // await page.getByLabel('Day').click();
+      // await page.getByLabel('Day').fill('30');
+      // await page.getByLabel('Month').click();
+      // await page.getByLabel('Month').fill('05');
+      // await page.getByLabel('Year').click();
+      // await page.getByLabel('Year').fill('2024');
+      // await app.basePage.clickSaveAndContinue();
+      // await page.locator('#applicant2Occupation').click();
+      // await page.locator('#applicant2Occupation').fill('teacher');
+      // await app.basePage.clickSaveAndContinue();
+      // await page.getByLabel('No - I do not need any extra').check();
+      // await app.basePage.clickSaveAndContinue();
+
+      // // Second applicant Your contact details
+      // await page.getByRole('link', { name: 'Your contact details  Second' }).click();
+      // await page.getByLabel('Yes').check();
+      // await app.basePage.clickSaveAndContinue();
+      // await page.getByLabel('Email address').click();
+      // await page.getByLabel('Email address').fill('firstname+lastname@domain.com');
+      // await page.getByLabel('UK phone number').click();
+      // await page.getByLabel('UK phone number').fill('0800800800');
+      // await app.basePage.clickSaveAndContinue();
+      // await page.getByLabel('English').check();
+      await app.basePage.clickSaveAndContinue();
+    });
+
+    test('submitting application with someone who is not my spouse or civil partner', async ({ page }) => {
+      console.log('what is this ?' + process.env.IDAM_TESTING_SUPPORT_USERS_URL);
+    });
+
+    test('submitting application on my own', async ({ page }) => {
+      // TO DO
+    });
+  }
+);
