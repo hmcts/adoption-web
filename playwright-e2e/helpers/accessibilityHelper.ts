797f99b99623c159bee3a32003b7c02886e7b431
abigailflorence97 <abigail.smith@hmcts.net>
2024-07-19 12:44:55 +0100

    fix linting

--- playwright-e2e/helpers/accessibilityHelper.ts
+++ playwright-e2e/helpers/accessibilityHelper.ts
@@ -1,5 +1,6 @@
 import AxeBuilder from '@axe-core/playwright';
 import { TestInfo } from '@playwright/test';
+
 import { expect } from '../fixtures/fixtures';
 
 type MakeAxeBuilder = () => AxeBuilder;
@@ -14,9 +15,9 @@ export async function runAccessibilityScan(makeAxeBuilder: MakeAxeBuilder, testI
     .disableRules(['aria-allowed-attr', 'target-size'])
     //axe-core triggers known GDS issue (https://github.com/alphagov/govuk-frontend/issues/979) on conditional radio buttons (https://design-system.service.gov.uk/components/radios/conditional-reveal/)
     //bug raised for target size: https://tools.hmcts.net/jira/browse/ADOP-2445
-    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22a", "wcag22aa"])
+    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa', 'wcag22a', 'wcag22aa'])
     .analyze();
   await attachTestInfo(testInfo, accessibilityScanResults);
   expect(accessibilityScanResults.violations).toEqual([]);
 }
-export { expect } from "@playwright/test";
+export { expect } from '@playwright/test';
