b34e72723b626138aab05cc4b51afbc2e78b7590
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-09-10 14:28:25 +0100

    Added test comment to playwright-e2e/pages/adoptionAgency.page.ts

--- playwright-e2e/pages/adoptionAgency.page.ts
+++ playwright-e2e/pages/adoptionAgency.page.ts
@@ -7,6 +7,8 @@ const randomPhoneNumber = faker.helpers.fromRegExp(/[1-7]{11}/);
 const randomSocialWorkerEmail = faker.internet.email({ firstName: `${randomFullName}`, provider: 'gov.uk' });
 const randomAuthorityEmail = faker.internet.email({ firstName: 'Sandwell', lastName: 'Council', provider: 'gov.uk' });
 
+
+// This is a test comment, Austen Stevens, 10/09/2024
 export default class AdoptionAgency {
   readonly heading: Locator;
   readonly nameOfChildsSocialWorker: Locator;
