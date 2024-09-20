f9ceb9648dfa70db837fa1cc773fa555a86fa47f
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-09-20 10:35:47 +0100

    spouse is not a partner citizen journey function completed

--- playwright-e2e/tests/submit-application-journey.spec.ts
+++ playwright-e2e/tests/submit-application-journey.spec.ts
@@ -38,7 +38,7 @@ test.describe('e2e submit journeys', () => {
   test(
     'submitting application where spouse is not a partner',
     e2eTestTags,
-    async({ page, makeAxeBuilder}, testInfo) => {
+    async ({ page, makeAxeBuilder }, testInfo) => {
       const app = new App(page);
       const appOneFirstName = faker.person.firstName();
       const appOneLastName = faker.person.lastName();
@@ -51,6 +51,91 @@ test.describe('e2e submit journeys', () => {
       await app.signIn.signIn(userEmail, userPassword);
       await app.numberOfApplicants.numberOfApplication('notSpouseOrCivilPartner');
       await app.numberOfApplicants.fillNotSpouseOrCivilPartnerDetails('Text to be randomly generated');
+      await app.basePage.clickSaveAndContinue();
+
+      // Date child move in with you
+      await app.tasklist.dateChildMovedIn.click();
+      await app.dateChildMoved.dateChildMovedInToday();
+      await app.basePage.clickSaveAndContinue();
+
+      //  Child's details before adoption
+      await app.tasklist.childsDetails.click();
+      await app.basePage.fillFirstLastName(appOneFirstName, appOneLastName);
+      await app.basePage.clickSaveAndContinue(); // where the error is occurring
+
+      // Child's details after adoption
+      await app.basePage.fillFirstLastName(childFirstName, childLastName);
+      await app.basePage.clickSaveAndContinue();
+      await app.childDetails.childsDob();
+      await app.basePage.clickSaveAndContinue();
+
+      // This is doing something else now
+      await app.tasklist.adoptionAgency.click();
+      await app.adoptionAgency.childsChildSocialWorkerDetails('Sandwell Metropolitan Council');
+      await app.basePage.saveAndContinue.click();
+      await app.adoptionAgency.childsYourSocialWorkerDetails('Sandwell Metropolitan Council');
+      await app.basePage.saveAndContinue.click();
+      await app.adoptionAgency.anotherAdoptionAgencyNo();
+      await app.basePage.saveAndContinue.click();
+
+      // The family court details
+      await app.tasklist.familyCourtDetails.click();
+      await app.basePage.selectLocation('Leicester County Court');
+      await app.basePage.clickSaveAndContinue();
+      await app.familyCourt.sameCourtYes();
+      await app.basePage.clickSaveAndContinue();
+
+      // First applicant Your personal details
+      await app.tasklist.firstApplicantPersonalDetails.click();
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.otherNamesSelectNo();
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.dob();
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.addOccupationFirst();
+      await app.basePage.clickSaveAndContinue();
+      await app.extraSupport.noSupportNeeded();
+      await app.basePage.clickSaveAndContinue();
+
+      // First applicant Your contact details
+      await app.tasklist.firstApplicantContactDetails.click();
+      await app.basePage.postcodeFindAddress('BN26 6AL', '0');
+      await app.basePage.clickSaveAndContinue();
+      await app.contactDetails.fillContactDetails('1234567890@domain.com', '0800800800');
+      await app.basePage.clickSaveAndContinue();
+      await app.contactDetails.englishLang.check();
+      await app.basePage.clickSaveAndContinue();
+
+      //Second applicant personal details
+      await app.tasklist.secondApplicantPersonalDetails.click();
+      await app.basePage.fillFirstLastName(appTwoFirstName, appTwoLastName);
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.otherNamesNo.check();
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.dob();
+      await app.basePage.clickSaveAndContinue();
+      await app.addApplicants.addOccupationSecond();
+      await app.basePage.clickSaveAndContinue();
+      await app.extraSupport.noSupportNeeded();
+      await app.basePage.clickSaveAndContinue();
+
+      //Second applicant contact details
+      await app.tasklist.secondApplicantContactDetails.click();
+      await app.page.getByLabel('Yes').check(); //do you live at the same address?
+      await app.basePage.clickSaveAndContinue();
+      await app.contactDetails.fillContactDetails('abcdefg@domain.com', '0800800800');
+      await app.basePage.clickSaveAndContinue();
+      await app.contactDetails.englishLang.check();
+      await app.basePage.clickSaveAndContinue();
+
+      //submit
+      await app.tasklist.reviewAndSubmit.click();
+      await app.pcq.noPcqAnswers();
+      await app.reviewSubmit.reviewAnswers('notSpouseOrCivilPartner');
+      await app.basePage.clickSaveAndContinue();
+      await app.reviewSubmit.statementOfTruthTwo(appOneFullname, appTwoFullname);
+      await app.reviewSubmit.fillCardDetails(appOneFullname, 'abcdefg@domain.com', 'BN26 6AL');
+      await runAccessibilityScan(makeAxeBuilder, testInfo);
     }
   );
 
