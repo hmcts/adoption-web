71d6cb2be3d16bf17230bd99bed32cb4cd0a2aa5
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-10-01 15:49:55 +0100

    submit-application-journey.spec.ts file refactored

--- playwright-e2e/tests/submit-application-journey.spec.ts
+++ playwright-e2e/tests/submit-application-journey.spec.ts
@@ -4,19 +4,10 @@ import { test as base } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 import { runAccessibilityScan } from '../helpers/accessibilityHelper';
-import {
-  citizenAdoptionApplicantContactDetails,
-  citizenAdoptionApplicantPersonalDetails,
-  citizenAdoptionChildDetailsAfterAdoption,
-  citizenAdoptionDateChildMovedIn,
-  citizenAdoptionFamilyCourtDetails,
-  citizenAdoptionSecondApplicantContactDetails,
-  citizenAdoptionSignIn,
-  citizenAdoptionSocialWorkDetails,
-  citzenAdoptionChildDetailsBeforeAdoption,
-} from '../helpers/e2eJourneyHelper';
+import * as e2eJourneyHelper from '../helpers/e2eJourneyHelper';
 import { setupUser, teardownUser } from '../hooks/createDeleteUser.hook';
 import App from '../pages/app.page';
+import { getPersonalDetailsStatus } from 'steps/application/task-list/utils';
 
 dotenv.config();
 
@@ -59,48 +50,46 @@ test.describe('e2e submit journeys', () => {
       const appTwoFullname = appTwoFirstName + ' ' + appTwoFirstName;
       const childFirstName = faker.person.firstName();
       const childLastName = faker.person.lastName();
-      await citizenAdoptionSignIn(app, userEmail, userPassword);
+      const stringNumberOfApplicationLocator = 'notSpouseOrCivilPartner';
+
+      // Sign-in
+      await e2eJourneyHelper.citizenAdoptionSignInWithNoPartner(
+        app,
+        userEmail,
+        userPassword,
+        stringNumberOfApplicationLocator
+      );
 
       // Date child move in with you
-      await citizenAdoptionDateChildMovedIn(app);
+      await e2eJourneyHelper.citizenAdoptionDateChildMovedIn(app);
 
       //  Child's details before adoption
-      await citzenAdoptionChildDetailsBeforeAdoption(app, appOneFirstName, appOneLastName);
+      await e2eJourneyHelper.citzenAdoptionChildDetailsBeforeAdoption(app, appOneFirstName, appOneLastName);
 
       // Child's details after adoption
-      await citizenAdoptionChildDetailsAfterAdoption(app, childFirstName, childLastName);
+      await e2eJourneyHelper.citizenAdoptionChildDetailsAfterAdoption(app, childFirstName, childLastName);
 
       // This is doing something else now
-      await citizenAdoptionSocialWorkDetails(app);
+      await e2eJourneyHelper.citizenAdoptionSocialWorkDetails(app);
 
       // The family court details
-      await citizenAdoptionFamilyCourtDetails(app);
+      await e2eJourneyHelper.citizenAdoptionFamilyCourtDetails(app);
 
       // First applicant Your personal details
-      await citizenAdoptionApplicantPersonalDetails(app);
+      await e2eJourneyHelper.citizenAdoptionApplicantPersonalDetails(app);
 
       // First applicant Your contact details
-      await citizenAdoptionApplicantContactDetails(app);
+      await e2eJourneyHelper.citizenAdoptionApplicantContactDetails(app);
 
       //Second applicant personal details
-      await citizenAdoptionSecondApplicantContactDetails(app, appTwoFirstName, appTwoLastName);
+      await e2eJourneyHelper.citizenAdoptionSecondApplicantPersonalDetails(app, appTwoFirstName, appTwoLastName);
 
       //Second applicant contact details
-      await app.tasklist.secondApplicantContactDetails.click();
-      await app.page.getByLabel('Yes').check(); //do you live at the same address?
-      await app.basePage.clickSaveAndContinue();
-      await app.contactDetails.fillContactDetails('abcdefg@domain.com', '0800800800');
-      await app.basePage.clickSaveAndContinue();
-      await app.contactDetails.englishLang.check();
-      await app.basePage.clickSaveAndContinue();
+      await e2eJourneyHelper.citizenAdoptionSecondApplicantContactDetails(app);
 
       //submit
-      await app.tasklist.reviewAndSubmit.click();
-      await app.pcq.noPcqAnswers();
-      await app.reviewSubmit.reviewAnswers('notSpouseOrCivilPartner');
-      await app.basePage.clickSaveAndContinue();
-      await app.reviewSubmit.statementOfTruthTwo(appOneFullname, appTwoFullname);
-      await app.reviewSubmit.fillCardDetails(appOneFullname, 'abcdefg@domain.com', 'BN26 6AL');
+      await e2eJourneyHelper.citizenAdoptionSubmitApplication(app, appOneFullname, appTwoFullname);
+
       await runAccessibilityScan(makeAxeBuilder, testInfo);
     }
   );
@@ -118,91 +107,46 @@ test.describe('e2e submit journeys', () => {
       const appTwoFullname = appTwoFirstName + ' ' + appTwoLastName;
       const childFirstName = faker.person.firstName();
       const childLastName = faker.person.lastName();
-      await app.signIn.signIn(userEmail, userPassword);
-      await app.numberOfApplicants.numberOfApplication('spouseOrCivilPartner');
-      await app.basePage.clickSaveAndContinue();
+      const stringNumberOfApplicationLocator = 'spouseOrCivilPartner';
+
+      // Sign in
+      await e2eJourneyHelper.citizenAdoptionSignInWithPartner(
+        app,
+        userEmail,
+        userPassword,
+        stringNumberOfApplicationLocator
+      );
 
       // Date child moved in with you
-      await app.tasklist.dateChildMovedIn.click();
-      await app.dateChildMoved.dateChildMovedInToday();
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citizenAdoptionDateChildMovedIn(app);
 
       // Child's details before adoption
-      await app.tasklist.childsDetails.click();
-      await app.basePage.fillFirstLastName(appOneFirstName, appOneLastName);
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citzenAdoptionChildDetailsBeforeAdoption(app, appOneFirstName, appOneFullname);
 
       // Child's details after adoption
-      await app.basePage.fillFirstLastName(childFirstName, childLastName);
-      await app.basePage.clickSaveAndContinue();
-      await app.childDetails.childsDob();
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citizenAdoptionChildDetailsAfterAdoption(app, childFirstName, childLastName);
 
-      await app.tasklist.adoptionAgency.click();
-      await app.adoptionAgency.childsChildSocialWorkerDetails('Sandwell Metropolitan Council');
-      await app.basePage.saveAndContinue.click();
-      await app.adoptionAgency.childsYourSocialWorkerDetails('Sandwell Metropolitan Council');
-      await app.basePage.saveAndContinue.click();
-      await app.adoptionAgency.anotherAdoptionAgencyNo();
-      await app.basePage.saveAndContinue.click();
+      // Social Worker Details
+      e2eJourneyHelper.citizenAdoptionFamilyCourtDetails(app);
 
       // The family court details
-      await app.tasklist.familyCourtDetails.click();
-      await app.basePage.selectLocation('Leicester County Court');
-      await app.basePage.clickSaveAndContinue();
-      await app.familyCourt.sameCourtYes();
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citizenAdoptionFamilyCourtDetails(app);
 
       // First applicant Your personal details
-      await app.tasklist.firstApplicantPersonalDetails.click();
-      await app.basePage.clickSaveAndContinue();
-      await app.addApplicants.otherNamesSelectNo();
-      await app.basePage.clickSaveAndContinue();
-      await app.addApplicants.dob();
-      await app.basePage.clickSaveAndContinue();
-      await app.addApplicants.addOccupationFirst();
-      await app.basePage.clickSaveAndContinue();
-      await app.extraSupport.noSupportNeeded();
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citizenAdoptionApplicantPersonalDetails(app);
 
       // First applicant Your contact details
-      await app.tasklist.firstApplicantContactDetails.click();
-      await app.basePage.postcodeFindAddress('BN26 6AL', '0');
-      await app.basePage.clickSaveAndContinue();
-      await app.contactDetails.fillContactDetails('1234567890@domain.com', '0800800800');
-      await app.basePage.clickSaveAndContinue();
-      await app.contactDetails.englishLang.check();
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citizenAdoptionApplicantContactDetails(app);
 
       //Second applicant personal details
-      await app.tasklist.secondApplicantPersonalDetails.click();
-      await app.basePage.fillFirstLastName(appTwoFirstName, appTwoLastName);
-      await app.basePage.clickSaveAndContinue();
-      await app.addApplicants.otherNamesNo.check();
-      await app.basePage.clickSaveAndContinue();
-      await app.addApplicants.dob();
-      await app.basePage.clickSaveAndContinue();
-      await app.addApplicants.addOccupationSecond();
-      await app.basePage.clickSaveAndContinue();
-      await app.extraSupport.noSupportNeeded();
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citizenAdoptionSecondApplicantPersonalDetails(app, appTwoFirstName, appTwoLastName);
 
       //Second applicant contact details
-      await app.tasklist.secondApplicantContactDetails.click();
-      await app.page.getByLabel('Yes').check(); //do you live at the same address?
-      await app.basePage.clickSaveAndContinue();
-      await app.contactDetails.fillContactDetails('abcdefg@domain.com', '0800800800');
-      await app.basePage.clickSaveAndContinue();
-      await app.contactDetails.englishLang.check();
-      await app.basePage.clickSaveAndContinue();
+      e2eJourneyHelper.citizenAdoptionSecondApplicantContactDetails(app);
 
       //submit
-      await app.tasklist.reviewAndSubmit.click();
-      await app.pcq.noPcqAnswers();
-      await app.reviewSubmit.reviewAnswers('spouseOrCivilPartner');
-      await app.basePage.clickSaveAndContinue();
-      await app.reviewSubmit.statementOfTruthTwo(appOneFullname, appTwoFullname);
-      await app.reviewSubmit.fillCardDetails(appOneFullname, 'abcdefg@domain.com', 'BN26 6AL');
+      e2eJourneyHelper.citizenAdoptionSubmitApplication(app, appOneFullname, appTwoFullname);
+
       await runAccessibilityScan(makeAxeBuilder, testInfo);
     }
   );
