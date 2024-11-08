3184008ac3c373ddf3652f1a9c3d4c83e8a40979
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-11-08 09:59:49 +0000

    check your answers refactored

--- playwright-e2e/page-objects/local-authority/laCheckYourAnswer.page.ts
+++ playwright-e2e/page-objects/local-authority/laCheckYourAnswer.page.ts
@@ -1,23 +1,19 @@
 import { Locator, Page } from '@playwright/test';
 
-export class LACheckYourAnswerPage {
+import BasePage from '../../pages/basePage.page';
+
+export class LACheckYourAnswerPage extends BasePage {
   readonly checkYourAnswersHeading: Locator;
 
   readonly continueButton: Locator;
   readonly saveAsDraftButton: Locator;
 
   constructor(page: Page) {
+    super(page);
+
     this.checkYourAnswersHeading = page.locator('h1').filter({ hasText: 'Check your answers' });
 
     this.continueButton = page.getByRole('button', { name: 'Continue' });
     this.saveAsDraftButton = page.getByRole('button', { name: 'Continue' });
-  }
-
-  async clickContinueButton(): Promise<void> {
-    await this.continueButton.click();
-  }
-
-  async clickSaveAsDraftButton(): Promise<void> {
-    await this.saveAsDraftButton.click();
   }
 }
