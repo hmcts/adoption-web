c4e9b77739e773fa08ab9286f0e43f664205aefb
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-11-14 09:37:23 +0000

    content test fixes, additions and redundant code removals

--- src/main/steps/la-portal/check-your-answers/content.test.ts
+++ src/main/steps/la-portal/check-your-answers/content.test.ts
@@ -19,6 +19,7 @@ const enContent = {
   title: 'Check your answers',
   change: 'Change',
   reason: 'Reason',
+  continue: 'Continue',
   submitApplication: 'Check your answers',
   checkInfoBeforeSubmit1:
     'Check all the information you have provided. If you need to edit any of the answers, select the change link at the end of the relevant answer.',
@@ -145,6 +146,7 @@ const cyContent: typeof enContent = {
   title: 'Adolygu eich atebion',
   change: 'Newid',
   reason: 'Rheswm',
+  continue: 'Parhau',
   submitApplication: 'Gwirio eich cais',
   checkInfoBeforeSubmit1:
     'Gwiriwch yr holl wybodaeth yr ydych wedi’i rhoi yn ofalus. Y cam nesaf yw arwyddo’r datganiad gwirionedd i ddatgan bod yr wybodaeth a roddwyd yn gywir. Unwaith y bydd wedi’i arwyddo a’r taliad wedi’i wneud, bydd eich cais yn cael ei gyflwyno i’r llys.',
@@ -295,10 +297,10 @@ describe('check-your-answer > content', () => {
     expect((form.submit.text as Function)(generatePageContent({ language: 'en' }))).toBe('Save and continue');
   });
 
-  /* test('should contain save-as-draft button', () => {
+  test('should contain welsh submit button in welsh content', () => {
     const generatedContent = generateContent(commonContent);
     const form = generatedContent.form as FormContent;
-    expect((form.saveAsDraft!.text as Function)(generatePageContent({ language: 'en' }))).toBe('Save as draft');
-  }); */
+    expect((form.submit.text as Function)(generatePageContent({ language: 'cy' }))).toBe('Cadw a pharhau');
+  });
 });
 /* eslint-enable @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any */
