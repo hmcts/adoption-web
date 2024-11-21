55938f71a7ca616a3a8b8da010e5e83688d1d044
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-11-21 14:25:22 +0000

    refactored file

--- playwright-e2e/page-objects/local-authority/birth-mother/laBirthMotherServeDocumentsPage.page.ts
+++ playwright-e2e/page-objects/local-authority/birth-mother/laBirthMotherServeDocumentsPage.page.ts
@@ -5,21 +5,9 @@ import BasePage from '../../../pages/basePage.page';
 export class LABirthMotherServeDocumentsPage extends BasePage {
   readonly birthMotherDocumentHeading: Locator;
 
-  readonly yesRadioButton: Locator;
-  readonly noRadioButton: Locator;
-
-  readonly saveAndContinueButton: Locator;
-  readonly saveAsDraftButton: Locator;
-
   constructor(page: Page) {
     super(page);
 
     this.birthMotherDocumentHeading = page.getByRole('heading', { name: 'Should the birth mother be' });
-
-    this.yesRadioButton = page.getByLabel('Yes');
-    this.noRadioButton = page.getByLabel('No');
-
-    this.saveAndContinueButton = page.getByRole('button', { name: 'Save and continue' });
-    this.saveAsDraftButton = page.getByRole('button', { name: 'Save as draft' });
   }
 }
