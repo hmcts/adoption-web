566ec8add0a955d345c0b481dfc002ebe15b3875
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-12-03 14:07:38 +0000

    removed redundant function reference

--- playwright-e2e/page-objects/local-authority/other-person/laOtherPersonResponsibilityPage.page.ts
+++ playwright-e2e/page-objects/local-authority/other-person/laOtherPersonResponsibilityPage.page.ts
@@ -5,17 +5,9 @@ import BasePage from '../../../pages/basePage.page';
 export class LAOtherPersonResponsibilityPage extends BasePage {
   readonly otherPersonResponsibilityHeading: Locator;
 
-  readonly noRadioButton: Locator;
-
   constructor(page: Page) {
     super(page);
 
     this.otherPersonResponsibilityHeading = page.getByRole('heading', { name: 'Is there another person who' });
-
-    this.noRadioButton = page.getByLabel('No', { exact: true });
-  }
-
-  async checkOtherPersonNoRadioButton(): Promise<void> {
-    await this.noRadioButton.check();
   }
 }
