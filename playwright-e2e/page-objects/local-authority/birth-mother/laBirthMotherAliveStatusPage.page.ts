60e109877d4819bb3d863b1a5b74173b3d059189
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2024-12-05 14:42:03 +0000

    birth mother radio button group refactor

--- playwright-e2e/page-objects/local-authority/birth-mother/laBirthMotherAliveStatusPage.page.ts
+++ playwright-e2e/page-objects/local-authority/birth-mother/laBirthMotherAliveStatusPage.page.ts
@@ -5,17 +5,22 @@ import BasePage from '../../../pages/basePage.page';
 export class LABirthMotherAliveStatusPage extends BasePage {
   readonly birthMotherStillAliveHeading: Locator;
 
+  readonly birthMotherGroup: Locator;
+
   readonly birthMotherAliveYesRadioButton: Locator;
   readonly birthMotherAliveNoRadioButton: Locator;
   readonly birthMotherAliveNotSureRadioButton: Locator;
 
   constructor(page: Page) {
     super(page);
+
+    this.birthMotherGroup = page.getByRole('group', { name: "Is the child's birth mother still alive?" });
+
     this.birthMotherStillAliveHeading = page.getByRole('heading', { name: "Is the child's birth mother" });
 
-    this.birthMotherAliveYesRadioButton = page.getByLabel('Yes');
-    this.birthMotherAliveNoRadioButton = page.getByLabel('No');
-    this.birthMotherAliveNotSureRadioButton = page.getByLabel('Not sure');
+    this.birthMotherAliveYesRadioButton = this.birthMotherGroup.getByLabel('Yes');
+    this.birthMotherAliveNoRadioButton = this.birthMotherGroup.getByLabel('No');
+    this.birthMotherAliveNotSureRadioButton = this.birthMotherGroup.getByLabel('Not sure');
   }
 
   async checkBirthMotherAliveYesRadioButton(): Promise<void> {
