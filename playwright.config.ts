2316dd2bc593baef3b0ec894ea186b39ef412b41
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2025-01-13 14:38:17 +0000

    Timeout increased

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,9 +13,9 @@ export default defineConfig({
   testDir: './playwright-e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  timeout: 80000,
+  timeout: 180000,
 
-  expect: { timeout: 80000 },
+  expect: { timeout: 180000 },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
