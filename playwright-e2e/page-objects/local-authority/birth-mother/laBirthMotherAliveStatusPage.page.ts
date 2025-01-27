13078ef18904ea7950ba7a5874eab2974f82ecd9
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2025-01-27 08:40:54 +0000

    LA birth mother merge page fix

--- playwright-e2e/page-objects/local-authority/birth-mother/laBirthMotherAliveStatusPage.page.ts
+++ playwright-e2e/page-objects/local-authority/birth-mother/laBirthMotherAliveStatusPage.page.ts
@@ -5,17 +5,22 @@ import BasePage from '../../../pages/basePage.page';
 export class LABirthMotherAliveStatusPage extends BasePage {
   readonly birthMotherStillAliveHeading: Locator;
 
+  readonly birthMotherGroup: Locator;
+
   readonly birthMotherAliveYesRadioButton: Locator;
   readonly birthMotherAliveNoRadioButton: Locator;
   readonly birthMotherAliveNotSureRadioButton: Locator;
 
   constructor(page: Page) {
     super(page);
+
     this.birthMotherStillAliveHeading = page.getByRole('heading', { name: "Is the child's birth mother" });
 
     this.birthMotherAliveYesRadioButton = page.getByLabel('Yes');
     this.birthMotherAliveNoRadioButton = page.getByLabel('No');
     this.birthMotherAliveNotSureRadioButton = page.getByLabel('Not sure');
+
+    this.birthMotherGroup = page.getByRole('group', { name: "Is the child's birth mother still alive?" });
   }
 
   async checkBirthMotherAliveYesRadioButton(): Promise<void> {
