e5b4b50c5dd2652cc573756d32198bbe12a52587
Austen Stevens <austen.stevens@justice.gov.uk>
2025-03-13 08:29:42 +0000

    ADOP-2608  Child details e2e tests (#1631)
    * Local Authority testing journey added
    
    * Birth certificate section removed
    
    * Mother section refactored
    
    * Father section refactored
    
    * base page super class extended
    
    * create fixture functionality implemented
    
    * create fixtures funtionality implemented into fixtures
    
    * laGettingStartedPag export amended
    
    * Child section refactored
    
    * Father section further refactoring
    
    * placement section refactored
    
    * Other person section refactored
    
    * sign in implementing base page super class
    
    * Create fixture fixture additions
    
    * Page Factory removed
    
    * sibligng court details refactored
    
    * Upload document refactored
    
    * statement of truth refactored
    
    * check your answers refactored
    
    * e2e tests refactored
    
    * navigateTo functionality implemented instead rather than constructor which triggers a bug on fixture instantiation
    
    * LA journey refactoring
    
    * fixed es lint and formatting issues
    
    * redundant code removed
    
    * citizen signin page fixture added
    
    * es lint fixes
    
    * Removal of button references
    
    * file and class name changes for fatherDocumentPage
    
    * removed redundant referecnes that are handled by the super class
    
    * removed redundant code that is handled by the super class
    
    * es lint fixes
    
    * Nationality super class added with small await corrections
    
    * Moother typo comment corrected to Mother
    
    * MotherDocument reference renamed to MotherServeDocument
    
    * missing await added
    
    * refactored file
    
    * Component redundancies removed
    
    * Bug fixes and citizen submitted page added for sign out functionality
    
    * Confirmation page url check
    
    * file rename changes
    
    * CZN renamed to CIT convention or citizen
    
    * base page nationality functions removed
    
    * InOrder renamed to Summary
    
    * removed redundant function reference
    
    * Moved laPlacementCourt to correct package
    
    * Note added to laChildDetailsPage
    
    * Aplly with page functionality added
    
    * Visibility test added
    
    * ApplyWith Citizen test coverage written
    
    * base interface added for later configuration
    
    * task list page class field setup
    
    * further task list component implementation
    
    * Bug fixes and task list tests added
    
    * Date child moved in e2e tests written and operational
    
    * Child full name tests and supported object added
    
    * ADOP-2617 - Name of Child e2e tests (#1630)
    
    * child adoption full name object created
    
    * refactoring and test template written
    
    * test class fixes and test finalisation
    
    * ADOP-2622 - Child DoB e2e tests (#1632)
    
    * Child DoB testing objects added
    
    * fixture fix
    
    * Further tests added for dob coverage
    
    * Test coverage completed
    
    * draft data e2e test fixed
    
    * PR comments revieweed and changed
    
    * refactoring
    
    * duplicate field reference removed
    
    * Additional changes based on comments
    
    * tooltips added and tested
    
    ---------
    
    Co-authored-by: GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>

---
+++ playwright-e2e/tests/citizen_journey/child-details/child-name-tests.spec.ts
@@ -0,0 +1,192 @@
+import { expect, test } from '../../../fixtures/fixtures';
+import { setupUser, teardownUser } from '../../../hooks/createDeleteUser.hook';
+import { runChangePageLanguageTest, runPageLanguageTest } from '../test-utils';
+test.describe('Citizen Journey child name test single parent', () => {
+  let userEmail: string;
+  let userPassword: string;
+  let userId: string;
+
+  test.beforeEach(async ({ signIn, citApplyingWithPage, citTaskListPage }) => {
+    const userInfo = await setupUser();
+    if (userInfo) {
+      userEmail = userInfo.email;
+      userPassword = userInfo.password;
+      userId = userInfo.id;
+    }
+    await signIn.navigateTo();
+    await signIn.signIn(userEmail, userPassword);
+    await citApplyingWithPage.checkApplyingOnMyOwnRadioButton();
+    await citApplyingWithPage.clickSaveAndContinue();
+    await citTaskListPage.clickChildDetailsLink();
+  });
+
+  test.afterEach('Status check', async () => {
+    await teardownUser(userId);
+  });
+
+  test('check default page is in English', async ({ citChildFullNamePage }) => {
+    await runPageLanguageTest('en', citChildFullNamePage);
+  });
+
+  test('check page is in Welsh after clicking Welsh language link', async ({ citChildFullNamePage }) => {
+    await runChangePageLanguageTest('cy', citChildFullNamePage);
+  });
+
+  test('check if page components are in correct visible state', async ({ citChildFullNamePage }) => {
+    await expect.soft(citChildFullNamePage.childDetailsTitle).toBeVisible();
+    await expect.soft(citChildFullNamePage.childFullNameHeading).toBeVisible();
+    await expect.soft(citChildFullNamePage.childHeadingTip).toBeVisible();
+    await expect.soft(citChildFullNamePage.firstNameTooltip).toBeVisible();
+    await expect.soft(citChildFullNamePage.lastNameTooltip).toBeVisible();
+    await expect.soft(citChildFullNamePage.firstName).toBeVisible();
+    await expect.soft(citChildFullNamePage.lastName).toBeVisible();
+    await expect.soft(citChildFullNamePage.contactUsForHelpDropdownLink).toBeVisible();
+    await expect.soft(citChildFullNamePage.contactACourtHeading).toBeHidden({ timeout: 500 });
+    await expect.soft(citChildFullNamePage.findACourtLink).toBeHidden({ timeout: 500 });
+
+    await expect.soft(citChildFullNamePage.saveAndContinue).toBeVisible();
+    await expect.soft(citChildFullNamePage.saveAsDraft).toBeVisible();
+
+    await expect.soft(citChildFullNamePage.errorFirstNameSummary).toBeHidden({ timeout: 500 });
+    await expect.soft(citChildFullNamePage.errorLastNameSummary).toBeHidden({ timeout: 500 });
+    expect(test.info().errors).toHaveLength(0);
+  });
+
+  test('check pressing save and continue button without filling required fields results in first name and last name error summaries', async ({
+    citChildFullNamePage,
+  }) => {
+    await citChildFullNamePage.clickSaveAndContinue();
+
+    await expect.soft(citChildFullNamePage.errorFirstNameSummary).toBeVisible();
+    await expect.soft(citChildFullNamePage.errorLastNameSummary).toBeVisible();
+
+    expect(test.info().errors).toHaveLength(0);
+  });
+
+  test('check pressing save and continue button filling in first name results in last name error summary', async ({
+    citChildFullNamePage,
+  }) => {
+    await citChildFullNamePage.fillFirstNameLabel('Joe');
+    await citChildFullNamePage.clickSaveAndContinue();
+
+    await expect.soft(citChildFullNamePage.errorFirstNameSummary).toBeHidden({ timeout: 500 });
+    await expect.soft(citChildFullNamePage.errorLastNameSummary).toBeVisible();
+
+    expect(test.info().errors).toHaveLength(0);
+  });
+
+  test('check pressing save and continue button filling in last name results in first name error summary', async ({
+    citChildFullNamePage,
+  }) => {
+    await citChildFullNamePage.fillLastNameLabel('Smith');
+    await citChildFullNamePage.clickSaveAndContinue();
+
+    await expect.soft(citChildFullNamePage.errorFirstNameSummary).toBeVisible();
+    await expect.soft(citChildFullNamePage.errorLastNameSummary).toBeHidden({ timeout: 500 });
+
+    expect(test.info().errors).toHaveLength(0);
+  });
+
+  test('check pressing draft button maintains filled first name label', async ({ page, citChildFullNamePage }) => {
+    await citChildFullNamePage.fillFirstNameLabel('Joe');
+    await citChildFullNamePage.clickSaveAsDraft();
+
+    const expectedUrl = 'https://adoption-web.aat.platform.hmcts.net/save-as-draft';
+    const actualUrl = page.url();
+    await expect(actualUrl).toBe(expectedUrl);
+
+    await page.goBack();
+
+    const expectedLabelValue = 'Joe';
+    const actualLabelValue = await citChildFullNamePage.firstName.inputValue();
+    await expect(actualLabelValue).toContain(expectedLabelValue);
+  });
+
+  test('check pressing draft button maintains filled last name label', async ({ page, citChildFullNamePage }) => {
+    await citChildFullNamePage.fillLastNameLabel('Smith');
+    await citChildFullNamePage.clickSaveAsDraft();
+
+    const expectedUrl = 'https://adoption-web.aat.platform.hmcts.net/save-as-draft';
+    const actualUrl = page.url();
+    await expect(actualUrl).toBe(expectedUrl);
+
+    await page.goBack();
+
+    const expectedLabelValue = 'Smith';
+    const actualLabelValue = await citChildFullNamePage.lastName.inputValue();
+    await expect(actualLabelValue).toContain(expectedLabelValue);
+  });
+
+  test('check pressing draft button maintains filled first name and last name labels', async ({
+    page,
+    citChildFullNamePage,
+  }) => {
+    await citChildFullNamePage.fillFirstNameLabel('Joe');
+    await citChildFullNamePage.fillLastNameLabel('Smith');
+    await citChildFullNamePage.clickSaveAsDraft();
+
+    const expectedUrl = 'https://adoption-web.aat.platform.hmcts.net/save-as-draft';
+    const actualUrl = page.url();
+    await expect(actualUrl).toBe(expectedUrl);
+
+    await page.goBack();
+
+    const expectedFirstNameLabelValue = 'Joe';
+    const expectedLastNameLabelValue = 'Smith';
+    const actualFirstNameLabelValue = await citChildFullNamePage.firstName.inputValue();
+    const actualLastNameLabelValue = await citChildFullNamePage.lastName.inputValue();
+
+    await expect.soft(actualFirstNameLabelValue).toBe(expectedFirstNameLabelValue);
+    await expect.soft(actualLastNameLabelValue).toBe(expectedLastNameLabelValue);
+
+    expect(test.info().errors).toHaveLength(0);
+  });
+
+  test('check pressing draft button then continuing with application maintains filled first name and last name labels', async ({
+    page,
+    citSaveAsDraftPage,
+    citTaskListPage,
+    citChildFullNamePage,
+  }) => {
+    await citChildFullNamePage.fillFirstNameLabel('Joe');
+    await citChildFullNamePage.fillLastNameLabel('Smith');
+    await citChildFullNamePage.clickSaveAsDraft();
+
+    let expectedUrl = 'https://adoption-web.aat.platform.hmcts.net/save-as-draft';
+    let actualUrl = page.url();
+    await expect(actualUrl).toBe(expectedUrl);
+
+    await citSaveAsDraftPage.clickContinueWithYourApplicationButton();
+
+    expectedUrl = 'https://adoption-web.aat.platform.hmcts.net/task-list';
+    actualUrl = page.url();
+    await expect(actualUrl).toBe(expectedUrl);
+
+    await citTaskListPage.clickChildDetailsLink();
+
+    const expectedFirstNameLabelValue = 'Joe';
+    const expectedLastNameLabelValue = 'Smith';
+    const actualFirstNameLabelValue = await citChildFullNamePage.firstName.inputValue();
+    const actualLastNameLabelValue = await citChildFullNamePage.lastName.inputValue();
+
+    await expect.soft(actualFirstNameLabelValue).toBe(expectedFirstNameLabelValue);
+    await expect.soft(actualLastNameLabelValue).toBe(expectedLastNameLabelValue);
+
+    expect(test.info().errors).toHaveLength(0);
+  });
+
+  test('check inserting valid name data then pressing save and continue advances to next step', async ({
+    page,
+    citChildFullNamePage,
+  }) => {
+    await citChildFullNamePage.fillFirstNameLabel('Joe');
+    await citChildFullNamePage.fillLastNameLabel('Smith');
+
+    await citChildFullNamePage.clickSaveAndContinue();
+
+    const expectedUrl = 'https://adoption-web.aat.platform.hmcts.net/children/full-name-after-adoption';
+    const actualUrl = page.url();
+
+    expect(actualUrl).toBe(expectedUrl);
+  });
+});
