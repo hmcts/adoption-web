426fefff0dc7e8b6e89f50dedace1c82b6776b70
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2025-04-22 10:28:47 +0100

    Tests and Page amended from code review

--- playwright-e2e/page-objects/citizen/adoption-agency-and-social-worker/citChildSocialWorkerDetailsPage.page.ts
+++ playwright-e2e/page-objects/citizen/adoption-agency-and-social-worker/citChildSocialWorkerDetailsPage.page.ts
@@ -5,6 +5,7 @@ import BasePage from '../../../pages/basePage.page';
 export class CITChildSocialWorkerDetailsPage extends BasePage {
   readonly applicationDetailsTitle: Locator;
   readonly childSocialWorkerTitle: Locator;
+  readonly childSocialWorkerSubtitle: Locator;
 
   readonly nameOfChildSocialWorkerText: Locator;
   readonly nameOfChildSocialWorkerLabel: Locator;
@@ -14,6 +15,7 @@ export class CITChildSocialWorkerDetailsPage extends BasePage {
 
   readonly emailAddressText: Locator;
   readonly emailAddressLabel: Locator;
+  readonly emailAddressHint: Locator;
 
   readonly childLocalAuthorityText: Locator;
   readonly childLocalAuthorityInputLabel: Locator;
@@ -21,6 +23,7 @@ export class CITChildSocialWorkerDetailsPage extends BasePage {
 
   readonly localAuthorityEmailText: Locator;
   readonly localAuthorityEmailLabel: Locator;
+  readonly localAuthorityEmailHint: Locator;
 
   readonly contactUsForHelpDropdownLink: Locator;
   readonly contactACourtHeading: Locator;
@@ -28,7 +31,7 @@ export class CITChildSocialWorkerDetailsPage extends BasePage {
 
   readonly errorNameOfChildSocialWorkerSummary: Locator;
   readonly errorEnterUKPhoneNumberSummary: Locator;
-  readonly errorNoOptionalEmailSummary: Locator;
+  readonly errorEmailFormatOptionalEmailSummary: Locator;
   readonly errorNoEmailSummary: Locator;
   readonly errorEnterLocalAuthoritySummary: Locator;
   readonly errorNonGovernmentEmail: Locator;
@@ -37,6 +40,9 @@ export class CITChildSocialWorkerDetailsPage extends BasePage {
     super(page);
     this.applicationDetailsTitle = page.getByText('Application details');
     this.childSocialWorkerTitle = page.getByRole('heading', { name: "Child's social worker details" });
+    this.childSocialWorkerSubtitle = page.getByText(
+      'You can get these details from your local authority or adoption agency.'
+    );
 
     this.nameOfChildSocialWorkerText = page.getByText("Name of child's social worker");
     this.nameOfChildSocialWorkerLabel = page.getByRole('textbox', { name: "Name of child's social worker" });
@@ -46,6 +52,7 @@ export class CITChildSocialWorkerDetailsPage extends BasePage {
 
     this.emailAddressText = page.getByText('Email address (if known)');
     this.emailAddressLabel = page.getByRole('textbox', { name: 'Email address (if known)' });
+    this.emailAddressHint = page.getByText('The email address should be an official government email');
 
     this.childLocalAuthorityText = page.getByText("Child's local authority");
     this.childLocalAuthorityInputLabel = page.locator('#location-picker');
@@ -53,6 +60,7 @@ export class CITChildSocialWorkerDetailsPage extends BasePage {
 
     this.localAuthorityEmailText = page.getByText('Local authority email address');
     this.localAuthorityEmailLabel = page.getByRole('textbox', { name: 'Local authority email address' });
+    this.localAuthorityEmailHint = page.getByText('This will be used to send a notification to the local authority');
 
     this.contactUsForHelpDropdownLink = page.getByText('Contact us for help');
     this.contactACourtHeading = page.getByRole('heading', { name: 'Contact a court that deals' });
@@ -60,7 +68,7 @@ export class CITChildSocialWorkerDetailsPage extends BasePage {
 
     this.errorNameOfChildSocialWorkerSummary = page.getByRole('link', { name: 'Enter name of child’s social worker' });
     this.errorEnterUKPhoneNumberSummary = page.getByRole('link', { name: 'Enter a UK telephone number' });
-    this.errorNoOptionalEmailSummary = page.locator('a[href="#childSocialWorkerEmail"]');
+    this.errorEmailFormatOptionalEmailSummary = page.locator('a[href="#childSocialWorkerEmail"]');
     this.errorNoEmailSummary = page.locator('a[href="#childLocalAuthorityEmail"]');
     this.errorEnterLocalAuthoritySummary = page.getByRole('link', { name: 'Enter name of local authority' });
     this.errorNonGovernmentEmail = page.getByRole('link', {
--- playwright-e2e/tests/citizen_journey/adoption-agency-and-social-worker/child-social-worker-tests.spec.ts
+++ playwright-e2e/tests/citizen_journey/adoption-agency-and-social-worker/child-social-worker-tests.spec.ts
@@ -23,7 +23,7 @@ test.describe('Citizen Journey child social worker test single parent', () => {
     await teardownUser(userId);
   });
 
-  test('chekc default page is in English', async ({ page }) => {
+  test('check default page is in English', async ({ page }) => {
     const langAttribute = await page.getAttribute('html', 'lang');
 
     expect(langAttribute).toMatch(/^en/);
@@ -43,24 +43,29 @@ test.describe('Citizen Journey child social worker test single parent', () => {
   test('check if page components are in correct visible state', async ({ citChildSocialWorkerDetailsPage }) => {
     await expect.soft(citChildSocialWorkerDetailsPage.applicationDetailsTitle).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.childSocialWorkerTitle).toBeVisible();
+    await expect.soft(citChildSocialWorkerDetailsPage.childSocialWorkerSubtitle).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.nameOfChildSocialWorkerText).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.nameOfChildSocialWorkerLabel).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.phoneNumberText).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.phoneNumberLabel).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.emailAddressText).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.emailAddressLabel).toBeVisible();
+    await expect.soft(citChildSocialWorkerDetailsPage.emailAddressHint).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.childLocalAuthorityText).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.childLocalAuthorityInputLabel).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.childLocalAuthorityDropdown).toBeHidden({ timeout: 500 });
     await expect.soft(citChildSocialWorkerDetailsPage.localAuthorityEmailText).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.localAuthorityEmailLabel).toBeVisible();
+    await expect.soft(citChildSocialWorkerDetailsPage.localAuthorityEmailHint).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.contactUsForHelpDropdownLink).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.contactACourtHeading).toBeHidden({ timeout: 500 });
     await expect.soft(citChildSocialWorkerDetailsPage.findACourtLink).toBeHidden({ timeout: 500 });
 
     await expect.soft(citChildSocialWorkerDetailsPage.errorNameOfChildSocialWorkerSummary).toBeHidden({ timeout: 500 });
     await expect.soft(citChildSocialWorkerDetailsPage.errorEnterUKPhoneNumberSummary).toBeHidden({ timeout: 500 });
-    await expect.soft(citChildSocialWorkerDetailsPage.errorNoOptionalEmailSummary).toBeHidden({ timeout: 500 });
+    await expect
+      .soft(citChildSocialWorkerDetailsPage.errorEmailFormatOptionalEmailSummary)
+      .toBeHidden({ timeout: 500 });
     await expect.soft(citChildSocialWorkerDetailsPage.errorNoEmailSummary).toBeHidden({ timeout: 500 });
     await expect.soft(citChildSocialWorkerDetailsPage.errorEnterLocalAuthoritySummary).toBeHidden({ timeout: 500 });
     await expect.soft(citChildSocialWorkerDetailsPage.errorNonGovernmentEmail).toBeHidden({ timeout: 500 });
@@ -71,14 +76,16 @@ test.describe('Citizen Journey child social worker test single parent', () => {
     expect(test.info().errors).toHaveLength(0);
   });
 
-  test('check pressing save and conitnue button without filling required field results in relevant errors', async ({
+  test('check pressing save and continue button without filling required field results in relevant errors', async ({
     citChildSocialWorkerDetailsPage,
   }) => {
     await citChildSocialWorkerDetailsPage.clickSaveAndContinue();
 
     await expect.soft(citChildSocialWorkerDetailsPage.errorNameOfChildSocialWorkerSummary).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.errorEnterUKPhoneNumberSummary).toBeVisible();
-    await expect.soft(citChildSocialWorkerDetailsPage.errorNoOptionalEmailSummary).toBeHidden({ timeout: 500 });
+    await expect
+      .soft(citChildSocialWorkerDetailsPage.errorEmailFormatOptionalEmailSummary)
+      .toBeHidden({ timeout: 500 });
     await expect.soft(citChildSocialWorkerDetailsPage.errorNoEmailSummary).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.errorEnterLocalAuthoritySummary).toBeVisible();
     await expect.soft(citChildSocialWorkerDetailsPage.errorNonGovernmentEmail).toBeHidden({ timeout: 500 });
@@ -86,44 +93,22 @@ test.describe('Citizen Journey child social worker test single parent', () => {
     expect(test.info().errors).toHaveLength(0);
   });
 
-  test('check incorrect format email results then pressing save and continue button results in relevant errors', async ({
+  test('check incorrect government email results then pressing save and continue button results in relevant errors', async ({
     citChildSocialWorkerDetailsPage,
   }) => {
-    await citChildSocialWorkerDetailsPage.fillEmailAddressLabel('test');
+    await citChildSocialWorkerDetailsPage.fillEmailAddressLabel('test@mailnator.com');
     await citChildSocialWorkerDetailsPage.clickSaveAndContinue();
 
-    await expect.soft(citChildSocialWorkerDetailsPage.errorNameOfChildSocialWorkerSummary).toBeVisible();
-    await expect.soft(citChildSocialWorkerDetailsPage.errorEnterUKPhoneNumberSummary).toBeVisible();
-    await expect.soft(citChildSocialWorkerDetailsPage.errorNoOptionalEmailSummary).toBeVisible();
-    await expect.soft(citChildSocialWorkerDetailsPage.errorNoEmailSummary).toBeVisible();
-    await expect.soft(citChildSocialWorkerDetailsPage.errorEnterLocalAuthoritySummary).toBeVisible();
-    await expect.soft(citChildSocialWorkerDetailsPage.errorNonGovernmentEmail).toBeHidden({ timeout: 500 });
-
-    expect(test.info().errors).toHaveLength(0);
+    await expect(citChildSocialWorkerDetailsPage.errorNonGovernmentEmail).toBeVisible();
   });
 
   test('check adding incorrect phone number then pressing save and continue button results in UK telephone number error', async ({
     citChildSocialWorkerDetailsPage,
   }) => {
-    await citChildSocialWorkerDetailsPage.fillNameOfChildSocialWorkerLabel('test');
     await citChildSocialWorkerDetailsPage.fillPhoneNumberLabel('01622');
-    await citChildSocialWorkerDetailsPage.selectLocalAuthority('Sandwell');
-    await citChildSocialWorkerDetailsPage.fillLocalAuthorityEmailLabel('ASTS@justice.gov.uk');
     await citChildSocialWorkerDetailsPage.clickSaveAndContinue();
 
     await expect(citChildSocialWorkerDetailsPage.errorEnterUKPhoneNumberSummary).toBeVisible();
-  });
-
-  test('check adding non approved goverment email address then pressing svae and continue button results in non approved error', async ({
-    citChildSocialWorkerDetailsPage,
-  }) => {
-    await citChildSocialWorkerDetailsPage.fillNameOfChildSocialWorkerLabel('test');
-    await citChildSocialWorkerDetailsPage.fillPhoneNumberLabel('01622 123456');
-    await citChildSocialWorkerDetailsPage.selectLocalAuthority('Sandwell');
-    await citChildSocialWorkerDetailsPage.fillLocalAuthorityEmailLabel('ASTS@justice.uk');
-    await citChildSocialWorkerDetailsPage.clickSaveAndContinue();
-
-    await expect(citChildSocialWorkerDetailsPage.errorNonGovernmentEmail).toBeHidden({ timeout: 500 });
   });
 
   test('check inserting data, saving draft then going back to page preserves data', async ({
@@ -166,6 +151,7 @@ test.describe('Citizen Journey child social worker test single parent', () => {
   }) => {
     await citChildSocialWorkerDetailsPage.fillNameOfChildSocialWorkerLabel('test');
     await citChildSocialWorkerDetailsPage.fillPhoneNumberLabel('01622 123456');
+    await citChildSocialWorkerDetailsPage.fillEmailAddressLabel('austen.stevens@justice.gov.uk');
     await citChildSocialWorkerDetailsPage.selectLocalAuthority('Sandwell');
     await citChildSocialWorkerDetailsPage.fillLocalAuthorityEmailLabel('austen.stevens@justice.gov.uk');
     await citChildSocialWorkerDetailsPage.clickSaveAndContinue();
