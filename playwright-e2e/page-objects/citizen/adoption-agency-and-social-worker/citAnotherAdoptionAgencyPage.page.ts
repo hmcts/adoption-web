8133812886257d66ef66f195e3433b5359d3dd8c
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2025-07-01 13:13:06 +0100

    page test structure amended

--- playwright-e2e/page-objects/citizen/adoption-agency-and-social-worker/citAnotherAdoptionAgencyPage.page.ts
+++ playwright-e2e/page-objects/citizen/adoption-agency-and-social-worker/citAnotherAdoptionAgencyPage.page.ts
@@ -6,25 +6,55 @@ export class CITAnotherAdoptionAgencyPage extends BasePage {
   readonly applicationDetailsTitle: Locator;
 
   readonly anotherAdoptionAgencyHeading: Locator;
-  readonly anotherAdoptionAgencyDescription: Locator;
 
-  readonly radioButtonGroup: Locator;
-  readonly yesRadioButton: Locator;
-  readonly noRadioButton: Locator;
+  readonly nameOfAdoptionAgencyHeading: Locator;
+  readonly nameOfAdoptionAgencyLabel: Locator;
 
-  readonly errorAnswerQuestionSummary: Locator;
+  readonly nameOfYourContactHeading: Locator;
+  readonly nameOfYourContactLabel: Locator;
+
+  readonly phoneNumberHeading: Locator;
+  readonly phoneNumberLabel: Locator;
+
+  readonly addressLineOneHeading: Locator;
+  readonly addressLineOneLabel: Locator;
+
+  readonly townOrCityHeading: Locator;
+  readonly townOrCityLabel: Locator;
+
+  readonly postcodeHeading: Locator;
+  readonly postcodeLabel: Locator;
+
+  readonly emailAddressHeading: Locator;
+  readonly emailAddressLabel: Locator;
 
   constructor(page: Page) {
     super(page);
-    this.applicationDetailsTitle = page.locator('span.govuk-caption-l');
+    this.applicationDetailsTitle = page.getByText('Application details');
 
-    this.anotherAdoptionAgencyHeading = page.locator('h1.govuk-heading-l');
-    this.anotherAdoptionAgencyDescription = page.locator('#hasAnotherAdopAgencyOrLA-hint');
+    this.anotherAdoptionAgencyHeading = page.getByRole('heading', {
+      name: 'Adoption agency or local authority details',
+    });
 
-    this.radioButtonGroup = page.locator('');
-    this.yesRadioButton = page.locator('');
-    this.noRadioButton = page.locator('');
+    this.nameOfAdoptionAgencyHeading = page.getByText('Name of adoption agency or local authority');
+    this.nameOfAdoptionAgencyLabel = page.getByRole('textbox', { name: 'Name of adoption agency or local authority' });
 
-    this.errorAnswerQuestionSummary = page.locator('');
+    this.nameOfYourContactHeading = page.getByText('Name of your contact');
+    this.nameOfYourContactLabel = page.getByRole('textbox', { name: 'Name of your contact' });
+
+    this.phoneNumberHeading = page.getByText('Phone number');
+    this.phoneNumberLabel = page.getByRole('textbox', { name: 'Phone number' });
+
+    this.addressLineOneHeading = page.getByText('Address line');
+    this.addressLineOneLabel = page.getByRole('textbox', { name: 'Address line' });
+
+    this.townOrCityHeading = page.getByText('Town or city');
+    this.townOrCityLabel = page.getByRole('textbox', { name: 'Town or city' });
+
+    this.postcodeHeading = page.getByText('Postcode');
+    this.postcodeLabel = page.getByRole('textbox', { name: 'Postcode' });
+
+    this.emailAddressHeading = page.getByText('Email address of your contact');
+    this.emailAddressLabel = page.getByRole('textbox', { name: 'Email address of your contact' });
   }
 }
