045e66107132249be9fcd3860192e286a24468e3
gaurav.chawla <gaurav.chawla@justice.gov.uk>
2025-09-03 12:34:05 +0100

    create case API

---
+++ playwright-e2e/utils/citizen-user.utils.ts
@@ -0,0 +1,49 @@
+import { IdamUtils } from '@hmcts/playwright-common';
+import { v4 as uuidv4 } from 'uuid';
+
+import { getAccessToken } from '../helpers/idamTestApiHelpers';
+
+type UserInfo = {
+  id: string;
+  email: string;
+  password: string;
+  forename: string;
+  surname: string;
+  sessionFile?: string;
+};
+
+export class CitizenUserUtils {
+  constructor(private idamUtils: IdamUtils) {}
+
+  async createUser(): Promise<UserInfo> {
+    const token = await getAccessToken();
+    const password = process.env.IDAM_CITIZEN_USER_PASSWORD as string;
+    const uniqueId = uuidv4();
+
+    const email = `TEST_ADOPTION_USER_citizen.${uniqueId}@test.local`;
+    const forename = 'fn_' + uniqueId.split('-')[0];
+    const surname = 'sn_' + uniqueId.split('-')[1];
+
+    const user = await this.idamUtils.createUser({
+      bearerToken: token || '',
+      password,
+      user: {
+        email,
+        forename,
+        surname,
+        roleNames: ['citizen', 'adoption-citizen-user'],
+      },
+    });
+    if (process.env.PWDEBUG) {
+      console.log(user);
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      password: user.password,
+      forename,
+      surname,
+    };
+  }
+}
--- src/main/app/auth/service/get-service-auth-token.ts
+++ src/main/app/auth/service/get-service-auth-token.ts
@@ -1,6 +1,5 @@
 import { Logger } from '@hmcts/nodejs-logging';
 import Axios from 'axios';
-import config from 'config';
 import { authenticator } from 'otplib';
 
 const logger = Logger.getLogger('service-auth-token');
@@ -9,9 +8,10 @@ let token;
 export const getTokenFromApi = async (): Promise<string> => {
   logger.info('Refreshing service auth token');
 
-  const url: string = config.get('services.authProvider.url') + '/lease';
-  const microservice: string = config.get('services.authProvider.microservice');
-  const secret: string = config.get('services.authProvider.secret');
+  // const url: string = config.get('services.authProvider.url') + '/lease';
+  const url = 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal/testing-support/lease';
+  const microservice = 'adoption_web';
+  const secret: any = process.env.IDAM_SECRET;
   const oneTimePassword = authenticator.generate(secret);
   const body = { microservice, oneTimePassword };
 
