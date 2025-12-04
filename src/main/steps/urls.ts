90119059dfe85618847f4eaab5a7ab7170ce3655
hfilmore <hannah.filmore@justice.gov.uk>
2025-12-04 13:20:34 +0000

    ADOP-2348 deprecate non-LA file upload urls

--- src/main/steps/urls.ts
+++ src/main/steps/urls.ts
@@ -10,9 +10,6 @@ export const TIMED_OUT_REDIRECT: PageLink = '/timed-out-redirect';
 export const KEEP_ALIVE_URL: PageLink = '/keep-alive';
 export const CSRF_TOKEN_ERROR_URL: PageLink = '/csrf-token-error';
 
-//export const RESPONDENT: PageLink = '/respondent';
-
-//export const APPLICATION_ENDED: PageLink = '/application-ended';
 export const TASK_LIST_URL: PageLink = '/task-list';
 
 export const APPLYING_WITH_URL: PageLink = '/applying-with';
@@ -48,16 +45,9 @@ export const APPLICANT_2_CONTACT_DETAILS: PageLink = `${APPLICANT_2}/contact-det
 export const APPLICANT_2_LANGUAGE_PREFERENCE: PageLink = `${APPLICANT_2}/language-preference`;
 
 export const CHILDREN: PageLink = '/children';
-// export const CHILDREN_PLACEMENT_ORDER_TYPE: PageLink = `${CHILDREN}/placement-order-type`;
-// export const CHILDREN_PLACEMENT_ORDER_NUMBER: PageLink = `${CHILDREN}/placement-order-number`;
-// export const CHILDREN_PLACEMENT_ORDER_COURT: PageLink = `${CHILDREN}/placement-order-court`;
-// export const CHILDREN_PLACEMENT_ORDER_DATE: PageLink = `${CHILDREN}/placement-order-date`;
-// export const CHILDREN_PLACEMENT_ORDER_SUMMARY: PageLink = `${CHILDREN}/placement-order-summary`;
-// export const CHILDREN_PLACEMENT_ORDER_CHECK_YOUR_ANSWERS: PageLink = `${CHILDREN}/placement-order-check-your-answers`;
 
 export const CHILDREN_FULL_NAME: PageLink = `${CHILDREN}/full-name`;
 export const CHILDREN_DATE_OF_BIRTH: PageLink = `${CHILDREN}/date-of-birth`;
-//export const CHILDREN_SEX_AT_BIRTH: PageLink = `${CHILDREN}/sex-at-birth`;
 /**
 * @deprecated CHILDREN_NATIONALITY has been moved to LA Portal
 */
@@ -67,11 +57,6 @@ export const CHILDREN_FIND_PLACEMENT_ORDER_COURT: PageLink = `${CHILDREN}/find-p
 export const CHILDREN_FIND_FAMILY_COURT: PageLink = `${CHILDREN}/find-family-court`;
 
 export const SIBLING: PageLink = '/sibling';
-
-// export const SIBLING_EXISTS: PageLink = `${SIBLING}/exists`;
-// export const SIBLING_RELATION: PageLink = `${SIBLING}/relation`;
-// export const SIBLING_ORDER_TYPE: PageLink = `${SIBLING}/placement-order-type`;
-// export const SIBLING_ORDER_CASE_NUMBER: PageLink = `${SIBLING}/placement-order-number`;
 /**
 * @deprecated SIBLING has been moved to LA Portal
 */
@@ -80,22 +65,16 @@ export const SIBLING_ORDER_SUMMARY: PageLink = `${SIBLING}/summary`;
 * @deprecated SIBLING has been moved to LA Portal
 */
 export const SIBLING_ORDER_CHECK_YOUR_ANSWERS: PageLink = `${SIBLING}/placement-order-check-your-answers`;
-// export const SIBLING_REMOVE_PLACEMENT_ORDER: PageLink = `${SIBLING}/remove-placement-order`;
 
 export const BIRTH_MOTHER: PageLink = '/birth-mother';
-// export const BIRTH_MOTHER_FULL_NAME: PageLink = `${BIRTH_MOTHER}/full-name`;
-// export const BIRTH_MOTHER_STILL_ALIVE: PageLink = `${BIRTH_MOTHER}/still-alive`;
 /**
 * @deprecated BIRTH_MOTHER section has been moved to LA Portal
 */
 export const BIRTH_MOTHER_NATIONALITY: PageLink = `${BIRTH_MOTHER}/nationality`;
-// export const BIRTH_MOTHER_OCCUPATION: PageLink = `${BIRTH_MOTHER}/occupation`;
-// export const BIRTH_MOTHER_ADDRESS_KNOWN: PageLink = `${BIRTH_MOTHER}/address-known`;
 /**
 * @deprecated BIRTH_MOTHER section has been moved to LA Portal
 */
 export const BIRTH_MOTHER_FIND_ADDRESS: PageLink = `${BIRTH_MOTHER}/address/lookup`;
-// export const BIRTH_MOTHER_SELECT_ADDRESS: PageLink = `${BIRTH_MOTHER}/address/select`;
 /**
 * @deprecated BIRTH_MOTHER section has been moved to LA Portal
 */
@@ -110,41 +89,21 @@ export const BIRTH_MOTHER_INTERNATIONAL_ADDRESS: PageLink = `${BIRTH_MOTHER}/add
 */
 export const RELATIONSHIP_DATE_URL: PageLink = '/date-from-certificate';
 
-//export const CHECK_JURISDICTION: PageLink = '/check-jurisdiction';
 /**
 * @deprecated
 */
 export const WHERE_YOUR_LIVES_ARE_BASED_URL: PageLink = '/where-your-lives-are-based';
-//export const JURISDICTION_INTERSTITIAL_URL: PageLink = '/you-can-use-english-welsh-courts';
-//export const RESIDUAL_JURISDICTION: PageLink = '/are-you-eligible-for-residual-jurisdiction';
-//export const JURISDICTION_MAY_NOT_BE_ABLE_TO: PageLink = '/you-may-not-be-able-to-england-and-wales';
-//export const JURISDICTION_CONNECTION_SUMMARY: PageLink = '/connection-summary';
-
-//export const YOUR_NAME: PageLink = '/enter-your-name';
-
-//export const APPLY_FINANCIAL_ORDER: PageLink = '/do-you-want-to-apply-financial-order';
-
-//export const SENT_TO_APPLICANT2_FOR_REVIEW: PageLink = '/application-sent-for-review';
 
 /**
 * @deprecated use REVIEW_PAY_SUBMIT section URLs instead
 */
 export const PAY_AND_SUBMIT: PageLink = '/pay-and-submit';
 
-//export const NO_RESPONSE_YET: PageLink = '/no-response-yet';
-
 export const APPLICATION_SUBMITTED: PageLink = '/application/submitted';
 
 export const DOWNLOAD_APPLICATION_SUMMARY: PageLink = '/download-summary';
 
 export const NEW_APPLICATION_REDIRECT: PageLink = '/new-application-redirect';
-
-//export const SWITCH_TO_SOLE_APPLICATION: PageLink = '/switch-to-sole-application';
-
-//export const CONFIRM_JOINT_APPLICATION: PageLink = '/confirm-your-joint-application';
-
-//export const HUB_PAGE: PageLink = '/hub-page';
-//export const HOW_DO_YOU_WANT_TO_RESPOND: PageLink = '/how-do-you-want-to-respond';
 
 export const ELIGIBILITY_URL: PageLink = '/eligibility';
 export const START_ELIGIBILITY_URL: PageLink = `${ELIGIBILITY_URL}/start`;
@@ -158,15 +117,10 @@ export const CHECK_ELIGIBILITY_URL_LIVED_UK: PageLink = `${ELIGIBILITY_URL}/live
 export const INELIGIBLE_TO_ADOPT: PageLink = `${ELIGIBILITY_URL}/cannot-apply`;
 
 export const OTHER_PARENT: PageLink = '/other-parent';
-// export const OTHER_PARENT_EXISTS: PageLink = `${OTHER_PARENT}/exists`;
-// export const OTHER_PARENT_NAME: PageLink = `${OTHER_PARENT}/full-name`;
-// export const OTHER_PARENT_RESPONSIBILITY_GRANTED: PageLink = `${OTHER_PARENT}/parental-responsibility-granted`;
-// export const OTHER_PARENT_ADDRESS_KNOWN: PageLink = `${OTHER_PARENT}/address-known`;
 /**
 * @deprecated OTHER_PARENT section has been moved to LA Portal
 */
 export const OTHER_PARENT_POSTCODE_LOOKUP: PageLink = `${OTHER_PARENT}/address/lookup`;
-// export const OTHER_PARENT_VERIFY_ADDRESS: PageLink = `${OTHER_PARENT}/address/select`;
 /**
 * @deprecated OTHER_PARENT section has been moved to LA Portal
 */
@@ -177,18 +131,10 @@ export const OTHER_PARENT_MANUAL_ADDRESS: PageLink = `${OTHER_PARENT}/address/ma
 export const OTHER_PARENT_INTERNATIONAL_ADDRESS: PageLink = `${OTHER_PARENT}/address/international`;
 
 export const BIRTH_FATHER = '/birth-father';
-// export const BIRTH_FATHER_NAME_ON_CERTIFICATE: PageLink = `${BIRTH_FATHER}/name-on-certificate`;
-// export const BIRTH_FATHER_FULL_NAME: PageLink = `${BIRTH_FATHER}/full-name`;
-// export const BIRTH_FATHER_ADDRESS_KNOWN: PageLink = `${BIRTH_FATHER}/address-known`;
-// export const BIRTH_FATHER_STILL_ALIVE: PageLink = `${BIRTH_FATHER}/alive`;
-// export const BIRTH_FATHER_PARENTAL_RESPONSIBILITY: PageLink = `${BIRTH_FATHER}/parental-responsibility`;
-// export const BIRTH_FATHER_PARENTAL_RESPONSIBILITY_GRANTED: PageLink = `${BIRTH_FATHER}/parental-responsibility/granted`;
-// export const BIRTH_FATHER_NO_PARENTAL_RESPONSIBILITY: PageLink = `${BIRTH_FATHER}/parental-responsibility/no-responsibility`;
 /**
 * @deprecated BIRTH_FATHER section has been moved to LA Portal
 */
 export const BIRTH_FATHER_ADDRESS_LOOKUP: PageLink = `${BIRTH_FATHER}/address/lookup`;
-//export const BIRTH_FATHER_ADDRESS_SELECT: PageLink = `${BIRTH_FATHER}/address/select`;
 /**
 * @deprecated BIRTH_FATHER section has been moved to LA Portal
 */
@@ -201,7 +147,6 @@ export const BIRTH_FATHER_INTERNATIONAL_ADDRESS: PageLink = `${BIRTH_FATHER}/add
 * @deprecated BIRTH_FATHER section has been moved to LA Portal
 */
 export const BIRTH_FATHER_NATIONALITY: PageLink = `${BIRTH_FATHER}/nationality`;
-//export const BIRTH_FATHER_OCCUPATION: PageLink = `${BIRTH_FATHER}/occupation`;
 
 export const REVIEW_PAY_SUBMIT: PageLink = '/review-pay-submit';
 export const CHECK_ANSWERS_URL: PageLink = `${REVIEW_PAY_SUBMIT}/check-your-answers`;
@@ -217,7 +162,13 @@ export const APPLICANT_SOCIAL_WORKER: PageLink = `${CHILDREN}/applicant-social-w
 export const STATEMENT_OF_TRUTH: PageLink = `${REVIEW_PAY_SUBMIT}/statement-of-truth`;
 export const EQUALITY: PageLink = `${REVIEW_PAY_SUBMIT}/equality`;
 
+/**
+* @deprecated see LA_PORTAL_UPLOAD_YOUR_DOCUMENTS
+*/
 export const UPLOAD_YOUR_DOCUMENTS: PageLink = '/upload-your-documents';
+/**
+* @deprecated see LA_DOCUMENT_MANAGER
+*/
 export const DOCUMENT_MANAGER: PageLink = '/document-manager';
 export const LA_DOCUMENT_MANAGER: PageLink = '/la-document-manager';
 
