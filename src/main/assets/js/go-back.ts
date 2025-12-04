ee62635acadaccfe5cc19671a1607e7b35c83114
hfilmore <hannah.filmore@justice.gov.uk>
2025-12-04 13:29:08 +0000

    ADOP-2348 removed unused code

--- src/main/assets/js/go-back.ts
+++ src/main/assets/js/go-back.ts
@@ -8,14 +8,7 @@ const backLink: HTMLAnchorElement | null = document.querySelector('.govuk-back-l
 if (backLink) {
   backLink.onclick = function (e) {
     e.preventDefault();
-    /* if (document.location.pathname === JURISDICTION_INTERSTITIAL_URL) {
-      document.location.pathname = CHECK_JURISDICTION;
-    } else if (
-      document.location.pathname === JURISDICTION_MAY_NOT_BE_ABLE_TO ||
-      document.location.pathname === JURISDICTION_CONNECTION_SUMMARY
-    ) {
-      document.location.pathname = WHERE_YOUR_LIVES_ARE_BASED_URL;
-    } else */ if (document.location.pathname === PAY_YOUR_FEE || document.location.pathname === PAY_AND_SUBMIT) {
+    if (document.location.pathname === PAY_YOUR_FEE || document.location.pathname === PAY_AND_SUBMIT) {
       document.location.href = `${PAYMENT_CALLBACK_URL}?back=true`;
     } else {
       history.go(-1);
