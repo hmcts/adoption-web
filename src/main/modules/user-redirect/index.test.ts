ad87b6727497cc7df661c0f332b1eb6de9a13398
hfilmore <hannah.filmore@justice.gov.uk>
2025-12-15 11:41:02 +0000

    ADOP-2348 added remaining unit tests

--- src/main/modules/user-redirect/index.test.ts
+++ src/main/modules/user-redirect/index.test.ts
@@ -3,17 +3,16 @@ jest.mock('@hmcts/nodejs-logging');
 import { Logger } from '@hmcts/nodejs-logging';
 import { Application } from 'express';
 
-//import { UserPathError } from '../../steps/error/error.controller';
+import { UserPathError } from '../../steps/error/error.controller';
 import { UserRole } from '../../app/case/definition';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
-import { KEEP_ALIVE_URL, TASK_LIST_URL} from '../../steps/urls';
+import { KEEP_ALIVE_URL, LA_PORTAL_TASK_LIST, TASK_LIST_URL} from '../../steps/urls';
 
 import { UserRedirectMiddleware } from '.';
 
 describe('user-redirect', () => {
   const req = {
     session: {},
-    //session: { user: {}, userCase: {} },
     path: '',
   };
   const res = mockResponse();
@@ -45,21 +44,28 @@ describe('user-redirect', () => {
 
   test('should call next for public links', () => {
     req.path = KEEP_ALIVE_URL;
+
     registeredMiddleware(req, res, mockNext);
     expect(mockNext).toHaveBeenCalled();
   });
 
-  // test('should not call next for Citizen links when no session.user', () => {
-  //   expect.assertions(1);
-  //   req.path = TASK_LIST_URL;
-  //   try {
-  //     registeredMiddleware(req, res, mockNext);
-  //   } catch (err) {
-  //     expect(err).toBeInstanceOf(UserPathError);
-  //     //expect(err.message).toMatch(/Citizen user id/);
-  //   }
-  //   //expect(res.statusCode).toBe(404);
-  // });
+  test('should throw for Citizen internal links when no session.user', () => {
+    req.path = TASK_LIST_URL;
+
+    return registeredMiddleware(req, res, mockNext).catch((err) => {
+      expect(err).toBeInstanceOf(UserPathError);
+      expect(err.message).toMatch(/Unauthorised user id undefined tried to access/);
+    });
+  });
+
+  test('should throw for LA internal links when no session.user', () => {
+    req.path = LA_PORTAL_TASK_LIST;
+
+    return registeredMiddleware(req, res, mockNext).catch((err) => {
+      expect(err).toBeInstanceOf(UserPathError);
+      expect(err.message).toMatch(/Unauthorised user id undefined tried to access/);
+    });
+  });
 
   test('should call next for citizen users acessing citizen links', () => {
     req.session = { 
@@ -69,8 +75,55 @@ describe('user-redirect', () => {
       }, 
       userCase: { id: '123' } };
     req.path = TASK_LIST_URL;
+
     registeredMiddleware(req, res, mockNext);
+
     expect(mockNext).toHaveBeenCalled();
-    //expect(mockLogger.warn).toHaveBeenCalledWith('Error in refreshing service auth token ');
+  });
+
+  test('should throw for citizen users acessing LA links', () => {
+    req.session = { 
+      user: {
+        id: 'user-123',
+        roles: [UserRole.CITIZEN],
+      }, 
+      userCase: { id: '123' } };
+    req.path = LA_PORTAL_TASK_LIST;
+
+    return registeredMiddleware(req, res, mockNext).catch((err) => {
+      expect(err).toBeInstanceOf(UserPathError);
+      expect(err.message).toMatch(/Citizen user id user-123 tried to access/);
+    });
+  });
+
+  test('should call next for LA users acessing LA links', () => {
+    req.session = { 
+      user: {
+        id: 'la-123',
+        roles: [UserRole.CASE_WORKER],
+        isSystemUser: true,
+      }, 
+      userCase: { id: '123' } };
+    req.path = LA_PORTAL_TASK_LIST;
+
+    registeredMiddleware(req, res, mockNext);
+
+    expect(mockNext).toHaveBeenCalled();
+  });
+
+  test('should throw for LA users acessing citizen links', () => {
+    req.session = { 
+      user: {
+        id: 'la-123',
+        roles: [UserRole.CASE_WORKER],
+        isSystemUser: true,
+      }, 
+      userCase: { id: '123' } };
+    req.path = TASK_LIST_URL;
+
+    return registeredMiddleware(req, res, mockNext).catch((err) => {
+      expect(err).toBeInstanceOf(UserPathError);
+      expect(err.message).toMatch(/LA user id la-123 tried to access/);
+    });
   });
 });
