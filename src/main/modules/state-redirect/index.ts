a4f218007022fadf43531a2e512e02787f2217c6
hfilmore <hannah.filmore@justice.gov.uk>
2025-12-15 16:02:57 +0000

    ADOP-2348 refactor & add unit tests

--- src/main/modules/state-redirect/index.ts
+++ src/main/modules/state-redirect/index.ts
@@ -29,11 +29,7 @@ import {
   LA_PORTAL_PRIVACY_POLICY,
   LA_PORTAL_TERMS_AND_CONDITIONS,
   NEW_APPLICATION_REDIRECT,
-  PAYMENT_CALLBACK_URL,
-  PAY_AND_SUBMIT,
-  PAY_YOUR_FEE,
   PRIVACY_POLICY,
-  PageLink,
   SAVE_AND_RELOGIN,
   SIGN_IN_URL,
   SIGN_OUT_URL,
@@ -90,19 +86,11 @@ export class StateRedirectMiddleware {
         logger.info(`StateRedirectMiddleware: Current path is ${req.path}`);
         logger.info(`StateRedirectMiddleware: Current state is ${req.session?.userCase?.state}`);
 
-        if (req.session?.userCase?.applyingWith === ApplyingWith.ALONE && req.path.startsWith(APPLICANT_2)) {
-          return res.redirect(TASK_LIST_URL);
-        }
-
-        if (this.PUBLIC_LINKS.some(item => req.path.startsWith(item))) {
-          //Footer links are accessible from anywhere in the application
+        if (this.isPublicLink(req)) {
           return next();
         }
-        if (
-          req.path.startsWith(LA_PORTAL) &&
-          req.path !== LA_PORTAL_CONFIRMATION_PAGE &&
-          [State.LaSubmitted].includes(req.session?.userCase?.state)
-        ) {
+
+        if (this.isInvalidUrlForLaSubmittedCase(req)) {
           logger.error(
             `User id ${req.session.user?.id} tried to access ${req.path} \
              after caseId ${req.session?.userCase?.id} LA Submitted`
@@ -110,11 +98,7 @@ export class StateRedirectMiddleware {
           return res.redirect(LA_PORTAL_CONFIRMATION_PAGE);
         }
 
-        if (
-          [State.Submitted, State.LaSubmitted].includes(req.session?.userCase?.state) &&
-          req.path !== HOME_URL &&
-          false === this.CITIZEN_SUBMITTED_CASE_URLS.some(item => req.path.startsWith(item))
-        ) {
+        if (this.isInvalidUrlAfterCitizenSubmit(req)) {
           logger.error(
             `User id ${req.session.user?.id} tried to access ${req.path} \
              but caseId ${req.session?.userCase?.id} in state ${req.session?.userCase?.state}`
@@ -122,22 +106,47 @@ export class StateRedirectMiddleware {
           return res.redirect(HOME_URL);
         }
 
-        if (
-          req.path.startsWith(CHECK_ANSWERS_URL) &&
-          getApplicationStatus(req.session.userCase) === SectionStatus.CAN_NOT_START_YET
-        ) {
-          // can not go to check-your-answers page before completing all the sections
+        if (this.isInvalidUrlForSingleApplicant(req)) {
           return res.redirect(TASK_LIST_URL);
         }
-        if (
-          req.session?.userCase?.state !== State.AwaitingPayment ||
-          [PAY_YOUR_FEE, PAY_AND_SUBMIT, PAYMENT_CALLBACK_URL].includes(req.path as PageLink)
-        ) {
-          return next();
+
+        if (this.isTryingToCheckAnswersBeforeAllSectionsComplete(req)) {
+          return res.redirect(TASK_LIST_URL);
         }
 
         return next();
       })
+    );
+  }
+
+  private isPublicLink(req: AppRequest): boolean {
+    return this.PUBLIC_LINKS.some(item => req.path.startsWith(item));
+  }
+
+  private isInvalidUrlForLaSubmittedCase(req: AppRequest): boolean {
+    return (
+      req.path.startsWith(LA_PORTAL) &&
+      req.path !== LA_PORTAL_CONFIRMATION_PAGE &&
+      [State.LaSubmitted].includes(req.session?.userCase?.state)
+    );
+  }
+
+  private isInvalidUrlAfterCitizenSubmit(req: AppRequest): boolean {
+    return (
+      [State.Submitted, State.LaSubmitted].includes(req.session?.userCase?.state) &&
+      req.path !== HOME_URL &&
+      false === this.CITIZEN_SUBMITTED_CASE_URLS.some(item => req.path.startsWith(item))
+    );
+  }
+
+  private isInvalidUrlForSingleApplicant(req: AppRequest): boolean {
+    return req.session?.userCase?.applyingWith === ApplyingWith.ALONE && req.path.startsWith(APPLICANT_2);
+  }
+
+  private isTryingToCheckAnswersBeforeAllSectionsComplete(req: AppRequest): boolean {
+    return (
+      req.path.startsWith(CHECK_ANSWERS_URL) &&
+      getApplicationStatus(req.session.userCase) === SectionStatus.CAN_NOT_START_YET
     );
   }
 }
