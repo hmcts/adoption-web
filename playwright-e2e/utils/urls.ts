9be3c5721da3ce92f1a44217732865bc96af0b3d
prabha15 <prabha.muthu@justice.gov.uk>
2026-05-14 09:04:46 +0100

    paramaterising TestURL

--- playwright-e2e/utils/urls.ts
+++ playwright-e2e/utils/urls.ts
@@ -4,14 +4,11 @@ dotenv.config();
 
 // aat, demo, perftest, ithc
 const env = process.env.ENVIRONMENT || 'aat';
-
 interface UrlConfig {
   [key: string]: string;
 }
 
 export const urlConfig: UrlConfig = {
-  citizenStartUrl:
-    process.env.TEST_URL + '/eligibility/start' || `https://adoption-web.${env}.platform.hmcts.net/eligibility/start`,
   laPortalUrl: process.env.TEST_URL + '/la-portal' || `https://adoption-web.${env}.platform.hmcts.net/la-portal`,
   citizenFrontendBaseUrl: process.env.TEST_URL || `https://adoption-web.${env}.platform.hmcts.net`,
 };
