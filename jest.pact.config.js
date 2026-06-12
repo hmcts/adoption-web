ac132652f271b78800a3281f55a7268419ef376b
hfilmore <hannah.filmore@justice.gov.uk>
2026-06-12 13:42:07 +0100

    ADOP-2841 improve clarity around Jest nested ESM dependency transpiling

--- jest.pact.config.js
+++ jest.pact.config.js
@@ -2,10 +2,10 @@ module.exports = {
   roots: ['<rootDir>/src/test/pact'],
   testRegex: String.raw`(/src/test/pact/*|\.test)\.(ts|js)$`,
   testEnvironment: 'node',
-  /* workaround to allow jest to transform ESM js files in node_modules
-   (e.g. https-proxy-agent which is pulled in by @pact-foundation/pact) */
   transform: {
-    [String.raw`^.+\.(ts|js)$`]: ['ts-jest', { tsconfig: { allowJs: true } }],
+    [String.raw`^.+\.(ts|js)$`]: ['ts-jest', {
+      tsconfig: { allowJs: true }
+    }],
   },
   transformIgnorePatterns: [
     'node_modules/(?!(@pact-foundation|https-proxy-agent|agent-base)/)',
