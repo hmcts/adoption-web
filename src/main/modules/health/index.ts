b9aa5e619eae6ec4cf66497aa706e73a759908cb
hfilmore <hannah.filmore@justice.gov.uk>
2026-09-10 18:08:39 +0100

    ADOP-2904 add HMCTS Access healthcheck

--- src/main/modules/health/index.ts
+++ src/main/modules/health/index.ts
@@ -14,12 +14,14 @@ export class HealthCheck {
       : null;
 
     const idamUrl = config.get('services.idam.tokenURL') as string;
+    const hmctsAccessUrl = config.get('services.idam.hmctsAccess.url') as string;
 
     healthcheck.addTo(app, {
       checks: {
         ...(redis ? { redis } : {}),
         'authProvider-api': healthcheck.web(new URL('/health', config.get('services.authProvider.url'))),
         'idam-api': healthcheck.web(new URL('/health', idamUrl.replace('/o/token', ''))),
+        'hmcts-access': healthcheck.web(new URL('/health', hmctsAccessUrl.replace('/o/authorize', ''))),
         'case-api': healthcheck.web(new URL('/health', config.get('services.case.url'))),
       },
       ...(redis
