5f166e659600976fd6a939c1644fac13f144e649
prabhamuthu15 <78162288+prabhamuthu15@users.noreply.github.com>
2026-09-18 11:16:01 +0100

    Mock case creation response for testing

--- src/main/app/case/CaseApi.ts
+++ src/main/app/case/CaseApi.ts
@@ -143,7 +143,7 @@ export class CaseApi {
       this.logger.info('createCase payload data', data);
       this.logger.info('createCase response id', response.data.id);
       response.data.data.status = response.data.state;
-      return { id: response.data.id, state: response.data.state, ...fromApiFormat(response.data.data) };
+      return { id: '', state: response.data.state, ...fromApiFormat(response.data.data) };
     } catch (err) {
       this.logError(err);
       throw new Error('Case could not be created.');
