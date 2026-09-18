c35fb504273b6a18a380e44a97db6d32da7b13c4
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2026-09-18 12:37:19 +0100

    fix content policy on form redirect chain

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import { Express, RequestHandler } from 'express';
 import helmet from 'helmet';
+import { getIdamUrl } from '../../app/auth/user/oidc';
 
 type ReferrerPolicyToken =
   | 'no-referrer'
@@ -74,6 +75,8 @@ export class Helmet {
       'https://www.gstatic.com',
     ];
 
+    const formAction = [self, getIdamUrl()];
+
     if (app.locals.developmentMode) {
       scriptSrc.push("'unsafe-eval'");
     }
@@ -89,6 +92,7 @@ export class Helmet {
           objectSrc: [self],
           scriptSrc,
           styleSrc: [self, ...tagManager, "'unsafe-inline'", 'https://fonts.googleapis.com'],
+          formAction,
         },
       }) as RequestHandler
     );
