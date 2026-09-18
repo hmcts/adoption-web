c02e7601acd29828a12e304af90a195d65e05853
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2026-09-18 13:47:51 +0100

    fix duplicate line after code merge

--- src/main/modules/helmet/index.ts
+++ src/main/modules/helmet/index.ts
@@ -1,7 +1,6 @@
 import * as express from 'express';
 import { Express, RequestHandler } from 'express';
 import helmet from 'helmet';
-import { getIdamUrl } from '../../app/auth/user/oidc';
 
 import { getIdamUrl } from '../../app/auth/user/oidc';
 
