462b2b5b4938a242d29d02cd21bd8bf8aac98764
prabhamuthu15 <78162288+prabhamuthu15@users.noreply.github.com>
2026-09-18 11:25:09 +0100

    RollBack

--- src/main/app/case/CaseApi.ts
+++ src/main/app/case/CaseApi.ts
@@ -140,10 +140,8 @@ export class CaseApi {
         event,
         event_token: token,
       });
-      this.logger.info('createCase payload data', data);
-      this.logger.info('createCase response id', response.data.id);
       response.data.data.status = response.data.state;
-      return { id: '', state: response.data.state, ...fromApiFormat(response.data.data) };
+      return { id: response.data.id, state: response.data.state, ...fromApiFormat(response.data.data) };
     } catch (err) {
       this.logError(err);
       throw new Error('Case could not be created.');
