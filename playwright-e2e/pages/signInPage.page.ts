73985929cbce916c3ef90bc6fdf5c7826fe423c0
prabhamuthu15 <78162288+prabhamuthu15@users.noreply.github.com>
2026-09-22 11:17:05 +0100

    fix eslint issue

--- playwright-e2e/pages/signInPage.page.ts
+++ playwright-e2e/pages/signInPage.page.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page ,expect} from '@playwright/test';
+import { type Locator, type Page, expect } from '@playwright/test';
 
 import { urlConfig } from '../utils/urls';
 
@@ -15,8 +15,8 @@ export default class SignIn {
     this.page = page;
     this.heading = page.getByRole('heading', { name: 'Sign in or create an account' });
     this.heading2 = page.getByRole('heading', { name: 'Sign in', exact: true });
-    this.email = page.getByRole('textbox', { name: 'Enter your email address',exact: true });
-    this.password = page.getByRole('textbox', { name: 'Enter your password' ,exact: true});
+    this.email = page.getByRole('textbox', { name: 'Enter your email address', exact: true });
+    this.password = page.getByRole('textbox', { name: 'Enter your password', exact: true });
     this.signinButton = page.getByRole('button', { name: 'Sign in' });
     this.continueButton = page.getByRole('button', { name: 'Continue' });
   }
@@ -27,14 +27,13 @@ export default class SignIn {
 
   async signIn(email: string, password: string): Promise<void> {
     await expect(
-      this.page.getByText( 'You may already have an account if you have used an HMCTS service before.')
+      this.page.getByText('You may already have an account if you have used an HMCTS service before.')
     ).toBeVisible();
     await this.signinButton.click();
     await this.email.fill(email);
     await this.continueButton.click();
     await this.password.fill(password);
     await this.continueButton.click();
-    await expect(this.page.getByText('Sign out',{exact:true})).toBeVisible();
-
+    await expect(this.page.getByText('Sign out', { exact: true })).toBeVisible();
   }
 }
