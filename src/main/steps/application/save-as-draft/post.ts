8b157c8d5a2675ed63ed6fa1612de7e80fda63e1
hfilmore <hannah.filmore@justice.gov.uk>
2026-09-25 16:20:16 +0100

    ADOP-2904 update to completely sign out from save-as-draft page

--- src/main/steps/application/save-as-draft/post.ts
+++ src/main/steps/application/save-as-draft/post.ts
@@ -4,7 +4,7 @@ import { Response } from 'express';
 import { AppRequest } from '../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../app/controller/PostController';
 import { FormFields, FormFieldsFn } from '../../../app/form/Form';
-import { TASK_LIST_URL } from '../../urls';
+import { SIGN_OUT_URL, TASK_LIST_URL } from '../../urls';
 
 @autobind
 export default class SaveAsDraftPostController extends PostController<AnyObject> {
@@ -15,7 +15,7 @@ export default class SaveAsDraftPostController extends PostController<AnyObject>
   public async post(req: AppRequest<AnyObject>, res: Response): Promise<void> {
     req.session.userCase.canPaymentIgnored = false;
     if (req.body.saveAsDraft) {
-      req.session.destroy(() => res.redirect('/'));
+      res.redirect(SIGN_OUT_URL);
     } else {
       return this.redirect(req, res, TASK_LIST_URL);
     }
