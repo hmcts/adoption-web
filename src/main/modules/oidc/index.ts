fbfcf1d5e6dfdc190adee1c5b20a70cd394413be
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2026-09-25 12:44:29 +0100

    save and log out

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -22,6 +22,7 @@ import {
   TERMS_AND_CONDITIONS,
   TIMED_OUT_REDIRECT,
   TIMED_OUT_URL,
+  SAVE_AND_RELOGIN,
 } from '../../steps/urls';
 
 /**
@@ -98,9 +99,9 @@ export class OidcMiddleware {
           }
         }
 
+        const lang = req.query.lang as string | undefined;
         if (req.path.startsWith(TIMED_OUT_REDIRECT)) {
           if (!req.session.laPortalKba) {
-            const lang = req.query.lang as string | undefined;
             logger.info('Citizen session has timed out. Lang = ', lang); // TODO remove
             return destroySessionsAndRedirect(req, res, next, `${TIMED_OUT_URL}?lang=${lang}`);
           } else {
@@ -108,8 +109,17 @@ export class OidcMiddleware {
             return destroySessionsAndRedirect(req, res, next, LA_PORTAL_KBA_CASE_REF);
           }
         }
+
         if (req.path.startsWith(TIMED_OUT_URL)) {
           return next();
+        }
+
+        if (req.path.startsWith(SAVE_AND_RELOGIN)) {
+          if (req.session?.user) {
+            return destroySessionsAndRedirect(req, res, next, `${SAVE_AND_RELOGIN}?lang=${lang}`);
+          } else {
+            return next();
+          }
         }
 
         if (
